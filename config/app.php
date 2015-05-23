391d880c294c2c5bb86ada4672674bcbd8457d5c
Ariel Vallese <alariva@gmail.com>
2015-05-23 12:57:01 -0400

    pkg: Add patricktalmadge/bootstrapper

--- config/app.php
+++ config/app.php
@@ -151,6 +151,8 @@ return [
 
 		'Intervention\Image\ImageServiceProvider',
 
+		'Bootstrapper\BootstrapperL5ServiceProvider',
+
 	],
 
 	/*
@@ -205,6 +207,31 @@ return [
 		'Debugbar' => 'Barryvdh\Debugbar\Facade',
 
 		'Image'    => 'Intervention\Image\Facades\Image',
+
+		'Accordion'      => 'Bootstrapper\Facades\Accordion',
+		'Alert'          => 'Bootstrapper\Facades\Alert',
+		'Badge'          => 'Bootstrapper\Facades\Badge',
+		'Breadcrumb'     => 'Bootstrapper\Facades\Breadcrumb',
+		'Button'         => 'Bootstrapper\Facades\Button',
+		'ButtonGroup'    => 'Bootstrapper\Facades\ButtonGroup',
+		'Carousel'       => 'Bootstrapper\Facades\Carousel',
+		'ControlGroup'   => 'Bootstrapper\Facades\ControlGroup',
+		'DropdownButton' => 'Bootstrapper\Facades\DropdownButton',
+		'Form'           => 'Bootstrapper\Facades\Form',
+		'Helpers'        => 'Bootstrapper\Facades\Helpers',
+		'Icon'           => 'Bootstrapper\Facades\Icon',
+		'InputGroup'     => 'Bootstrapper\Facades\InputGroup',
+		'Image'          => 'Bootstrapper\Facades\Image',
+		'Label'          => 'Bootstrapper\Facades\Label',
+		'MediaObject'    => 'Bootstrapper\Facades\MediaObject',
+		'Modal'          => 'Bootstrapper\Facades\Modal',
+		'Navbar'         => 'Bootstrapper\Facades\Navbar',
+		'Navigation'     => 'Bootstrapper\Facades\Navigation',
+		'Panel'          => 'Bootstrapper\Facades\Panel',
+		'ProgressBar'    => 'Bootstrapper\Facades\ProgressBar',
+		'Tabbable'       => 'Bootstrapper\Facades\Tabbable',
+		'Table'          => 'Bootstrapper\Facades\Table',
+		'Thumbnail'      => 'Bootstrapper\Facades\Thumbnail',
 
 	],
 
