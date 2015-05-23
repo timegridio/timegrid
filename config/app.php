6715d4c495b8b34013161eeed7cde9c316218df2
Ariel Vallese <alariva@gmail.com>
2015-05-23 12:32:28 -0400

    pkg: Add barryvdh/laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -145,6 +145,8 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 
+		'Barryvdh\Debugbar\ServiceProvider',
+
 	],
 
 	/*
@@ -192,6 +194,8 @@ return [
 		'URL'       => 'Illuminate\Support\Facades\URL',
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
+
+		'Debugbar' => 'Barryvdh\Debugbar\Facade',
 
 	],
 
