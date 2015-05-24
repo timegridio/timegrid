2a549d1f26996610df2e20920c9aa3af429066f1
Ariel Vallese <alariva@gmail.com>
2015-05-24 02:56:17 -0400

    pkg: Add guzzlehttp/guzzle for enabling mailing

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => 'support@xbooking.com.ar', 'name' => 'XBooking'],
 
 	/*
 	|--------------------------------------------------------------------------
