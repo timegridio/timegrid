ad2a0f3d0c9ba0a7101f884bf286939f0e5f0c05
Ariel Vallese <alariva@gmail.com>
2015-05-25 21:18:01 -0400

    pkg: Add laracasts/generators

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,10 @@ class AppServiceProvider extends ServiceProvider {
 			'Illuminate\Contracts\Auth\Registrar',
 			'App\Services\Registrar'
 		);
+    	
+    	if ($this->app->environment() == 'local') {
+    	    $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
+    	}
 	}
 
 }
