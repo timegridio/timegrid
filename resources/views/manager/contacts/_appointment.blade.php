9570b197c69e153432cfb49f1bec1e1ffd3acd8b
Ariel Vallese <alariva@gmail.com>
2015-06-26 01:13:34 +0200

    Enhance appointment layout

--- resources/views/manager/contacts/_appointment.blade.php
+++ resources/views/manager/contacts/_appointment.blade.php
@@ -1,16 +1,21 @@
 <div class="panel panel-default">
   <!-- Default panel contents -->
-  <div class="panel-heading">{{ trans('appointment.status.'.$appointment->statusLabel) }}</div>
+  <div class="panel-heading">{!! Icon::hand_up() !!}&nbsp;{{ trans('appointments.status.'.$appointment->statusLabel) }}</div>
   <div class="panel-body">
     <p>{{ $appointment->comments }}</p>
   </div>
 
   <!-- List group -->
   <ul class="list-group">
-    <li class="list-group-item">{!! Icon::calendar() !!} {{ $appointment->date }}</li>
-    <li class="list-group-item">{!! Icon::time() !!} {{ $appointment->time }}</li>
-    <li class="list-group-item">{!! Icon::hourglass() !!} {{ $appointment->finishTime }}</li>
   </ul>
+    <li class="list-group-item">
+        {!! Icon::calendar() !!}&nbsp;{{ $appointment->date }} 
+        {!! Icon::time() !!}&nbsp;{{ $appointment->time }} 
+        &nbsp;{{ trans('appointments.text.to') }}&nbsp;
+        {{ $appointment->finishTime }}&nbsp;
+        {!! Icon::hourglass() !!}&nbsp;{{ $appointment->duration }}&nbsp;
+        {{ trans('appointments.text.minutes') }}
+    </li>
 
-  <div class="panel-footer">{!! Icon::barcode() !!} {{ $appointment->code }}</div>
+  <div class="panel-footer">{!! Icon::barcode() !!}&nbsp;{{ $appointment->code }}</div>
 </div>
