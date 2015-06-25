c11807df66b182fadff2cab118f0176edd6fa95b
Ariel Vallese <alariva@gmail.com>
2015-06-25 21:27:24 +0200

    Change Contact index list layout

--- resources/views/manager/businesses/_contacts.blade.php
+++ resources/views/manager/businesses/_contacts.blade.php
@@ -1,21 +1,107 @@
-<div class="panel panel-default">
-	<div class="panel-heading">{{ $business->name }}</div>
+@section('scripts')
+<script>
+/*
+Please consider that the JS part isn't production ready at all, I just code it to show the concept of merging filters and titles together !
+*/
+$(document).ready(function(){
+    $('.filterable .btn-filter').click(function(){
+        var $panel = $(this).parents('.filterable'),
+        $filters = $panel.find('.filters input'),
+        $tbody = $panel.find('.table tbody');
+        if ($filters.prop('disabled') == true) {
+            $filters.prop('disabled', false);
+            $filters.first().focus();
+        } else {
+            $filters.val('').prop('disabled', true);
+            $tbody.find('.no-result').remove();
+            $tbody.find('tr').show();
+        }
+    });
 
-	<div class="panel-body">
+    $('.filterable .filters input').keyup(function(e){
+        /* Ignore tab key */
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        /* Useful DOM data and selectors */
+        var $input = $(this),
+        inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filterable'),
+        column = $panel.find('.filters th').index($input.parents('th')),
+        $table = $panel.find('.table'),
+        $rows = $table.find('tbody tr');
+        /* Dirtiest filter function ever ;) */
+        var $filteredRows = $rows.filter(function(){
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        /* Clean previous no-result if exist */
+        $table.find('tbody .no-result').remove();
+        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+        $rows.show();
+        $filteredRows.hide();
+        /* Prepend no-result row if all rows are filtered */
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
+        }
+    });
+});
+</script>
+@endsection
 
-		<table class="table table-stripped">
-		@foreach ($business->contacts as $contact)
-			<tr>
-				<td>{!! link_to(route('manager.contacts.show', $contact->id), $contact->firstname) !!}</td>
-				<td>{{ $contact->lastname }}</td>
-			</tr>
-		@endforeach
-		</table>
+@section('style')
+.filterable {
+    margin-top: 15px;
+}
+.filterable .panel-heading .pull-right {
+    margin-top: -20px;
+}
+.filterable .filters input[disabled] {
+    background-color: transparent;
+    border: none;
+    cursor: auto;
+    box-shadow: none;
+    padding: 0;
+    height: auto;
+}
+.filterable .filters input[disabled]::-webkit-input-placeholder {
+    color: #333;
+}
+.filterable .filters input[disabled]::-moz-placeholder {
+    color: #333;
+}
+.filterable .filters input[disabled]:-ms-input-placeholder {
+    color: #333;
+}
+@endsection
 
-	</div>
+    <div class="row">
+        <div class="panel panel-primary filterable">
+            <div class="panel-heading">
+                <h3 class="panel-title">Contacts</h3>
+                <div class="pull-right">
+                    <button class="btn btn-default btn-xs btn-filter"><span class="glyphicon glyphicon-filter"></span> Filter</button>
+                </div>
+            </div>
+            <table class="table">
+                <thead>
+                    <tr class="filters">
+                        <th><input type="text" class="form-control" placeholder="#" disabled></th>
+                        <th><input type="text" class="form-control" placeholder="First Name" disabled></th>
+                        <th><input type="text" class="form-control" placeholder="Last Name" disabled></th>
+                        <th><input type="text" class="form-control" placeholder="Username" disabled></th>
+                    </tr>
+                </thead>
+                <tbody>
+					@foreach ($business->contacts as $contact)			
+						<tr>			
+							<td>{{ $contact->id }}</td>
+							<td>{!! link_to(route('manager.contacts.show', $contact->id), $contact->firstname) !!}</td>
+							<td>{{ $contact->lastname }}</td>
+						</tr>
+					@endforeach
+                </tbody>
+            </table>
+        </div>
+	{!! Button::primary(trans('manager.businesses.contacts.btn.create'))->asLinkTo( action('ContactsController@create') ) !!}
+    </div>
 
-	<div class="panel-footer">
-		{!! Button::primary(trans('manager.businesses.contacts.btn.create'))->asLinkTo( action('ContactsController@create') ) !!}
-	</div>
-
-</div>
