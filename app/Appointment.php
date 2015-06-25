e193be44f4a4786f045aeee82d1efe70d86e57b0
Ariel Vallese <alariva@gmail.com>
2015-06-25 23:47:43 +0200

    Add appointment finishTime and statusLabel attributes

--- app/Appointment.php
+++ app/Appointment.php
@@ -2,13 +2,50 @@
 
 use Illuminate\Database\Eloquent\Model;
 
+use Carbon\Carbon;
+
 class Appointment extends Model {
 
 	protected $fillable = ['contact_id', 'business_id', 'date', 'time', 'duration', 'comments'];
 
+	const STATUS_RESERVED  = 'R';
+	const STATUS_CONFIRMED = 'C';
+	const STATUS_ANNULATED = 'A';
+	const STATUS_SERVED    = 'S';
+
 	public function contact()
 	{
 		return $this->belongsTo('\App\Contact');
+	}
+
+	public function getFinishTimeAttribute()
+	{
+		if (is_numeric($this->duration)) {
+			$carbon = new Carbon("{$this->date} {$this->time}");
+			return $carbon->addMinutes($this->duration);
+		}
+	}
+
+	public function getStatusLabelAttribute()
+	{
+		switch ($this->status) {
+			case Self::STATUS_RESERVED:
+				$label = trans('appointments.status.reserved');
+				break;
+			case Self::STATUS_CONFIRMED:
+				$label = trans('appointments.status.confirmed');
+				break;
+			case Self::STATUS_ANNULATED:
+				$label = trans('appointments.status.annulated');
+				break;
+			case Self::STATUS_SERVED:
+				$label = trans('appointments.status.annulated');
+				break;
+			default:
+				$label = '?';
+				break;
+		}
+		return $label;
 	}
 
 }
