9c0fa5bd742eaa91f8458f31e4dac475123346b0
Ariel Vallese <alariva@gmail.com>
2015-06-26 00:44:52 +0200

    Enhance appointment attributes display

--- resources/views/manager/contacts/_appointment.blade.php
+++ resources/views/manager/contacts/_appointment.blade.php
@@ -1,8 +1,6 @@
-
-
 <div class="panel panel-default">
   <!-- Default panel contents -->
-  <div class="panel-heading">{{ $appointment->statusName }}</div>
+  <div class="panel-heading">{{ $appointment->statusLabel }}</div>
   <div class="panel-body">
     <p>{{ $appointment->comments }}</p>
   </div>
@@ -13,4 +11,6 @@
     <li class="list-group-item">{!! Icon::time() !!} {{ $appointment->time }}</li>
     <li class="list-group-item">{!! Icon::hourglass() !!} {{ $appointment->finishTime }}</li>
   </ul>
+
+  <div class="panel-footer">{!! Icon::barcode() !!} {{ $appointment->code }}</div>
 </div>
