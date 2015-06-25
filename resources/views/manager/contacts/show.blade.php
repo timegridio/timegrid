c1a85febf176224e0ecaa4700ea1b96b6118c5e5
Ariel Vallese <alariva@gmail.com>
2015-06-25 21:27:57 +0200

    Change Contact profile layout

--- resources/views/manager/contacts/show.blade.php
+++ resources/views/manager/contacts/show.blade.php
@@ -1,315 +1,160 @@
 @extends('app')
 
 <style>
-.card {
-    padding-top: 20px;
-    margin: 10px 0 20px 0;
-    background-color: rgba(214, 224, 226, 0.2);
-    border-top-width: 0;
-    border-bottom-width: 2px;
-    -webkit-border-radius: 3px;
-    -moz-border-radius: 3px;
-    border-radius: 3px;
-    -webkit-box-shadow: none;
-    -moz-box-shadow: none;
-    box-shadow: none;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
+.user-row {
+    margin-bottom: 14px;
 }
 
-.card .card-heading {
-    padding: 0 20px;
-    margin: 0;
+.user-row:last-child {
+    margin-bottom: 0;
 }
 
-.card .card-heading.simple {
-    font-size: 20px;
-    font-weight: 300;
-    color: #777;
-    border-bottom: 1px solid #e5e5e5;
+.dropdown-user {
+    margin: 13px 0;
+    padding: 5px;
+    height: 100%;
 }
 
-.card .card-heading.image img {
-    display: inline-block;
-    width: 46px;
-    height: 46px;
-    margin-right: 15px;
-    vertical-align: top;
-    border: 0;
-    -webkit-border-radius: 50%;
-    -moz-border-radius: 50%;
-    border-radius: 50%;
+.dropdown-user:hover {
+    cursor: pointer;
 }
 
-.card .card-heading.image .card-heading-header {
-    display: inline-block;
-    vertical-align: top;
+.table-user-information > tbody > tr {
+    border-top: 1px solid rgb(221, 221, 221);
 }
 
-.card .card-heading.image .card-heading-header h3 {
-    margin: 0;
-    font-size: 14px;
-    line-height: 16px;
-    color: #262626;
+.table-user-information > tbody > tr:first-child {
+    border-top: 0;
 }
 
-.card .card-heading.image .card-heading-header span {
-    font-size: 12px;
-    color: #999999;
+.table-user-information > tbody > tr > td {
+    border-top: 0;
 }
-
-.card .card-body {
-    padding: 0 20px;
-    margin-top: 20px;
+.toppad
+{
+    margin-top:20px;
 }
-
-.card .card-media {
-    padding: 0 20px;
-    margin: 0 -14px;
-}
-
-.card .card-media img {
-    max-width: 100%;
-    max-height: 100%;
-}
-
-.card .card-actions {
-    min-height: 30px;
-    padding: 0 20px 20px 20px;
-    margin: 20px 0 0 0;
-}
-
-.card .card-comments {
-    padding: 20px;
-    margin: 0;
-    background-color: #f8f8f8;
-}
-
-.card .card-comments .comments-collapse-toggle {
-    padding: 0;
-    margin: 0 20px 12px 20px;
-}
-
-.card .card-comments .comments-collapse-toggle a,
-.card .card-comments .comments-collapse-toggle span {
-    padding-right: 5px;
-    overflow: hidden;
-    font-size: 12px;
-    color: #999;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-}
-
-.card-comments .media-heading {
-    font-size: 13px;
-    font-weight: bold;
-}
-
-.card.people {
-    position: relative;
-    display: inline-block;
-    width: 170px;
-    height: 300px;
-    padding-top: 0;
-    margin-left: 20px;
-    overflow: hidden;
-    vertical-align: top;
-}
-
-.card.people:first-child {
-    margin-left: 0;
-}
-
-.card.people .card-top {
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: inline-block;
-    width: 170px;
-    height: 150px;
-    background-color: #ffffff;
-}
-
-.card.people .card-top.green {
-    background-color: #53a93f;
-}
-
-.card.people .card-top.blue {
-    background-color: #427fed;
-}
-
-.card.people .card-info {
-    position: absolute;
-    top: 150px;
-    display: inline-block;
-    width: 100%;
-    height: 101px;
-    overflow: hidden;
-    background: #ffffff;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-}
-
-.card.people .card-info .title {
-    display: block;
-    margin: 8px 14px 0 14px;
-    overflow: hidden;
-    font-size: 16px;
-    font-weight: bold;
-    line-height: 18px;
-    color: #404040;
-}
-
-.card.people .card-info .desc {
-    display: block;
-    margin: 8px 14px 0 14px;
-    overflow: hidden;
-    font-size: 12px;
-    line-height: 16px;
-    color: #737373;
-    text-overflow: ellipsis;
-}
-
-.card.people .card-bottom {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    display: inline-block;
-    width: 100%;
-    padding: 10px 20px;
-    line-height: 29px;
-    text-align: center;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-}
-
-.card.hovercard {
-    position: relative;
-    padding-top: 0;
-    overflow: hidden;
-    text-align: center;
-    background-color: rgba(214, 224, 226, 0.2);
-}
-
-.card.hovercard .cardheader {
-    background: url("http://lorempixel.com/850/280/nature/4/");
-    background-size: cover;
-    height: 135px;
-}
-
-.card.hovercard .avatar {
-    position: relative;
-    top: -50px;
-    margin-bottom: -50px;
-}
-
-.card.hovercard .avatar img {
-    width: 100px;
-    height: 100px;
-    max-width: 100px;
-    max-height: 100px;
-    -webkit-border-radius: 50%;
-    -moz-border-radius: 50%;
-    border-radius: 50%;
-    border: 5px solid rgba(255,255,255,0.5);
-}
-
-.card.hovercard .info {
-    padding: 4px 8px 10px;
-}
-
-.card.hovercard .info .title {
-    margin-bottom: 4px;
-    font-size: 24px;
-    line-height: 1;
-    color: #262626;
-    vertical-align: middle;
-}
-
-.card.hovercard .info .desc {
-    overflow: hidden;
-    font-size: 12px;
-    line-height: 20px;
-    color: #737373;
-    text-overflow: ellipsis;
-}
-
-.card.hovercard .bottom {
-    padding: 0 20px;
-    margin-bottom: 17px;
-}
-
-.btn{ border-radius: 50%; width:32px; height:32px; line-height:18px;  }
 </style>
-
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+@endsection
 
 @section('content')
 <div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{ $contact->firstname }} {{ $contact->lastname }}</div>
 
-				<div class="panel-body">
-					@include('flash::message')
+    @include('flash::message')
 
-					@if($errors->has())
-						<div class="alert alert-danger">
-							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-							<ul>
-							@foreach($errors->all() as $error)
-								<li>{{ $error }}</li>
-							@endforeach
-							</ul>
-						</div>
-					@endif
+    @if($errors->has())
+        <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+            </ul>
+        </div>
+    @endif
 
-            		<div class="card hovercard">
-		            	<div class="cardheader"></div>
-            		    <div class="avatar">
-            		        <img alt="" src="http://lorempixel.com/100/100/people/9/">
-            		    </div>
-            		    <div class="info">
-            		        <div class="title">
-            		            {{ $contact->firstname }} {{ $contact->lastname }}
-            		        </div>
-            		        <div class="desc">{{ $contact->mobile }}</div>
-            		        <div class="desc">{{ $contact->birthdate }} {{ $contact->age() }}</div>
-            		        <div class="desc">{{ $contact->notes }}</div>
-            		        <div class="desc">{{ $contact->businesses()->first()->slug }}</div>
-            		    </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
 
-            		    <div class="bottom">
-            		        <a class="btn btn-primary btn-twitter btn-sm" href="https://twitter.com/webmaniac">
-            		            <i class="fa fa-twitter"></i>
-            		        </a>
-            		        <a class="btn btn-danger btn-sm" rel="publisher"
-            		           href="https://plus.google.com/+ahmshahnuralam">
-            		            <i class="fa fa-google-plus"></i>
-            		        </a>
-            		        <a class="btn btn-primary btn-sm" rel="publisher"
-            		           href="https://plus.google.com/shahnuralam">
-            		            <i class="fa fa-facebook"></i>
-            		        </a>
-            		        <a class="btn btn-warning btn-sm" rel="publisher" href="https://plus.google.com/shahnuralam">
-            		            <i class="fa fa-behance"></i>
-            		        </a>
-            		    </div>
-            		</div>
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title">{{ $contact->fullname }}</h3>
+            </div>
 
-				</div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=100" class="img-circle"> </div>
+                
+                <div class=" col-md-9 col-lg-9 "> 
+                  <table class="table table-user-information">
+                    <tbody>
+                    <tr>
+                        <td>{{ trans('manager.contacts.label.birthdate') }}</td>
+                        <td>{{ $contact->birthdate }} ({{ $contact->age() }})</td>
+                    </tr>
+                    <tr>
+                        <td>{{ trans('manager.contacts.label.notes') }}</td>
+                        <td>{{ $contact->notes }}</td>
+                    </tr>
+                    <tr>
+                        <td>Other</td>
+                        <td>#</td>
+                    </tr>
+                   
+                    <tr>
+                      <tr>
+                        <td>{{ trans('manager.contacts.label.gender') }}</td>
+                        <td>{{ trans('manager.contacts.label.gender.'.$contact->gender) }}</td>
+                      </tr>
+                        <tr>
+                        <td>Home Address</td>
+                        <td>#</td>
+                      </tr>
+                      <tr>
+                        <td>Email</td>
+                        <td>#</td>
+                      </tr>
+                        <td>{{ trans('manager.contacts.label.mobile') }}</td>
+                        <td>{{ $contact->mobile }}</td>
+                    </tr>
+                     
+                    </tbody>
+                  </table>
+                  
+                  <a href="#" class="btn btn-primary">Reservar Turno</a>
+                  <a href="#" class="btn btn-primary">Contactar</a>
+                </div>
+              </div>
+            </div>
+                 <div class="panel-footer">
+                        <a data-original-title="Broadcast Message" data-toggle="tooltip" type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-envelope"></i></a>
+                        <span class="pull-right">
+                            <a href="edit.html" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
+                            <a data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
+                        </span>
+                 </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
 
-				<div class="panel-footer">
-                    has {{ $contact->hasAppointment() }} / |
-                    c: {{ $contact->appointmentsCount }}
-				</div>
+@section('scripts')
+<script>
+$(document).ready(function() {
+    var panels = $('.user-infos');
+    var panelsButton = $('.dropdown-user');
+    panels.hide();
 
-			</div>
-		</div>
-	</div>
-</div>
+    //Click dropdown
+    panelsButton.click(function() {
+        //get data-for attribute
+        var dataFor = $(this).attr('data-for');
+        var idFor = $(dataFor);
+
+        //current button
+        var currentButton = $(this);
+        idFor.slideToggle(400, function() {
+            //Completed slidetoggle
+            if(idFor.is(':visible'))
+            {
+                currentButton.html('<i class="glyphicon glyphicon-chevron-up text-muted"></i>');
+            }
+            else
+            {
+                currentButton.html('<i class="glyphicon glyphicon-chevron-down text-muted"></i>');
+            }
+        })
+    });
+
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('button').click(function(e) {
+        e.preventDefault();
+        alert("This is a demo.\n :-)");
+    });
+});
+</script>
 @endsection
