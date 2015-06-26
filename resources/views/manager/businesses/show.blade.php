9f829cac92e33609f9415aa7925ea97ffeeb2c72
Ariel Vallese <alariva@gmail.com>
2015-06-26 23:28:53 +0200

    Enhance Business show layout

--- resources/views/manager/businesses/show.blade.php
+++ resources/views/manager/businesses/show.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="container">
-
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
+
 			<div class="panel panel-default">
-				<div class="panel-heading">{{ trans('manager.businesses.show.title') }}</div>
+				<div class="panel-heading">{{ $business->name }}</div>
 
 				<div class="panel-body">
 					@include('flash::message')
@@ -22,20 +22,13 @@
 						</div>
 					@endif
 
-					<table class="table table-stripped">
-						<tr>
-							<td>{!! Button::primary($business->slug)->asLinkTo(action('BusinessesController@edit', $business)) !!}</td>
-							<td>{{ $business->name }}</td>
-							<td>{{ $business->description }}</td>
-						</tr>
-					</table>
+					<p>{{ $business->description }}</p>
 
 				</div>
 
 				<div class="panel-footer">
-
+				{!! Button::withIcon(Icon::edit())->primary()->asLinkTo(action('BusinessesController@edit', $business)) !!}
 				</div>
-
 			</div>
 
 			@include('manager.businesses._contacts')
