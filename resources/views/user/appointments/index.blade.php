b4a3ddd351fd0424941261c729f154128ac8379a
Ariel Vallese <alariva@gmail.com>
2015-06-30 00:51:28 +0200

    Make Appointment code to be displayed with Bootstrap code for cleaner reading

--- resources/views/user/appointments/index.blade.php
+++ resources/views/user/appointments/index.blade.php
@@ -1,12 +1,12 @@
 @extends('app')
 
 @section('content')
-
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">{{ trans('user.appointments.index.title') }}</div>
+				
+        <div class="panel-heading">{{ trans('user.appointments.index.title') }}</div>
 
 				<div class="panel-body">
 
@@ -15,7 +15,7 @@
           <table class="table table-condensed">
             @foreach ($appointments as $appointment)
               <tr>
-                <td>{{ $appointment->code }}</td>
+                <td><code>{{ $appointment->code }}</code></td>
                 <td>{{ $appointment->status }}</td>
                 <td>{{ $appointment->start_at->timezone($appointment->tz)->toDateString() }}</td>
                 <td title="{{ $appointment->tz }}">{{ $appointment->start_at->timezone($appointment->tz)->toTimeString() }}</td>
@@ -31,93 +31,8 @@
 				<div class="panel-footer">
 
 				</div>
-
 			</div>
 		</div>
 	</div>
 </div>
-
-@endsection
-
-@section('scripts')
-<script type="text/javascript">
-/*
-<a href="posts/2" data-method="delete"> <---- We want to send an HTTP DELETE request
-
-- Or, request confirmation in the process -
-
-<a href="posts/2" data-method="delete" data-confirm="Are you sure?">
-*/
- 
-(function() {
- 
-  var laravel = {
-    initialize: function() {
-      this.methodLinks = $('a[data-method]');
- 
-      this.registerEvents();
-    },
- 
-    registerEvents: function() {
-      this.methodLinks.on('click', this.handleMethod);
-    },
- 
-    handleMethod: function(e) {
-      var link = $(this);
-      var httpMethod = link.data('method').toUpperCase();
-      var form;
- 
-      // If the data-method attribute is not PUT or DELETE,
-      // then we don't know what to do. Just ignore.
-      if ( $.inArray(httpMethod, ['PUT', 'DELETE']) === - 1 ) {
-        return;
-      }
- 
-      // Allow user to optionally provide data-confirm="Are you sure?"
-      if ( link.data('confirm') ) {
-        if ( ! laravel.verifyConfirm(link) ) {
-          return false;
-        }
-      }
- 
-      form = laravel.createForm(link);
-      form.submit();
- 
-      e.preventDefault();
-    },
- 
-    verifyConfirm: function(link) {
-      return confirm(link.data('confirm'));
-    },
- 
-    createForm: function(link) {
-      var form = 
-      $('<form>', {
-        'method': 'POST',
-        'action': link.attr('href')
-      });
- 
-      var token = 
-      $('<input>', {
-        'type': 'hidden',
-        'name': '_token',
-          'value': '{{{ csrf_token() }}}' // hmmmm...
-        });
- 
-      var hiddenInput =
-      $('<input>', {
-        'name': '_method',
-        'type': 'hidden',
-        'value': link.data('method')
-      });
- 
-      return form.append(token, hiddenInput)
-                 .appendTo('body');
-    }
-  };
- 
-  laravel.initialize();
- 
-})();
-</script>
 @endsection
