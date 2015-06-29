a33e71029a6dc74a267187bcb5f005fd74cdf80c
Ariel Vallese <alariva@gmail.com>
2015-06-30 00:27:43 +0200

    Remove unused error message displaying

--- resources/views/manager/contacts/show.blade.php
+++ resources/views/manager/contacts/show.blade.php
@@ -37,18 +37,7 @@
 @section('content')
 <div class="container">
 
-    @include('flash::message')
-
-    @if($errors->has())
-        <div class="alert alert-danger">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-            </ul>
-        </div>
-    @endif
+      @include('flash::message')
 
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
