48ff981d59119ffcae91ece6e817ece09e774ec4
Ariel Vallese <alariva@gmail.com>
2015-07-01 15:42:28 +0200

    Add table heading

--- resources/views/user/appointments/index.blade.php
+++ resources/views/user/appointments/index.blade.php
@@ -10,6 +10,18 @@
 
 				<div class="panel-body">
           <table class="table table-condensed">
+            <thead>
+                <tr>
+                    <th><span class="hidden-md">{!! Icon::barcode() !!}</span>   <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.code') }}</span></th>
+                    <th><span class="hidden-md">{!! Icon::asterisk() !!}</span>  <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.status') }}</span></th>
+                    <th><span class="hidden-md">{!! Icon::calendar() !!}</span>  <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.calendar') }}</span></th>
+                    <th><span class="hidden-md">{!! Icon::time() !!}</span>      <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.start_time') }}</span></th>
+                    <th><span class="hidden-md">{!! Icon::time() !!}</span>      <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.finish_time') }}</span></th>
+                    <th><span class="hidden-md">{!! Icon::hourglass() !!}</span> <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.duration') }}</span></th>
+                    <th><span class="hidden-md">{!! Icon::map_marker() !!}</span><span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.business') }}</span></th>
+                </tr>
+            </thead>
+            <tbody>
             @foreach ($appointments as $appointment)
               <tr>
                 <td><code>{{ $appointment->code }}</code></td>
@@ -21,6 +33,7 @@
                 <td>{{ $appointment->business->name }}</td>
               </tr>
             @endforeach
+            </tbody>
           </table>
 
         </div>
