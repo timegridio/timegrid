daa93aa62cdc9f5cfd9205a49e698d168202999f
Ariel Vallese <alariva@gmail.com>
2015-07-02 04:47:37 +0200

    Enhance Business show user page

--- resources/views/user/businesses/show.blade.php
+++ resources/views/user/businesses/show.blade.php
@@ -8,18 +8,18 @@
 
 				<div class="panel-heading">
 					@if(\Auth::user()->isOwner($business))
-						{!! Icon::star() !!} {!! link_to(route('manager.business.show', $business->id), $business->name) !!}
+						{!! Icon::star() !!} {!! link_to(route('manager.business.show', $business), $business->name) !!}
 					@else
 						{{ $business->name }}
 					@endif
 				</div>
 
 				<div class="panel-body">
-					{!! Button::normal(trans('user.appointments.btn.book'))->asLinkTo( route('user.booking.book') ) !!}
+					{{ $business->description }}
 				</div>
 
 				<div class="panel-footer">
-
+					{!! Button::normal(trans('user.appointments.btn.book'))->asLinkTo( route('user.booking.book') ) !!}
 				</div>
 
 			</div>
