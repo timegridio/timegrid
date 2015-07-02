a87ae5cd95902f4d93a8fa2f3427e179f2ed3b42
Ariel Vallese <alariva@gmail.com>
2015-07-02 21:56:57 +0200

    Make email field nullable

--- database/migrations/2015_06_22_152354_create_contacts_table.php
+++ database/migrations/2015_06_22_152354_create_contacts_table.php
@@ -27,7 +27,7 @@ class CreateContactsTable extends Migration {
 			$table->date('birthdate')->nullable();
 			$table->char('mobile', 15)->nullable();
 			$table->char('mobile_country', 2)->nullable();
-			$table->string('email');
+			$table->string('email')->nullable();
 			$table->string('notes')->nullable();
 			$table->timestamps();
 			$table->softDeletes();
