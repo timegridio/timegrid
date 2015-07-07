bba974513848854ba954ab230b854d192271447f
Ariel Vallese <alariva@gmail.com>
2015-07-07 20:51:29 +0200

    Fix Localization handling

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -10,16 +10,20 @@ use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Session;
+use Carbon\Carbon;
 
-class Language implements Middleware {
-
+class Language implements Middleware
+{
     public function handle($request, Closure $next)
     {
-        if (Session::has('applocale') AND array_key_exists(Session::get('applocale'), Config::get('languages'))) {
+        if (Session::has('applocale') and array_key_exists(Session::get('applocale'), Config::get('languages'))) {
             App::setLocale(Session::get('applocale'));
-        }
-        else {
+            setlocale(LC_TIME, Session::get('applocale'));
+            Carbon::setLocale(\Locale::getPrimaryLanguage(Session::get('applocale')));
+        } else {
             App::setLocale(Config::get('app.fallback_locale'));
+            setlocale(LC_TIME, Config::get('app.fallback_locale'));
+            Carbon::setLocale(\Locale::getPrimaryLanguage(Config::get('app.fallback_locale')));
         }
         return $next($request);
     }
--- config/languages.php
+++ config/languages.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'en' => 'English',
-    'es_AR' => 'Español (AR)',
-    'es_ES' => 'Español (ES)',
+    'en_US.utf8' => 'English',
+    'es_AR.utf8' => 'Español',
 ];
