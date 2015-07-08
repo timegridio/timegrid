3afdaaee42645e7079ec9f16e3ed435d5112b009
Ariel Vallese <alariva@gmail.com>
2015-07-08 03:15:50 +0200

    Enhance view for first time Business registration (invitation)

--- resources/views/manager/businesses/index.blade.php
+++ resources/views/manager/businesses/index.blade.php
@@ -10,26 +10,30 @@
 				<div class="panel-heading">{{ trans('manager.businesses.index.title') }}</div>
 
 				<div class="panel-body">
-					{!! Alert::info(trans('manager.businesses.index.help')) !!}
+					@if(\Auth::user()->hasBusiness())
+						{!! Alert::info(trans('manager.businesses.index.help')) !!}
+	
+						<table class="table table-condensed">
+						@foreach ($businesses as $business)
+							<tr>
+								<td>{!! Button::primary($business->slug)->asLinkTo( route('manager.business.show', ['business' => $business]) ) !!}</td>
+								<td>{{ $business->name }}</td>
+								<td>{{ $business->description }}</td>
+							</tr>
+						@endforeach
+						</table>
 
-					<table class="table table-condensed">
-					@foreach ($businesses as $business)
-						<tr>
-							<td>{!! Button::primary($business->slug)->asLinkTo( route('manager.business.show', ['business' => $business]) ) !!}</td>
-							<td>{{ $business->name }}</td>
-							<td>{{ $business->description }}</td>
-						</tr>
-					@endforeach
-					</table>
+						<div class="panel-footer">
+							{!! DropdownButton::normal(trans('app.home.btn.actions'))->withContents([
+										['url' => route('manager.business.index'),  'label' => trans('manager.businesses.index.btn.manage')],
+										['url' => route('manager.business.create'), 'label' => trans('manager.businesses.index.btn.register')],
+							]) !!}
+						</div>
+					@else
+						{!! Alert::info(trans('manager.businesses.index.register_business_help')) !!}
+						<div class="text-center">{!! Button::success(trans('user.businesses.index.btn.power_create'))->large()->asLinkTo( route('manager.business.create') ) !!}</div>
+					@endif
 				</div>
-
-				<div class="panel-footer">
-					{!! DropdownButton::normal(trans('app.home.btn.actions'))->withContents([
-								['url' => route('manager.business.index'),  'label' => trans('manager.businesses.index.btn.manage')],
-								['url' => route('manager.business.create'), 'label' => trans('manager.businesses.index.btn.register')],
-					]) !!}
-				</div>
-
 			</div>
 		</div>
 	</div>
