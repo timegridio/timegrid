a5fc737dd0127ae0fbed8b76bb144d9ec05c4044
Ariel Vallese <alariva@gmail.com>
2015-07-15 01:02:43 +0200

    Reindent

--- resources/views/manager/businesses/services/_form.blade.php
+++ resources/views/manager/businesses/services/_form.blade.php
@@ -1,22 +1,22 @@
 <div class="container">
-	<div class="row">
-		<div class="form-group col-xs-6">
-			{!! Form::text('name', null, 
-				array('required', 
-					  'class'=>'form-control', 
-					  'placeholder'=> trans('manager.service.form.name.label') )) !!}
-		</div>
-	</div>
-	<div class="row">
-		<div class="form-group col-xs-11">
-			{!! Form::textarea('description', null, 
-				array('class'=>'form-control', 
-					  'placeholder'=> trans('manager.contacts.form.description.label') )) !!}
-		</div>
-	</div>
-	<div class="row">
-		<div class="notes form-group col-xs-11">
-			{!! Button::primary($submitLabel)->submit() !!}
-		</div>
-	</div>
+    <div class="row">
+        <div class="form-group col-xs-6">
+            {!! Form::text('name', null,
+                array('required',
+                      'class'=>'form-control',
+                      'placeholder'=> trans('manager.service.form.name.label') )) !!}
+        </div>
+    </div>
+    <div class="row">
+        <div class="form-group col-xs-11">
+            {!! Form::textarea('description', null,
+                array('class'=>'form-control',
+                      'placeholder'=> trans('manager.contacts.form.description.label') )) !!}
+        </div>
+    </div>
+    <div class="row">
+        <div class="notes form-group col-xs-11">
+            {!! Button::primary($submitLabel)->submit() !!}
+        </div>
+    </div>
 </div>
