3c56640b42387b228b905de9e91cfeb9a99ffa7e
Ariel Vallese <alariva@gmail.com>
2015-07-15 00:41:50 +0200

    Reindent PSR-2

--- resources/views/manager/contacts/create.blade.php
+++ resources/views/manager/contacts/create.blade.php
@@ -2,22 +2,20 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{ trans('manager.contacts.create.title') }}</div>
+    <div class="row">
+        <div class="panel panel-default">
+        <div class="panel-heading">{{ trans('manager.contacts.create.title') }}</div>
 
-				<div class="panel-body">
-					@include('_errors')
+        <div class="panel-body">
+            @include('_errors')
 
-					{!! Form::model(new App\Contact, ['route' => ['manager.business.contact.store', $business]]) !!}
-						@include('manager.contacts._form',['submitLabel' => trans('manager.contacts.btn.store')])
-					{!! Form::close() !!}
-				</div>
+            {!! Form::model(new App\Contact, ['route' => ['manager.business.contact.store', $business]]) !!}
+                @include('manager.contacts._form',['submitLabel' => trans('manager.contacts.btn.store')])
+            {!! Form::close() !!}
+        </div>
 
-				<div class="panel-footer">
-					
-				</div>
-			</div>
-	</div>
+        <div class="panel-footer"></div>
+        </div>
+    </div>
 </div>
 @endsection
