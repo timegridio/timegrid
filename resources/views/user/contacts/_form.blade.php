115ea18e99f08381598eecdf3b978c76540b9bae
Ariel Vallese <alariva@gmail.com>
2015-07-17 20:00:12 +0200

    Fix entire layout

--- resources/views/user/contacts/_form.blade.php
+++ resources/views/user/contacts/_form.blade.php
@@ -1,42 +1,17 @@
 @section('css')
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" href="{{ asset('css/jquery-ui.min.css') }}">
 <link rel="stylesheet" href="{{ asset('css/bootstrap-select.min.css') }}">
-<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}">
 @endsection
 
-<div class="container">
 	<div class="row">
 		<div class="form-group col-xs-4">
-			{!! Form::text('nin', null, 
-				array('class'=>'form-control', 
-					  'placeholder'=> trans('manager.contacts.form.nin.label') )) !!}
-		</div>
-		<div class="form-group col-xs-7">
-			{!! Form::email('email', null, 
-				array('class'=>'form-control', 
-					  'placeholder'=> trans('manager.contacts.form.email.label') )) !!}
-		</div>
-	</div>
-	<div class="row">
-		<div class="form-group col-xs-5">
-			{!! Form::select('gender', ['M' => trans('manager.contacts.form.gender.male.label'), 'F' => trans('manager.contacts.form.gender.female.label')], 'M', ['class'=>'selectpicker'] ) !!}
-		</div>
-		<div class="form-group col-xs-6">
-			{!! Form::date('birthdate', isset($contact) ? old('birthdate', $contact->birthdate ? $contact->birthdate->toDateString() : null) : null, 
-				array('class'=>'form-control',
-					  'id'=>'birthdate',
-					  'placeholder'=> trans('manager.contacts.form.birthdate.label'),
-					  'title'=> trans('manager.contacts.form.birthdate.label') )) !!}
-		</div>
-	</div>
-	<div class="row">
-		<div class="form-group col-xs-6">
 			{!! Form::text('firstname', null, 
 				array('required', 
 					  'class'=>'form-control', 
 					  'placeholder'=> trans('manager.contacts.form.firstname.label') )) !!}
 		</div>
-		<div class="form-group col-xs-5">
+		<div class="form-group col-xs-8">
 			{!! Form::text('lastname', null, 
 				array('required', 
 					  'class'=>'form-control', 
@@ -45,20 +20,50 @@
 	</div>
 	<div class="row">
 		<div class="form-group col-xs-4">
+			{!! Form::text('nin', null, 
+				array('class'=>'form-control', 
+					  'placeholder'=> trans('manager.contacts.form.nin.label') )) !!}
+		</div>
+		<div class="form-group col-xs-8">
+			{!! Form::email('email', null, 
+				array('class'=>'form-control', 
+					  'placeholder'=> trans('manager.contacts.form.email.label') )) !!}
+		</div>
+	</div>
+	<div class="row">
+		<div class="form-group col-xs-4">
+			{!! Form::select('gender', ['M' => trans('manager.contacts.form.gender.male.label'), 'F' => trans('manager.contacts.form.gender.female.label')], 'M', ['class'=>'selectpicker'] ) !!}
+		</div>
+		<div class="form-group col-xs-8">
+			{!! Form::date('birthdate', isset($contact) ? old('birthdate', $contact->birthdate ? $contact->birthdate->toDateString() : null) : null, 
+				array('class'=>'form-control',
+					  'id'=>'birthdate',
+					  'placeholder'=> trans('manager.contacts.form.birthdate.label'),
+					  'title'=> trans('manager.contacts.form.birthdate.label') )) !!}
+		</div>
+	</div>
+	<div class="row">
+		<div class="form-group col-xs-4">
 			{!! Form::select('mobile_country', Location::lists(), isset($contact) ? old('mobile_country', $contact->mobile_country ) : Location::get()->countryCode, ['class'=>'selectpicker'] ) !!}
 		</div>
-		<div class="form-group col-xs-7">
+		<div class="form-group col-xs-8">
 			{!! Form::text('mobile', null, 
 				array('class'=>'form-control', 
 					  'placeholder'=> trans('manager.contacts.form.mobile.label') )) !!}
 		</div>
 	</div>
+{{-- 	<div class="row">
+		<div class="form-group col-xs-10">
+			{!! Form::textarea('notes', null, 
+				array('class'=>'form-control', 
+					  'placeholder'=> trans('manager.contacts.form.notes.label') )) !!}
+		</div>
+	</div> --}}
 	<div class="row">
-		<div class="notes form-group col-xs-11">
-			{!! Button::primary($submitLabel)->submit() !!}
+		<div class="notes form-group col-xs-12">
+			{!! Button::primary($submitLabel)->block()->submit() !!}
 		</div>
 	</div>
-</div>
 
 @section('footer_scripts')
 	<script src="{{ asset('js/bootstrap-select.min.js') }}"></script>
