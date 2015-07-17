99ad8de6ff98cef577e0fa6c011cd635cc2a8e75
Ariel Vallese <alariva@gmail.com>
2015-07-18 00:56:51 +0200

    Add manage business nav menu item

--- resources/views/user/_navmenu.blade.php
+++ resources/views/user/_navmenu.blade.php
@@ -6,5 +6,7 @@
 		<li><a href="{{ route('user.businesses.suscriptions') }}">{{ trans('app.nav.user.business.my_suscriptions') }}</a></li>
 		<li class="nav-divider"></li>
 		<li><a href="{{ route('user.booking.list') }}">{{ trans('app.nav.user.business.my_appointments') }}</a></li>
+        <li class="nav-divider"></li>
+        <li><a href="{{ route('manager.business.index') }}">{{ trans('app.nav.manager.business.register') }}</a></li>
 	</ul>
 </li>
