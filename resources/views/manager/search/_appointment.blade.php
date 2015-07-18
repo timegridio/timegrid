151312d66f53045a0849bca30b69938dbf3f52a6
Ariel Vallese <alariva@gmail.com>
2015-07-18 19:10:33 +0200

    Enhance search result views

--- resources/views/manager/search/_appointment.blade.php
+++ resources/views/manager/search/_appointment.blade.php
@@ -1,2 +1,4 @@
-<li class="list-group-item">{!! $appointment->widget()->code() !!} {!! $appointment->widget()->dateLabel() !!}</li>
+<li class="list-group-item">
+{!! Button::normal($appointment->widget()->code(4))->withIcon(Icon::calendar())->asLinkTo(route('manager.business.agenda.index', $appointment->business->id)) !!}
+{!! $appointment->widget()->dateLabel() !!}</li>
 
