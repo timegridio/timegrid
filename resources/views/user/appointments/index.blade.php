ed81a407cf7d14498b57d52b2e1b8d24fa51325b
Ariel Vallese <alariva@gmail.com>
2015-07-18 04:42:02 +0200

    Enhance user Appointment list view

--- resources/views/user/appointments/index.blade.php
+++ resources/views/user/appointments/index.blade.php
@@ -9,27 +9,18 @@
                 <thead>
                 <tr>
                     <th><span class="hidden-md">{!! Icon::barcode() !!}</span>    <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.code') }}</span></th>
+                    <th><span class="hidden-md">{!! Icon::user() !!}</span>    <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.contact') }}</span></th>
                     <th><span class="hidden-md">{!! Icon::asterisk() !!}</span>   <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.status') }}</span></th>
                     <th><span class="hidden-md">{!! Icon::calendar() !!}</span>   <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.calendar') }}</span></th>
                     <th><span class="hidden-md">{!! Icon::time() !!}</span>       <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.start_time') }}</span></th>
-                    <th><span class="hidden-md">{!! Icon::time() !!}</span>       <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.finish_time') }}</span></th>
-                    <th><span class="hidden-md">{!! Icon::hourglass() !!}</span>  <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.duration') }}</span></th>
                     <th><span class="hidden-md">{!! Icon::briefcase() !!}</span><span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.service') }}</span></th>
                     <th><span class="hidden-md">{!! Icon::map_marker() !!}</span> <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.business') }}</span></th>
+                    <th></th>
                 </tr>
                 </thead>
                 <tbody>
                     @foreach ($appointments as $appointment)
-                    <tr>
-                        <td>{!! Button::normal($appointment->widget()->code())->asLinkTo( route('user.booking.show', [$appointment->business, $appointment]) ) !!}</td>
-                        <td>{!! $appointment->widget()->statusLabel() !!}</td>
-                        <td>{!! $appointment->widget()->dateLabel() !!}</td>
-                        <td title="{{ $appointment->tz }}">{{ $appointment->start_at->timezone($appointment->tz)->toTimeString() }}</td>
-                        <td title="{{ $appointment->tz }}">{{ $appointment->finish_at->timezone($appointment->tz)->toTimeString() }}</td>
-                        <td>{{ $appointment->duration }}</td>
-                        <td>{{ $appointment->service ? $appointment->service->name : '' }}</td>
-                        <td>{{ $appointment->business->name }}</td>
-                    </tr>
+                        {!! $appointment->widget()->only(['code', 'contact', 'status', 'date', 'start_at', 'service', 'business', 'diffForHumans'])->row() !!}
                     @endforeach
                 </tbody>
             </table>
