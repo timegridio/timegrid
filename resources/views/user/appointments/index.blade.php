b87dc76e1b05e095e9e319aa29c3806864c4d7ec
Ariel Vallese <alariva@gmail.com>
2015-07-18 02:19:11 +0200

    Simplify layout

--- resources/views/user/appointments/index.blade.php
+++ resources/views/user/appointments/index.blade.php
@@ -2,15 +2,11 @@
 
 @section('content')
 <div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				
+    <div class="panel panel-default">
         <div class="panel-heading">{{ trans('user.appointments.index.title') }}</div>
-
-				<div class="panel-body">
-          <table class="table table-condensed table-hover">
-            <thead>
+        <div class="panel-body">
+            <table class="table table-striped table-hover">
+                <thead>
                 <tr>
                     <th><span class="hidden-md">{!! Icon::barcode() !!}</span>    <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.code') }}</span></th>
                     <th><span class="hidden-md">{!! Icon::asterisk() !!}</span>   <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.status') }}</span></th>
@@ -21,30 +17,24 @@
                     <th><span class="hidden-md">{!! Icon::briefcase() !!}</span><span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.service') }}</span></th>
                     <th><span class="hidden-md">{!! Icon::map_marker() !!}</span> <span class="hidden-xs hidden-sm">{{ trans('user.appointments.index.th.business') }}</span></th>
                 </tr>
-            </thead>
-            <tbody>
-            @foreach ($appointments as $appointment)
-              <tr>
-                <td>{!! $appointment->widget()->code() !!}</td>
-                <td>{!! $appointment->widget()->statusLabel() !!}</td>
-                <td>{!! $appointment->widget()->dateLabel() !!}</td>
-                <td title="{{ $appointment->tz }}">{{ $appointment->start_at->timezone($appointment->tz)->toTimeString() }}</td>
-                <td title="{{ $appointment->tz }}">{{ $appointment->finish_at->timezone($appointment->tz)->toTimeString() }}</td>
-                <td>{{ $appointment->duration }}</td>
-                <td>{{ $appointment->service ? $appointment->service->name : '' }}</td>
-                <td>{{ $appointment->business->name }}</td>
-              </tr>
-            @endforeach
-            </tbody>
-          </table>
-
+                </thead>
+                <tbody>
+                    @foreach ($appointments as $appointment)
+                    <tr>
+                        <td>{!! Button::normal($appointment->widget()->code())->asLinkTo( route('user.booking.show', [$appointment->business, $appointment]) ) !!}</td>
+                        <td>{!! $appointment->widget()->statusLabel() !!}</td>
+                        <td>{!! $appointment->widget()->dateLabel() !!}</td>
+                        <td title="{{ $appointment->tz }}">{{ $appointment->start_at->timezone($appointment->tz)->toTimeString() }}</td>
+                        <td title="{{ $appointment->tz }}">{{ $appointment->finish_at->timezone($appointment->tz)->toTimeString() }}</td>
+                        <td>{{ $appointment->duration }}</td>
+                        <td>{{ $appointment->service ? $appointment->service->name : '' }}</td>
+                        <td>{{ $appointment->business->name }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-
-				<div class="panel-footer">
-
-				</div>
-			</div>
-		</div>
-	</div>
+        <div class="panel-footer"></div>
+    </div>
 </div>
 @endsection
