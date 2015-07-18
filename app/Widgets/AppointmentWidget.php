3b66b24862ce232bf089cf16a0fad62545e74977
Ariel Vallese <alariva@gmail.com>
2015-07-18 03:25:08 +0200

    Provide better field selection support

--- app/Widgets/AppointmentWidget.php
+++ app/Widgets/AppointmentWidget.php
@@ -12,9 +12,25 @@ class AppointmentWidget
 {
     protected $appointment = null;
 
+    protected $fields = [];
+
     public function __construct(Appointment $appointment)
     {
         $this->appointment = $appointment;
+
+        $this->fields = ['code', 'contact', 'status', 'date', 'start_at', 'service', 'action', 'diffForHumans'];
+    }
+
+    public function only(Array $options)
+    {
+        $this->fields = array_intersect($options, $this->fields);
+        return $this;
+    }
+
+    public function except(Array $options)
+    {
+        $this->fields = array_diff($this->fields, $options);
+        return $this;
     }
 
     public function statusLabel()
@@ -89,7 +105,7 @@ class AppointmentWidget
         return $this->appointment->start_at->timezone($this->appointment->business->timezone)->diffForHumans();
     }
 
-    public function fullrow()
+    public function row()
     {
         $class = $this->appointment->status == Appointment::STATUS_RESERVED  ? 'bg-warning' : '';
         $class = $this->appointment->status == Appointment::STATUS_CONFIRMED ? 'bg-success' : $class;
@@ -97,23 +113,66 @@ class AppointmentWidget
         $highlight = $this->appointment->start_at->isToday() ? 'today' : '';
 
         $out  = "<tr id=\"{$this->appointment->code}\" class=\"{$class}\">";
-        $out .= '<td>'. $this->code(4) .'</td>';
-        $out .= '<td>'. $this->contactName() .'</td>';
-        $out .= '<td>'. $this->statusLabel() .'</td>';
-        $out .= '<td>'. $this->dateLabel() .'</td>';
-        $out .= "<td title=\"{$this->appointment->tz}\">".$this->appointment->start_at->timezone($this->appointment->tz)->toTimeString().'</td>';
-        $out .= '<td>'. ($this->appointment->service ? $this->appointment->service->name : '') .'</td>';
-        $out .= '<td>'. $this->actionButtons() .'</td>';
-        $out .= "<td class=\"{$highlight}\">". $this->diffForHumans() .'</td>';
+        
+        $cols = [];
+        $cols['code']          = '<td>'. $this->code(4) .'</td>';
+        $cols['contact']       = '<td>'. $this->contactName() .'</td>';
+        $cols['status']        = '<td>'. $this->statusLabel() .'</td>';
+        $cols['date']          = '<td>'. $this->dateLabel() .'</td>';
+        $cols['start_at']      = "<td title=\"{$this->appointment->tz}\">".$this->appointment->start_at->timezone($this->appointment->tz)->toTimeString().'</td>';
+        $cols['service']       = '<td>'. ($this->appointment->service ? $this->appointment->service->name : '') .'</td>';
+        $cols['action']        = '<td>'. $this->actionButtons() .'</td>';
+        $cols['diffForHumans'] = "<td class=\"{$highlight}\">". $this->diffForHumans() .'</td>';
+
+        foreach ($this->fields as $field) {
+            $out .= $cols[$field];
+        }
+
         $out .= '</tr>';
+        return $out;
+    }
+
+    public function table()
+    {
+        $out  = '<table class="table table-condensed table-hover">';
+        $out .= $this->thead();
+        $out .= '<tbody class="searchable">';
+        foreach ($this->appointments as $appointment){
+            $out .= $this->row();
+        }
+        $out .= '</tbody></table>';
+        return $out;
+    }
+
+    public function thead()
+    {
+        $cols = [];
+        $cols['code']          = '<th><span class="hidden-md">'. Icon::barcode() .'</span> <span class="hidden-xs hidden-sm">'. trans('user.appointments.index.th.code') . '</span></th>';
+        $cols['contact']       = '<th><span class="hidden-md">'. Icon::user() .'</span> <span class="">'. trans('user.appointments.index.th.contact') . '</span></th>';
+        $cols['status']        = '<th><span class="hidden-md">'. Icon::asterisk() .'</span> <span class="hidden-xs hidden-sm">'. trans('user.appointments.index.th.status') . '</span></th>';
+        $cols['date']          = '<th><span class="hidden-md">'. Icon::calendar() .'</span> <span class="hidden-xs hidden-sm">'. trans('user.appointments.index.th.calendar') . '</span></th>';
+        $cols['start_at']      = '<th><span class="hidden-md">'. Icon::time() .'</span> <span class="hidden-xs hidden-sm">'. trans('user.appointments.index.th.start_time') . '</span></th>';
+        $cols['service']       = '<th><span class="hidden-md">'. Icon::briefcase() .'</span> <span class="hidden-xs hidden-sm">'. trans('user.appointments.index.th.service') . '</span></th>';
+        $cols['action']        = '<th></th>';
+        $cols['diffForHumans'] = '<th></th>';
+        
+        $out = '<thead><tr>';
+        foreach ($this->fields as $field) {
+            $out .= $cols[$field];
+        }
+        $out .= '</tr></thead>';
         return $out;
     }
 
     public function panel()
     {
+        $header = $this->statusLabel();
+        $footer = Icon::barcode() . '&nbsp;' . $this->code();
+
         switch ($this->appointment->status) {
             case Appointment::STATUS_ANNULATED:
                 $panel = Panel::danger();
+                $header .= '&nbsp;&nbsp;' . trans('appointments.alert.annulated');
                 break;
             case Appointment::STATUS_CONFIRMED:
                 $panel = Panel::success();
@@ -126,9 +185,6 @@ class AppointmentWidget
                 $panel = Panel::normal();
                 break;
         }
-
-        $header = $this->statusLabel();
-        $footer = Icon::barcode() . '&nbsp;' . $this->code();
 
         $body  = '<ul class="list-group">';
         $body .= '<li class="list-group-item">';
