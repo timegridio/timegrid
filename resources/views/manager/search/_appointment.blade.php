e5b87d73f20f4debc42de7f4a88140e9030982a7
Ariel Vallese <alariva@gmail.com>
2015-07-19 11:50:13 +0200

    Enhance Appointment search results view by adding links

--- resources/views/manager/search/_appointment.blade.php
+++ resources/views/manager/search/_appointment.blade.php
@@ -1,4 +1,6 @@
 <li class="list-group-item">
 {!! Button::normal($appointment->widget()->code(4))->withIcon(Icon::calendar())->asLinkTo(route('manager.business.agenda.index', $appointment->business->id)) !!}
-{!! $appointment->widget()->dateLabel() !!}
+{!! Button::normal($appointment->widget()->dateLabel())->withIcon(Icon::calendar())->asLinkTo(route('manager.business.agenda.index', $appointment->business->id)) !!}
+{!! Button::normal($appointment->contact->fullname)->withIcon(Icon::user())->asLinkTo(route('manager.business.contact.show', [$appointment->business->id, $appointment->contact->id])) !!}
+{!! Button::normal($appointment->service->name)->withIcon(Icon::tag())->asLinkTo(route('manager.business.service.show', [$appointment->business->id, $appointment->service->id])) !!}
 </li>
