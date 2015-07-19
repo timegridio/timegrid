1ee797b09fe7400536511319f6fdaeb6c35ed865
Ariel Vallese <alariva@gmail.com>
2015-07-19 13:43:32 +0200

    Use Lorempixel

--- resources/views/wizard.blade.php
+++ resources/views/wizard.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-md-5">
                         
                         {!! Panel::normal()->withHeader('<strong>Prestadores</strong> Quiero dar turnos por internet')->withBody(
-                            Thumbnail::image('//placehold.it/350x150')->caption('Si <strong>tenés un negocio o sos prestador</strong>, acá podés dar de alta tu comercio y empezar a dar turnos por internet.').
+                            Thumbnail::image('//lorempixel.com/350/150/business/7')->caption('Si <strong>tenés un negocio o sos prestador</strong>, acá podés dar de alta tu comercio y empezar a dar turnos por internet.').
                             Button::success('Soy prestador!')->large()->block()->asLinkTo(route('manager.business.index'))
                         ) !!}
 
@@ -18,7 +18,7 @@
                 <div class="col-md-5">
     
                     {!! Panel::normal()->withHeader('<strong>Clientes</strong> Quiero pedir turnos por internet')->withBody(
-                        Thumbnail::image('//placehold.it/350x150')->caption('Si <strong>querés encontrar un prestador</strong> para reservar sus servicios, acá podés encontrar el que más te gusta y reservar tu turno!.').
+                        Thumbnail::image('//lorempixel.com/350/150/people/1')->caption('Si <strong>querés encontrar un prestador</strong> para reservar sus servicios, acá podés encontrar el que más te gusta y reservar tu turno!.').
                         Button::primary('Soy cliente!')->large()->block()->asLinkTo(route('user.businesses.list'))
                         ) !!}
 
