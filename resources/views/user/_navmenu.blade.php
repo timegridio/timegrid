f2a3a81b2192a4b6e6b78fc6cb8ed0b8b5bdb3a0
Ariel Vallese <alariva@gmail.com>
2015-07-19 17:12:09 +0200

    Change menu label to wizard

--- resources/views/user/_navmenu.blade.php
+++ resources/views/user/_navmenu.blade.php
@@ -7,6 +7,6 @@
         <li class="nav-divider"></li>
         <li><a href="{{ route('user.booking.list') }}">{{ trans('app.nav.user.business.my_appointments') }}</a></li>
         <li class="nav-divider"></li>
-        <li><a href="{{ route('wizard.welcome') }}">{{ trans('app.nav.manager.business.register') }}</a></li>
+        <li><a href="{{ route('wizard.welcome') }}">{{ trans('app.nav.wizard') }}</a></li>
     </ul>
 </li>
