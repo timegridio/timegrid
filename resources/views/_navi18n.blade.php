c938fa2dcb5021bf48421f9e89e5453da22ce538
Ariel Vallese <alariva@gmail.com>
2015-07-19 10:51:54 +0200

    PSR-2 reindent

--- resources/views/_navi18n.blade.php
+++ resources/views/_navi18n.blade.php
@@ -1,5 +1,5 @@
 {{-- Language Switcher --}}
-<li class="dropdown">					
+<li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Config::get('languages')[App::getLocale()] }} <b class="caret"></b></a>
     <ul class="dropdown-menu">
         @foreach (Config::get('languages') as $lang => $language)
@@ -11,4 +11,4 @@
         @endforeach
     </ul>
 </li>
-{{-- Language Switcher --}}	
+{{-- Language Switcher --}} 
