c3309a19ba6647bffa2295d6c64aaf63b31d70cc
Ariel Vallese <alariva@gmail.com>
2015-07-20 19:12:31 +0200

    Load wording from trans()

--- resources/views/wizard.blade.php
+++ resources/views/wizard.blade.php
@@ -3,23 +3,23 @@
 @section('content')
 <div class="container">
     <div class="row">
-        {!! Alert::info('Qué necesitás hacer?') !!}
+        {!! Alert::info(trans('wizard.alert')) !!}
     </div>
     <div class="row">
             <div class="col-md-offset-2">
                 <div class="col-md-5">
                         
-                        {!! Panel::normal()->withHeader('<strong>Prestadores</strong> Quiero dar turnos por internet')->withBody(
-                            Thumbnail::image('//lorempixel.com/350/150/business/7')->caption('Si <strong>tenés un negocio o sos prestador</strong>, acá podés dar de alta tu comercio y empezar a dar turnos por internet.').
-                            Button::success('Soy prestador!')->large()->block()->asLinkTo(route('wizard.pricing'))
+                        {!! Panel::normal()->withHeader(trans('wizard.business.header'))->withBody(
+                            Thumbnail::image('//lorempixel.com/350/150/business/7')->caption(trans('wizard.business.caption')).
+                            Button::success(trans('wizard.business.btn'))->large()->block()->asLinkTo(route('wizard.pricing'))
                         ) !!}
 
                 </div>
                 <div class="col-md-5">
     
-                    {!! Panel::normal()->withHeader('<strong>Clientes</strong> Quiero pedir turnos por internet')->withBody(
-                        Thumbnail::image('//lorempixel.com/350/150/people/1')->caption('Si <strong>querés encontrar un prestador</strong> para reservar sus servicios, acá podés encontrar el que más te gusta y reservar tu turno!.').
-                        Button::primary('Soy cliente!')->large()->block()->asLinkTo(route('user.businesses.list'))
+                    {!! Panel::normal()->withHeader(trans('wizard.user.header'))->withBody(
+                        Thumbnail::image('//lorempixel.com/350/150/people/1')->caption(trans('wizard.user.caption')).
+                        Button::primary(trans('wizard.user.btn'))->large()->block()->asLinkTo(route('user.businesses.list'))
                         ) !!}
 
                 </div>
