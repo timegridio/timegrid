95aeb435bd270a86e932eb5be2ebd9e2dce17ff5
Ariel Vallese <alariva@gmail.com>
2015-07-20 22:16:06 +0200

    Make selected business link only visible if set

--- resources/views/user/_navmenu.blade.php
+++ resources/views/user/_navmenu.blade.php
@@ -1,7 +1,10 @@
 <li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ trans('app.nav.user.business.menu') }} <b class="caret"></b></a>
     <ul class="dropdown-menu">
-        <li><a href="{{ route('user.businesses.home') }}">{{ trans('app.nav.user.business.home') }}</a></li>
+    @if($business = Session::get('selected.business'))
+        <li><a href="{{ route('user.businesses.home') }}">{!! Icon::map_marker() !!}&nbsp;{{ $business->name }}</a></li>
+        <li class="nav-divider"></li>
+    @endif
         <li><a href="{{ route('user.businesses.list') }}">{{ trans('app.nav.user.business.selector') }}</a></li>
         <li><a href="{{ route('user.businesses.suscriptions') }}">{{ trans('app.nav.user.business.my_suscriptions') }}</a></li>
         <li class="nav-divider"></li>
