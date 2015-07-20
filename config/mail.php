5a98ace58644f227914a44d4a86f1ddb1019197e
Ariel Vallese <alariva@gmail.com>
2015-07-21 01:37:35 +0200

    Update sender info

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
 	|
 	*/
 
-	'from' => ['address' => 'support@xbooking.com.ar', 'name' => 'XBooking'],
+	'from' => ['address' => 'support@timegrid.io', 'name' => 'timegrid'],
 
 	/*
 	|--------------------------------------------------------------------------
