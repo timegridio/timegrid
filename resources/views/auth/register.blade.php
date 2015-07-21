19d05581a38100abc7c96cdaf339ee0a2f6cb91a
Ariel Vallese <alariva@gmail.com>
2015-07-21 14:26:12 +0200

    Remove old validation code and enhance with bootstrap-validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,41 +25,58 @@
                         <p>&nbsp;</p>
                     @endif
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}" id="registration" role="form">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                         <div class="form-group">
                             <label class="col-md-4 control-label">{{ trans('auth.register.name') }}</label>
                             <div class="col-md-6">
-                                <input class="form-control" name="name" value="{{ old('name') }}" id="name">
+                                <div class="input-group">
+                                    <span class="input-group-addon">{!! Icon::user() !!}</span>
+                                    <input class="form-control" name="name" value="{{ old('name') }}" data-minlength="3" required>
+                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                                </div>
+                                <div class="help-block with-errors"></div>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">{{ trans('auth.register.email') }}</label>
+                            <label for="email" class="col-md-4 control-label">{{ trans('auth.register.email') }}</label>
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}" id="email">
+                                <div class="input-group">
+                                    <span class="input-group-addon">{!! Icon::envelope() !!}</span>
+                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}" id="email" required>
+                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                                </div>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="col-md-4 control-label">{{ trans('auth.register.password') }}</label>
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password" id="password">
+                                <div class="input-group">
+                                    <span class="input-group-addon">{!! Icon::lock() !!}</span>
+                                    <input type="password" class="form-control" name="password" id="password" data-minlength="6" placeholder="{{trans('validation.custom.password.min', ['min'=>'6'])}}" required>
+                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                                </div>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <label class="col-md-4 control-label">{{ trans('auth.register.password_confirmation') }}</label>
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password_confirmation" id="confirmation">
+                                <div class="input-group">
+                                    <span class="input-group-addon">{!! Icon::lock() !!}</span>
+                                    <input type="password" class="form-control" name="password_confirmation" data-match="#password" data-match-error="{{trans('validation.custom.password.confirmed')}}" required>
+                                    <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                                </div>
+                                <div class="help-block with-errors"></div>
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary" id="submit">
-                                    {!! Icon::ok()->withAttributes(['id' => 'okicon', 'class' => 'hidden']) !!}
                                     {{ trans('auth.register.btn.submit') }}
                                 </button>
                             </div>
@@ -67,13 +84,6 @@
                     </form>
                 </div>
 
-                <div class="panel-footer">
-                        <div class="progress">
-                          <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;" id="progress">
-                            0%
-                          </div>
-                        </div>
-                </div>
             </div>
         </div>
     </div>
@@ -81,70 +91,20 @@
 @endsection
 
 @section('footer_scripts')
+<script src="{{asset('js/bootstrap-validator.min.js')}}"></script>
 @parent
 <script type="text/javascript">
-function enableButton(button, enable)
-{
-    var icon = $('#okicon');
-    if (enable) {
-        icon.removeClass('hidden');
-        icon.show();
-        button.addClass('btn-success');
-        button.removeClass('btn-primary');
-    }
-    else
-    {
-        icon.hide();
-        button.addClass('btn-primary');
-        button.removeClass('btn-success');
-    }
-}
-
-function validateEmail(email) {
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    return email != '' && emailReg.test( email );
-}
-
-function validatePasswords(password, confirmation)
-{
-    return password.length >= 6 && password == confirmation;
-}
-
-function updateProgress(progress, p)
-{
-    progress.attr('style', 'width:'+p+'%');
-    progress.attr('aria-valuenow', p);
-    progress.text(p+'%');
-    if(p == 100) {
-        progress.addClass('progress-bar-success')
-    } else {
-        progress.removeClass('progress-bar-success') 
-    }
-}
-
-$(document).ready(function(){
-
-        var button = $('#submit');
-        var progress = $('#progress');
-
-        $('input').keyup(function(e){
-            /* Ignore tab key */
-            var code = e.keyCode || e.which;
-            if (code == '9') return;
-
-            var name = $('#name').val();
-            var email = $('#email').val();
-            var password = $('#password').val();
-            var confirmation = $('#confirmation').val();
-            var emailvalidation = validateEmail(email);
-            var pwvalidation = validatePasswords(password, confirmation);
-            
-            p = (0.25*(name.length > 2) + 0.25*(emailvalidation) + 0.25*(password.length >= 6) + 0.25*(pwvalidation))*100;
-            
-            updateProgress(progress, p);
-            enableButton (button, p == 100);
+    $(document).ready(function(){
+        $('#registration').validator({
+            feedback: {
+              success: 'glyphicon-ok',
+              error: 'glyphicon-remove'
+            },
+            errors: {
+              match: '{{trans('validation.custom.password.confirmed')}}',
+              minlength: '{{trans('validation.custom.name.min')}}'
+            }
         });
-        enableButton (button, false);
-});
-</script>>
+    });
+</script>
 @endsection
