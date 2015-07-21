20f46eabbfec95630c26de133a7d4475a4681aaf
Ariel Vallese <alariva@gmail.com>
2015-07-21 13:10:23 +0200

    Add gravatar for user navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -60,7 +60,7 @@
                         <li><a href="{{ url('/auth/register') }}">{{ trans('app.nav.register') }}</a></li>
                     @else
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->email }} <span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><img src="{{ Gravatar::get(Auth::user()->email, ['size' => 24, 'secure' => true]) }}" class="img-circle"> {{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/auth/logout') }}">{{ trans('app.nav.logout') }}</a></li>
                             </ul>
