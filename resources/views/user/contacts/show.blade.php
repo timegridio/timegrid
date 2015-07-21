c0262ac051e8146f6e46da8211cafd1d50e39a1e
Ariel Vallese <alariva@gmail.com>
2015-07-21 18:09:57 +0200

    Apply strike-through to annulated appointments

--- resources/views/user/contacts/show.blade.php
+++ resources/views/user/contacts/show.blade.php
@@ -36,6 +36,8 @@
 
 @section('content')
 <div class="container">
+{!! Form::open(['id' => 'postAppointmentStatus', 'method' => 'post', 'route' => ['manager.business.agenda.action']]) !!}
+
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xs-offset-0 col-sm-offset-0 col-md-offset-2 col-lg-offset-2 toppad" >
 
@@ -115,5 +117,60 @@
 
                 </div>
             </div>
+{!! Form::close() !!}
 </div>
+@endsection
+
+@section('footer_scripts')
+@parent
+<script>
+$(document).ready(function(){
+
+function prepareEvents(){
+
+        console.log('prepareEvents()');
+
+        var form = $('#postAppointmentStatus');
+        var button = $('.action');
+        var buttons = $('.actiongroup');
+        var token = $('input[name=_token]');
+
+        button.click(function (event){
+
+        event.preventDefault();
+
+        var business = $(this).data('business');
+        var appointment = $(this).data('appointment');
+        var action = $(this).data('action');
+        var code = $(this).data('code');
+        var row = $('#'+code);
+
+        $(this).parent().hide();
+
+            $.ajax({
+                url: form.attr('action'),
+                method: 'post',
+                dataType: 'json',
+                headers: {
+                    'X-CSRF-TOKEN': token.val()
+                },
+                data: { business: business, appointment: appointment, action: action, widget:'panel' }
+            }).done(function (data) {
+                    console.log('AJAX Done');
+                    $('#'+code).replaceWith(data.html);
+            }).fail(function (data) {
+                    console.log('AJAX Fail');
+            }).always(function (data) {
+                    $(this).parent().show();
+                    prepareEvents();
+                    console.log('AJAX Finish');
+                    console.log(data);
+            });
+        });
+    }
+
+prepareEvents();
+
+});
+</script>
 @endsection
