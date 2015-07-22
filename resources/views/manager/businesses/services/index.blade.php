006d57ab9c3bc5c9da64306947304c2353b04b4e
Ariel Vallese <alariva@gmail.com>
2015-07-22 17:46:43 +0200

    Remove slug from view. Useless by now.

--- resources/views/manager/businesses/services/index.blade.php
+++ resources/views/manager/businesses/services/index.blade.php
@@ -13,7 +13,6 @@
                     <table class="table table-condensed">
                         <tr>
                             <th>{{ trans('manager.services.index.th.name') }}</th>
-                            <th>{{ trans('manager.services.index.th.slug') }}</th>
                         </tr>
                     @foreach ($services as $service)
                         <tr>
@@ -23,7 +22,6 @@
                                 {!! Button::normal($service->name)->asLinkTo( route('manager.business.service.show', [$business->id, $service->id]) ) !!}
                             </div>
                             </td>
-                            <td>{{ $service->slug }}</td>
                         </tr>
                     @endforeach
                     </table>
