b1491e159095d779efff72520c6e86344f6a8242
Ariel Vallese <alariva@gmail.com>
2015-07-22 18:32:27 +0200

    Add Home
    Home redirects the arriving user to his interest panel as a Business owner or just User

--- app/Http/Controllers/User/WizardController.php
+++ app/Http/Controllers/User/WizardController.php
@@ -5,11 +5,23 @@ namespace App\Http\Controllers\User;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use Session;
+use Auth;
 use Redirect;
 
 class WizardController extends Controller
 {
+    public function getHome()
+    {
+        if (Auth::user()->hasBusiness()) {
+            return Redirect::route('manager.business.index');
+        } else {
+            if (Auth::user()->hasContacts()) {
+                return Redirect::route('user.businesses.list');
+            }
+        }
+        return view('wizard');
+    }
+
     public function getWelcome()
     {
         return view('wizard');
