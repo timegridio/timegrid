b4e52dfd8cb78166d473ba3fa7c7bcc43a3c9dec
Ariel Vallese <alariva@gmail.com>
2015-07-23 15:10:40 +0200

    Update wording

--- resources/lang/es_AR.utf8/emails.php
+++ resources/lang/es_AR.utf8/emails.php
@@ -3,5 +3,8 @@
 return array (
   'welcome' => [
     'subject' => 'Bienvenido a timegrid.io'
-  ]
+  ],
+  'appointment' => [
+    'reserved' => ['subject' => 'Información de tu reserva']
+  ],
 );
