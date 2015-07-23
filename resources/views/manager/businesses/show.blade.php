1dcd18cb55a45d0b87e776cd4eb9cce35f68b617
Ariel Vallese <alariva@gmail.com>
2015-07-23 20:54:09 +0200

    Add ids

--- resources/views/manager/businesses/show.blade.php
+++ resources/views/manager/businesses/show.blade.php
@@ -15,15 +15,15 @@
 @section('content')
 <div class="container">
 
-    <div class="panel panel-default">
+    <div class="panel panel-default" id="dashboard">
         <div class="panel-heading">
           <span class="btn-group"> 
-            {!! Button::withIcon(Icon::trash())->danger()->withAttributes(['data-method' => 'DELETE', 'data-confirm' => trans('app.general.btn.confirm_deletion')])->asLinkTo( route('manager.business.destroy', $business) ) !!}
-            {!! Button::withIcon(Icon::edit())->primary()->asLinkTo( route('manager.business.edit', $business) ) !!}
-            {!! Button::withIcon(Icon::tag())->normal()->asLinkTo( route('manager.business.service.index', $business) ) !!}
-            {!! Button::withIcon(Icon::time())->normal()->asLinkTo( route('manager.business.vacancy.create', $business) ) !!}
-            {!! Button::withIcon(Icon::calendar())->normal()->asLinkTo( route('manager.business.agenda.index', $business) ) !!}
-            {!! Button::withIcon(Icon::user())->normal()->asLinkTo( route('manager.business.contact.index', $business) ) !!}
+            {!! Button::withIcon(Icon::trash())->danger()->withAttributes(['id' => 'btnDelete', 'data-method' => 'DELETE', 'data-confirm' => trans('app.general.btn.confirm_deletion')])->asLinkTo( route('manager.business.destroy', $business) ) !!}
+            {!! Button::withIcon(Icon::edit())->primary()->withAttributes(['id' => 'btnEdit'])->asLinkTo( route('manager.business.edit', $business) ) !!}
+            {!! Button::withIcon(Icon::tag())->normal()->withAttributes(['id' => 'btnServices'])->asLinkTo( route('manager.business.service.index', $business) ) !!}
+            {!! Button::withIcon(Icon::time())->normal()->withAttributes(['id' => 'btnVacancies'])->asLinkTo( route('manager.business.vacancy.create', $business) ) !!}
+            {!! Button::withIcon(Icon::calendar())->withAttributes(['id' => 'btnAgenda'])->normal()->asLinkTo( route('manager.business.agenda.index', $business) ) !!}
+            {!! Button::withIcon(Icon::user())->withAttributes(['id' => 'btnContacts'])->normal()->asLinkTo( route('manager.business.contact.index', $business) ) !!}
           </span>
         </div>
 
@@ -55,42 +55,42 @@
               <div class="col-md-2">
                     <div class="panel panel-default panel-success">
                       <div class="panel-heading">{{ trans('manager.businesses.dashboard.panel.title_appointments_active') }}</div>
-                      <div class="panel-body"><h1 class="text-center">{{ $business->bookings()->ofDate(Carbon::now())->get()->count() }}</h1></div>
+                      <div class="panel-body" id="indicator1"><h1 class="text-center">{{ $business->bookings()->ofDate(Carbon::now())->get()->count() }}</h1></div>
                       <div class="panel-footer">{{ trans('manager.businesses.dashboard.panel.title_appointments_today') }}</div>
                     </div>
               </div>
               <div class="col-md-2">
                     <div class="panel panel-default panel-danger">
                       <div class="panel-heading">{{ trans('manager.businesses.dashboard.panel.title_appointments_annulated') }}</div>
-                      <div class="panel-body"><h1 class="text-center">{{ $business->bookings()->ofDate(Carbon::now())->annulated()->get()->count() }}</h1></div>
+                      <div class="panel-body" id="indicator2"><h1 class="text-center">{{ $business->bookings()->ofDate(Carbon::now())->annulated()->get()->count() }}</h1></div>
                       <div class="panel-footer">{{ trans('manager.businesses.dashboard.panel.title_appointments_today') }}</div>
                     </div>
               </div>
               <div class="col-md-2">
                     <div class="panel panel-default panel-warning">
                       <div class="panel-heading">{{ trans('manager.businesses.dashboard.panel.title_appointments_active') }}</div>
-                      <div class="panel-body"><h1 class="text-center">{{ $business->bookings()->ofDate(Carbon::tomorrow())->active()->get()->count() }}</h1></div>
+                      <div class="panel-body" id="indicator3"><h1 class="text-center">{{ $business->bookings()->ofDate(Carbon::tomorrow())->active()->get()->count() }}</h1></div>
                       <div class="panel-footer">{{ trans('manager.businesses.dashboard.panel.title_appointments_tomorrow') }}</div>
                     </div>
               </div>
               <div class="col-md-2">
                     <div class="panel panel-default panel-success">
                       <div class="panel-heading">{{ trans('manager.businesses.dashboard.panel.title_appointments_active') }}</div>
-                      <div class="panel-body"><h1 class="text-center">{{ $business->bookings()->active()->get()->count() }}</h1></div>
+                      <div class="panel-body" id="indicator4"><h1 class="text-center">{{ $business->bookings()->active()->get()->count() }}</h1></div>
                       <div class="panel-footer">{{ trans('manager.businesses.dashboard.panel.title_appointments_total') }}</div>
                     </div>
               </div>
               <div class="col-md-2">
                     <div class="panel panel-default panel-info">
                       <div class="panel-heading">{{ trans('manager.businesses.dashboard.panel.title_appointments_served') }}</div>
-                      <div class="panel-body"><h1 class="text-center">{{ $business->bookings()->served()->get()->count() }}</h1></div>
+                      <div class="panel-body" id="indicator5"><h1 class="text-center">{{ $business->bookings()->served()->get()->count() }}</h1></div>
                       <div class="panel-footer">{{ trans('manager.businesses.dashboard.panel.title_appointments_total') }}</div>
                     </div>
               </div>
               <div class="col-md-2">
                     <div class="panel panel-default panel-info">
                       <div class="panel-heading">{{ trans('manager.businesses.dashboard.panel.title_appointments_total') }}</div>
-                      <div class="panel-body"><h1 class="text-center">{{ $business->bookings()->get()->count() }}</h1></div>
+                      <div class="panel-body" id="indicator6"><h1 class="text-center">{{ $business->bookings()->get()->count() }}</h1></div>
                       <div class="panel-footer">{{ trans('manager.businesses.dashboard.panel.title_appointments_total') }}</div>
                     </div>
               </div>
@@ -100,19 +100,19 @@
               <div class="col-md-4">
                     <div class="panel panel-default">
                       <div class="panel-heading">{{ trans('manager.businesses.dashboard.panel.title_contacts_registered') }}</div>
-                      <div class="panel-body"><h2 class="text-center">{{ $business->contacts()->count() }}</h2></div>
+                      <div class="panel-body" id="indicator7"><h2 class="text-center">{{ $business->contacts()->count() }}</h2></div>
                     </div>
               </div>
               <div class="col-md-4">
                     <div class="panel panel-default">
                       <div class="panel-heading">{{ trans('manager.businesses.dashboard.panel.title_contacts_active') }}</div>
-                      <div class="panel-body"><h2 class="text-center">{{ $business->contacts()->whereNotNull('user_id')->count() }}</h2></div>
+                      <div class="panel-body" id="indicator8"><h2 class="text-center">{{ $business->contacts()->whereNotNull('user_id')->count() }}</h2></div>
                     </div>
               </div>
               <div class="col-md-4">
                     <div class="panel panel-default">
                       <div class="panel-heading">{{ trans('manager.businesses.dashboard.panel.title_contacts_with_nin') }}</div>
-                      <div class="panel-body"><h2 class="text-center">{{ $business->contacts()->whereNotNull('nin')->count() }}</h2></div>
+                      <div class="panel-body" id="indicator9"><h2 class="text-center">{{ $business->contacts()->whereNotNull('nin')->count() }}</h2></div>
                     </div>
               </div>
             </div>
@@ -124,6 +124,8 @@
 @endsection
 
 @section('footer_scripts')
+@parent
+<script src="{{ asset('js/bootstrap-tour.min.js') }}"></script>
 <script type="text/javascript">
 (function() {
  
@@ -191,7 +193,152 @@
   };
  
   laravel.initialize();
- 
+
+ // Instance the tour
+var tourDashboard = new Tour({
+  duration: 4500,
+  delay: 150,
+  steps: [
+  {
+    element: "#general",
+    title: "{{trans('tour.dashboard.panel.title')}}",
+    content: "{{trans('tour.dashboard.panel.content')}}",
+    orphan: true,
+    duration: 12000
+  },
+  {
+    element: "#btnEdit",
+    title: "{{trans('tour.dashboard.edit.title')}}",
+    content: "{{trans('tour.dashboard.edit.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#btnServices",
+    title: "{{trans('tour.dashboard.services.title')}}",
+    content: "{{trans('tour.dashboard.services.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#btnVacancies",
+    title: "{{trans('tour.dashboard.vacancies.title')}}",
+    content: "{{trans('tour.dashboard.vacancies.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#btnAgenda",
+    title: "{{trans('tour.dashboard.agenda.title')}}",
+    content: "{{trans('tour.dashboard.agenda.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#btnContacts",
+    title: "{{trans('tour.dashboard.contacts.title')}}",
+    content: "{{trans('tour.dashboard.contacts.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#indicator1",
+    title: "{{trans('tour.dashboard.indicator1.title')}}",
+    content: "{{trans('tour.dashboard.indicator1.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#indicator2",
+    title: "{{trans('tour.dashboard.indicator2.title')}}",
+    content: "{{trans('tour.dashboard.indicator2.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#indicator3",
+    title: "{{trans('tour.dashboard.indicator3.title')}}",
+    content: "{{trans('tour.dashboard.indicator3.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#indicator4",
+    title: "{{trans('tour.dashboard.indicator4.title')}}",
+    content: "{{trans('tour.dashboard.indicator4.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#indicator5",
+    title: "{{trans('tour.dashboard.indicator5.title')}}",
+    content: "{{trans('tour.dashboard.indicator5.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#indicator6",
+    title: "{{trans('tour.dashboard.indicator6.title')}}",
+    content: "{{trans('tour.dashboard.indicator6.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#indicator7",
+    title: "{{trans('tour.dashboard.indicator7.title')}}",
+    content: "{{trans('tour.dashboard.indicator7.content')}}",
+    placement: "top"
+  },
+  {
+    element: "#indicator8",
+    title: "{{trans('tour.dashboard.indicator8.title')}}",
+    content: "{{trans('tour.dashboard.indicator8.content')}}",
+    placement: "top"
+  },
+  {
+    element: "#indicator9",
+    title: "{{trans('tour.dashboard.indicator9.title')}}",
+    content: "{{trans('tour.dashboard.indicator9.content')}}",
+    placement: "top"
+  },
+  {
+    element: "#search",
+    title: "{{trans('tour.dashboard.search.title')}}",
+    content: "{{trans('tour.dashboard.search.content')}}",
+    placement: "bottom",
+    duration: 8000
+  },
+  {
+    element: "#navHome",
+    title: "{{trans('tour.dashboard.home.title')}}",
+    content: "{{trans('tour.dashboard.home.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#navLang",
+    title: "{{trans('tour.dashboard.lang.title')}}",
+    content: "{{trans('tour.dashboard.lang.content')}}",
+    placement: "bottom",
+    duration: 7000
+  },
+  {
+    element: "#navProfile",
+    title: "{{trans('tour.dashboard.profile.title')}}",
+    content: "{{trans('tour.dashboard.profile.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#btnDelete",
+    title: "{{trans('tour.dashboard.delete.title')}}",
+    content: "{{trans('tour.dashboard.delete.content')}}",
+    placement: "bottom"
+  },
+  {
+    element: "#enjoy",
+    title: "{{trans('tour.dashboard.enjoy.title')}}",
+    content: "{{trans('tour.dashboard.enjoy.content')}}",
+    orphan: true,
+    duration: 12000
+  },
+]});
+
+// Initialize the tour
+tourDashboard.init();
+
+// Start the tour
+tourDashboard.start();
+
+
 })();
 </script>
 @endsection
+
