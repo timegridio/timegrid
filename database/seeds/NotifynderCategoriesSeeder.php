aaae046375c91f6394b89e7b1de569e7ea6947f1
Ariel Vallese <alariva@gmail.com>
2015-07-25 18:19:28 +0200

    Add Notifynder category user.importedContacts

--- database/seeds/NotifynderCategoriesSeeder.php
+++ database/seeds/NotifynderCategoriesSeeder.php
@@ -17,6 +17,7 @@ class NotifynderCategoriesSeeder extends Seeder
         Notifynder::addCategory('user.suscribedBusiness', '{from.username} suscribed to business');
         Notifynder::addCategory('user.checkingVacancies', '{from.username} checks vacancies');
         Notifynder::addCategory('user.updatedBusinessPreferences', '{from.username} updated :business preferences');
+        Notifynder::addCategory('user.importedContacts', '{from.username} imported :count contacts');
         Notifynder::addCategory('appointment.reserve', '{from.username} made a reservation for :business');
         Notifynder::addCategory('appointment.annulate', '{from.username} annulated appointment');
         Notifynder::addCategory('appointment.confirm', '{from.username} confirmed appointment');
