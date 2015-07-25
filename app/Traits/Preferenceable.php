badd65d3da34f85dc5fe3fb62a5a8245dd687968
Ariel Vallese <alariva@gmail.com>
2015-07-25 15:42:11 +0200

    Bugfix preferences storing

--- app/Traits/Preferenceable.php
+++ app/Traits/Preferenceable.php
@@ -11,10 +11,32 @@ trait Preferenceable
 
     public function pref($key, $value = null, $type = 'string')
     {
-        if ($value) {
-            $this->preferences()->updateOrCreate(['key' => $key], ['value' => $value, 'type' => $type]);
+        if (isset($value)) {
+            $this->preferences()->updateOrCreate(['key' => $key], ['value' => $this->cast($value, $type), 'type' => $type]);
+            return $value;
         }
         $default = \App\Preference::getDefault($this, $key);
         return($pref = $this->preferences()->forKey($key)->first()) ? $pref->value() : $default->value();
+    }
+
+    private function cast($value, $type)
+    {
+        switch ($type) {
+            case 'bool':
+                return boolval($value);
+                break;
+            case 'int':
+                return intval($value);
+                break;
+            case 'float':
+                return floatval($value);
+                break;
+            case 'string':
+                return $value;
+                break;
+            default:
+                return $value;
+                break;
+        }
     }
 }
