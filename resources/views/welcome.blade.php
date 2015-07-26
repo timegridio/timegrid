08beaf40497376235e19003257afbc43dc541f98
Ariel Vallese <alariva@gmail.com>
2015-07-26 02:01:12 +0200

    Update welcome look and feel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,41 @@
 <html lang="en">
     <head>
         <title>{{trans('app.name')}}</title>
-        
-<!--        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" />
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
+        <style type="text/css">
+        @import url(http://fonts.googleapis.com/css?family=Montserrat);
+        @import url(http://fonts.googleapis.com/css?family=Arvo);
+        h1 {font-family: 'Montserrat', sans-serif;}
+        .jumbotron {
+            color:#fff; 
+            background-color: #367FA9;
+        }
+        img {
+            filter: none;
+            -webkit-filter: grayscale(20%);
+            -moz-filter:    grayscale(20%);
+            -ms-filter:     grayscale(20%);
+            -o-filter:      grayscale(20%);
+        }
+        img:hover {
+            filter: none;
+            -webkit-filter: grayscale(0%);
+            -moz-filter:    grayscale(0%);
+            -ms-filter:     grayscale(0%);
+            -o-filter:      grayscale(0%);
+        }
+        #inspire {font-family: 'Arvo', serif;}
+        #optimize:hover{background-color: #FFD8CC;}
+        #contact:hover{background-color: #FFF0CC;}
+        #do:hover{background-color: #E4FFCC;}
+        #love:hover{background-color: #FFCCEE;}
+        </style>
 
     </head>
     <body>
@@ -17,11 +46,16 @@
 
         <!-- Jumbotron Header -->
         <header class="jumbotron hero-spacer">
-            <h1><img src="{{ asset('img/timegrid-logo.png') }}" /> {{ trans('welcome.jumbotron.title') }}</h1>
-            <p>{{ trans('welcome.jumbotron.description') }}</p>
+            <h1>
+                <img class="hidden-xs hidden-sm" src="{{ asset('img/timegrid-logo-white.png') }}" />
+                {{ trans('welcome.jumbotron.title') }}
+            </h1>
+            <p class="hidden-xs" id="inspire">{{ trans('welcome.jumbotron.description') }}</p>
             <div class="row">
-                {!! Button::primary(trans('welcome.jumbotron.btn.begin'))->asLinkTo( url('auth/register') ) !!}
-                {!! Button::normal(trans('welcome.jumbotron.btn.login'))->asLinkTo( url('auth/login') ) !!}
+                <span class="btn-group">
+                    {!! Button::success(trans('welcome.jumbotron.btn.begin'))->asLinkTo( url('auth/register') ) !!}
+                    {!! Button::normal(trans('welcome.jumbotron.btn.login'))->asLinkTo( url('auth/login') ) !!}
+                </span>
             </div>
         </header>
 
@@ -29,52 +63,56 @@
         <div class="row">
 
             <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
+                <div class="thumbnail" id="optimize">
+                    <img src="{{asset('img/jumbo/optimize.png')}}" alt="">
                     <div class="caption">
                         <h3>{{trans('welcome.feature.1.title')}}</h3>
                         <p>{{trans('welcome.feature.1.content')}}</p>
                         <p>
-                            <a href="#" class="btn btn-primary">{{trans('welcome.feature.1.btn.action')}}</a> <a href="#" class="btn btn-default">{{trans('welcome.feature.1.btn.info')}}</a>
+                            <a href="#" class="btn btn-primary">{{trans('welcome.feature.1.btn.action')}}</a>
+                            <a href="#" class="btn btn-default">{{trans('welcome.feature.1.btn.info')}}</a>
                         </p>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
+                <div class="thumbnail" id="contact">
+                    <img src="{{asset('img/jumbo/contact.png')}}" alt="">
                     <div class="caption">
                         <h3>{{trans('welcome.feature.2.title')}}</h3>
                         <p>{{trans('welcome.feature.2.content')}}</p>
                         <p>
-                            <a href="#" class="btn btn-primary">{{trans('welcome.feature.2.btn.action')}}</a> <a href="#" class="btn btn-default">{{trans('welcome.feature.2.btn.info')}}</a>
+                            <a href="#" class="btn btn-primary">{{trans('welcome.feature.2.btn.action')}}</a>
+                            <a href="#" class="btn btn-default">{{trans('welcome.feature.2.btn.info')}}</a>
                         </p>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
+                <div class="thumbnail" id="do">
+                    <img src="{{asset('img/jumbo/do.png')}}" alt="">
                     <div class="caption">
                         <h3>{{trans('welcome.feature.3.title')}}</h3>
                         <p>{{trans('welcome.feature.3.content')}}</p>
                         <p>
-                            <a href="#" class="btn btn-primary">{{trans('welcome.feature.3.btn.action')}}</a> <a href="#" class="btn btn-default">{{trans('welcome.feature.3.btn.info')}}</a>
+                            <a href="#" class="btn btn-primary">{{trans('welcome.feature.3.btn.action')}}</a>
+                            <a href="#" class="btn btn-default">{{trans('welcome.feature.3.btn.info')}}</a>
                         </p>
                     </div>
                 </div>
             </div>
 
             <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
+                <div class="thumbnail" id="love">
+                    <img src="{{asset('img/jumbo/love.png')}}" alt="">
                     <div class="caption">
                         <h3>{{trans('welcome.feature.4.title')}}</h3>
                         <p>{{trans('welcome.feature.4.content')}}</p>
                         <p>
-                            <a href="#" class="btn btn-primary">{{trans('welcome.feature.4.btn.action')}}</a> <a href="#" class="btn btn-default">{{trans('welcome.feature.4.btn.info')}}</a>
+                            <a href="#" class="btn btn-primary">{{trans('welcome.feature.4.btn.action')}}</a>
+                            <a href="#" class="btn btn-default">{{trans('welcome.feature.4.btn.info')}}</a>
                         </p>
                     </div>
                 </div>
