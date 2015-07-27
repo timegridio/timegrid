62b9c4f2389bc1e708fed32fcf9e5efd88abb37a
Ariel Vallese <alariva@gmail.com>
2015-07-27 19:10:45 +0200

    Add Docblock and PSR-2

--- app/Service.php
+++ app/Service.php
@@ -2,26 +2,56 @@
 
 namespace App;
 
-#use Illuminate\Database\Eloquent\Model;
-
 class Service extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ['name', 'business_id', 'description', 'prerequisites', 'duration'];
 
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
     protected $guarded = ['id', 'slug'];
 
+    /**
+     * belongs to Business
+     * @return Illuminate\Database\Query Relationship Service belongs to Business query
+     */
     public function business()
     {
         return $this->belongsTo('App\Business');
     }
 
+    /**
+     * TODO: Check slug setting can be moved to a more proper place
+     * 
+     * Save the model to the database.
+     *
+     * @param  array  $options
+     * @return bool
+     */
     public function save(array $options = array())
     {
         $this->attributes['slug'] = str_slug($this->attributes['name']);
 
-        parent::save();
+        return parent::save();
     }
 
+    ////////////
+    // Scopes //
+    ////////////
+
+    /**
+     * Scope Slug
+     * @param  Illuminate\Database\Query $query
+     * @param  string $slug  Slug of the desired Service
+     * @return Illuminate\Database\Query Scoped query
+     */
     public function scopeSlug($query, $slug)
     {
         return $query->where('slug', '=', $slug)->get();
