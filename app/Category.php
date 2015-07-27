d4d06af6202e8eacbe39fa8c2a2db93bf592e739
Ariel Vallese <alariva@gmail.com>
2015-07-27 19:16:32 +0200

    Add Docblock and PSR-2

--- app/Category.php
+++ app/Category.php
@@ -2,12 +2,19 @@
 
 namespace App;
 
-#use Illuminate\Database\Eloquent\Model;
-
 class Category extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = ['slug', 'name', 'description', 'strategy'];
 
+    /**
+     * clasifies Businesses
+     * @return Illuminate\Database\Query Relationship Category has Businesses query
+     */
     public function businesses()
     {
         return $this->hasMany('App\Business')->withTimestamps();
