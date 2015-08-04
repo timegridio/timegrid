d2e5a525da891fa0ed5c8ed5d1bdf12f1ab336a5
Ariel Vallese <alariva@gmail.com>
2015-08-04 22:58:43 +0200

    Bugfix: Re-Enable Business owner check

--- resources/views/manager/search/_contact.blade.php
+++ resources/views/manager/search/_contact.blade.php
@@ -1,8 +1,8 @@
 <li class="list-group-item">
 @foreach ($contact->businesses as $business)
-    {{-- @if (Auth::user()->isOwner($business)) --}}    
+    @if (Auth::user()->isOwner($business))
         {!! Button::normal($business->name)->withIcon(Icon::home())->asLinkTo(route('manager.business.show', $business->id)) !!}
         {!! Button::normal($contact->fullname)->withIcon(Icon::user())->asLinkTo(route('manager.business.contact.show', [$business->id, $contact->id])) !!}
-    {{-- @endif --}}
+    @endif
 @endforeach
 </li>
