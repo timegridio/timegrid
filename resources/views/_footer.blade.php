f50fc077bc98050062c847ded8758ea75b99df82
Ariel Vallese <alariva@gmail.com>
2015-08-10 16:39:19 +0200

    Add advice footer for non production environments

--- resources/views/_footer.blade.php
+++ resources/views/_footer.blade.php
@@ -1,6 +1,14 @@
 <footer class="footer">
-  <div class="container">
-    <p>&nbsp;</p>
-    <p class="text-muted" title="v.{{ getenv('APP_VERSION') }}">{{trans('app.name')}}</p>
-  </div>
+    <div class="container">
+        <p>&nbsp;</p>
+        <p class="text-muted" title="v.{{ getenv('APP_VERSION') }}">
+            {{trans('app.name')}}
+            @if (env('APP_ENV') == 'local')
+                <span class="pull-right">{!! Label::danger('LOCAL') !!} <span class="text-danger">&nbsp;{{ trans('app.footer.local') }}</span></span>
+            @endif
+            @if (env('APP_ENV') == 'demo')
+                <span class="pull-right">{!! Label::danger('DEMO') !!} <span class="text-danger">&nbsp;{{ trans('app.footer.demo') }}</span></span>
+            @endif
+        </p>
+    </div>
 </footer>
