a08779d1e884025fb42c2bebd4a623b7f3362947
Ariel Vallese <alariva@gmail.com>
2015-11-13 14:22:52 +0100

    Fix Github social redirect URL

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
     'github' => [
         'client_id' => getenv('GITHUB_CLIENT_ID'),
         'client_secret' => getenv('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/social/login/github'
+        'redirect' => 'http://demo.timegrid.io/social/login/github'
     ],
 
     'facebook' => [
