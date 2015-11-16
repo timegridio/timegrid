312f0f101869512f59dcb14a214906a8b4886881
Jose Herrera <josevictorherrera@gmail.com>
2015-11-16 19:24:16 +0000

    handle no subscriptions

--- resources/views/user/businesses/suscriptions.blade.php
+++ resources/views/user/businesses/suscriptions.blade.php
@@ -9,23 +9,27 @@
                 <div class="panel-heading">{{ trans('user.businesses.suscriptions.title') }}</div>
 
                 <div class="panel-body">
+                    @if (!$contacts->isEmpty())
                     <table class="table table-condensed">
-                    @foreach ($contacts as $contact)
-                        <tr>
-                            <td>{{ $contact->nin }}</td>
-                            <td>{{ $contact->firstname }}</td>
-                            <td>{{ $contact->lastname }}</td>
-                            <td>{{ $contact->email }}</td>
-                            <td>
-                            @if($contact->businesses()->count())
-                                @foreach ($contact->businesses as $business)
-                                    {!! Button::normal($business->slug)->asLinkTo( route('user.business.contact.show', [$business, $contact])) !!}
-                                @endforeach
-                            @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                    </table>
+	                    @foreach ($contacts as $contact)
+	                        <tr>
+	                            <td>{{ $contact->nin }}</td>
+	                            <td>{{ $contact->firstname }}</td>
+	                            <td>{{ $contact->lastname }}</td>
+	                            <td>{{ $contact->email }}</td>
+	                            <td>
+	                            @if($contact->businesses()->count())
+	                                @foreach ($contact->businesses as $business)
+	                                    {!! Button::normal($business->slug)->asLinkTo( route('user.business.contact.show', [$business, $contact])) !!}
+	                                @endforeach
+	                            @endif
+	                            </td>
+	                        </tr>
+	                    @endforeach
+	                    </table>
+                    @else
+                    	No subscriptions.
+                    @endif
                 </div>
 
                 <div class="panel-footer">
