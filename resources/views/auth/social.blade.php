a0f125831963d6bc190238092278bbaed85b3df1
Jose Herrera <josevictorherrera@gmail.com>
2015-11-16 18:08:14 +0000

    centered oAuth2 links, resized row

--- resources/views/auth/social.blade.php
+++ resources/views/auth/social.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-md-6 col-md-offset-3 text-center">
+    <div class="col-md-12 text-center">
         <a class="btn btn-primary" href="{{ route('social.login', ['github']) }}">Github</a>
         <a class="btn btn-primary" href="{{ route('social.login', ['facebook']) }}">Facebook</a>
         <a class="btn btn-primary" href="{{ route('social.login', ['google']) }}">Google</a>
