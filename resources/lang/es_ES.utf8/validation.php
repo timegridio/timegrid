7b35b1fc68852b74b1d91d607ad1049f7c9767b9
Jose V Herrera <josevictorherrera@gmail.com>
2015-11-19 18:16:52 +0000

    add 'required' string

--- resources/lang/es_ES.utf8/validation.php
+++ resources/lang/es_ES.utf8/validation.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+  'phone' => 'El móvil debe estar en formato internacional sin código de pais, sin espacios ni guiones: Ej. 1132125991',
+  'required' => 'No completo un campo requirido.',
   'custom' => [
     'name' => [
         'min' => 'Dinos tu nombre completo',
