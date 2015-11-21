972f1b9dffe6bcb87625e0880374c7f7cf1c1332
Ariel Vallese <alariva@gmail.com>
2015-11-21 22:15:10 +0100

    Enhanced styling
    https://codeclimate.com/github/alariva/timegrid/resources/lang/es_AR.utf8/manager.php

--- resources/lang/es_AR.utf8/manager.php
+++ resources/lang/es_AR.utf8/manager.php
@@ -1,20 +1,19 @@
 <?php
 /*************************************************************************
- Generated via "php artisan localization:missing" at 2015/07/29 12:57:57 
+ Generated via "php artisan localization:missing" at 2015/07/29 12:57:57
 *************************************************************************/
 
-return array (
-  //==================================== Translations ====================================//
-  'business' => 
-  array (
-    'alert' => 
-    array (
+return array(
+  'business' =>
+  array(
+    'alert' =>
+    array(
       'deactivate_confirm' => '¡Atención! Desactivar el prestador es una operación IRREVERSIBLE. ¿Seguro desactivar?',
     ),
-    'btn' => 
-    array (
-      'tooltip' => 
-      array (
+    'btn' =>
+    array(
+      'tooltip' =>
+      array(
         'agenda' => 'Agenda de Turnos',
         'contacts' => 'Lista de Contactos',
         'edit' => 'Editar datos del Prestador',
@@ -23,60 +22,60 @@ return array (
         'vacancies' => 'Publicar Disponibilidad',
       ),
     ),
-    'hint' => 
-    array (
+    'hint' =>
+    array(
       'out_of_vacancies' => 'Publicá tu disponibilidad<br><br>Para que puedan empezar a pedirte turnos es necesario que indiques tu disponibilidad',
       'set_services' => 'Agregá los servicios que brindás',
     ),
-    'service' => 
-    array (
-      'msg' => 
-      array (
-        'update' => 
-        array (
+    'service' =>
+    array(
+      'msg' =>
+      array(
+        'update' =>
+        array(
           'success' => 'Servicio Actualizado!',
         ),
       ),
     ),
-    'form' => 
-    array (
-      'strategy' => 
-      array (
+    'form' =>
+    array(
+      'strategy' =>
+      array(
         'dateslot' => 'Por fecha',
         'timeslot' => 'Por fecha y horario',
       ),
     ),
   ),
-  'businesses' => 
-  array (
-    'btn' => 
-    array (
+  'businesses' =>
+  array(
+    'btn' =>
+    array(
       'deactivate' => 'Desactivar este prestador',
       'store' => 'Registrar',
       'update' => 'Actualizar',
       'return' => 'Volver',
     ),
-    'contacts' => 
-    array (
-      'btn' => 
-      array (
+    'contacts' =>
+    array(
+      'btn' =>
+      array(
         'create' => 'Agregar un contacto',
         'import' => 'Importar contactos',
       ),
     ),
-    'create' => 
-    array (
+    'create' =>
+    array(
       'title' => 'Registrar un prestador',
     ),
-    'dashboard' => 
-    array (
-      'alert' => 
-      array (
+    'dashboard' =>
+    array(
+      'alert' =>
+      array(
         'no_services_set' => 'Aún no tienes servicios cargados! Hazlo aquí!',
         'no_vacancies_set' => 'Aún no tienes publicada tu disponibilidad! Hazlo aquí!',
       ),
-      'panel' => 
-      array (
+      'panel' =>
+      array(
         'title_appointments_active' => 'Activos',
         'title_appointments_annulated' => 'Anulados',
         'title_appointments_served' => 'Atendidos',
@@ -87,114 +86,114 @@ return array (
         'title_contacts_registered' => 'Registrados',
         'title_contacts_total' => 'Totales',
       ),
-      'meta' => 
-      array (
+      'meta' =>
+      array(
         'title_owner' => 'Propietario',
         'title_registered_since' => 'Registrado desde',
       ),
     ),
-    'edit' => 
-    array (
+    'edit' =>
+    array(
       'title' => 'Edición de datos del prestador',
     ),
-    'form' => 
-    array (
-      'category' => 
-      array (
+    'form' =>
+    array(
+      'category' =>
+      array(
         'label' => 'Rubro Comercial',
       ),
-      'description' => 
-      array (
+      'description' =>
+      array(
         'label' => 'Describite',
         'placeholder' => 'Describí tu prestación a rasgos generales',
       ),
-      'name' => 
-      array (
+      'name' =>
+      array(
         'label' => 'Nombre',
         'placeholder' => 'Nombre comercial',
       ),
-      'phone' => 
-      array (
+      'phone' =>
+      array(
         'label' => 'Móvil',
         'placeholder' => 'tu móvil de contacto',
         'hint' => 'sin espacios ni guiones',
       ),
-      'postal_address' => 
-      array (
+      'postal_address' =>
+      array(
         'label' => 'Dirección Postal',
         'placeholder' => 'altura calle, barrio, ciudad, país',
       ),
-      'social_facebook' => 
-      array (
+      'social_facebook' =>
+      array(
         'label' => 'Dirección de Facebook',
         'placeholder' => 'https://www.facebook.com/timegrid.io',
       ),
-      'slug' => 
-      array (
+      'slug' =>
+      array(
         'label' => 'Alias',
         'placeholder' => 'así será el link en la web',
       ),
-      'timezone' => 
-      array (
+      'timezone' =>
+      array(
         'label' => 'Zona Horaria',
       ),
     ),
-    'index' => 
-    array (
+    'index' =>
+    array(
       'help' => 'Desde aquí puedes administrar todos los prestadores',
       'register_business_help' => 'Si eres prestador y quieres dar turnos online, es tu oportunidad!',
       'title' => 'Mis Prestadores',
-      'btn' => 
-      array (
+      'btn' =>
+      array(
         'manage' => 'Administrar prestadores',
         'register' => 'Registrar un prestador',
       ),
     ),
-    'msg' => 
-    array (
-      'create' => 
-      array (
+    'msg' =>
+    array(
+      'create' =>
+      array(
         'success' => '¡Muy bien! Vamos a registrarte con el plan :plan',
       ),
-      'destroy' => 
-      array (
+      'destroy' =>
+      array(
         'success' => 'Prestador removido',
       ),
-      'preferences' => 
-      array (
+      'preferences' =>
+      array(
         'success' => 'Actualizaste las preferencias Ok',
       ),
-      'store' => 
-      array (
+      'store' =>
+      array(
         'business_already_exists' => 'El prestador ya está registrado',
         'restored_trashed' => 'Prestador restaurado',
         'success' => 'Prestador registrado',
       ),
-      'update' => 
-      array (
+      'update' =>
+      array(
         'success' => 'Datos del prestador actualizados',
       ),
     ),
-    'preferences' => 
-    array (
+    'preferences' =>
+    array(
       'title' => 'Preferencias del prestador',
     ),
-    'vacancies' => 
-    array (
-      'btn' => 
-      array (
+    'vacancies' =>
+    array(
+      'btn' =>
+      array(
         'update' => 'Actualizar Disponibilidad',
       ),
     ),
-    'show' => 
-    array (
+    'show' =>
+    array(
       'title' => 'Prestador',
     ),
   ),
-  'contacts' => 
-  array (
-    'btn' => 
-    array (
+  'contacts' =>
+  array(
+    'btn' =>
+    array(
       'confirm_delete' => '¿Seguro querés eliminar el contacto?',
       'delete' => 'Eliminar',
       'edit' => 'Editar',
@@ -202,78 +201,78 @@ return array (
       'store' => 'Guardar',
       'update' => 'Actualizar',
     ),
-    'create' => 
-    array (
+    'create' =>
+    array(
       'title' => 'Contactos',
     ),
-    'form' => 
-    array (
-      'birthdate' => 
-      array (
+    'form' =>
+    array(
+      'birthdate' =>
+      array(
         'label' => 'Nacimiento',
         'placeholder' => 'fecha de nacimiento',
       ),
-      'data' => 
-      array (
+      'data' =>
+      array(
         'label' => 'datos en formato CSV',
       ),
-      'description' => 
-      array (
+      'description' =>
+      array(
         'label' => 'Descripción',
       ),
-      'email' => 
-      array (
+      'email' =>
+      array(
         'label' => 'Email',
         'placeholder' => 'email@ejemplo.com',
       ),
-      'firstname' => 
-      array (
+      'firstname' =>
+      array(
         'label' => 'Nombre',
         'placeholder' => 'primer nombre',
       ),
-      'gender' => 
-      array (
-        'female' => 
-        array (
+      'gender' =>
+      array(
+        'female' =>
+        array(
           'label' => 'Femenino',
         ),
-        'male' => 
-        array (
+        'male' =>
+        array(
           'label' => 'Masculino',
         ),
         'label' => 'Sexo',
       ),
-      'lastname' => 
-      array (
+      'lastname' =>
+      array(
         'label' => 'Apellido',
         'placeholder' => 'apellido',
       ),
-      'mobile' => 
-      array (
+      'mobile' =>
+      array(
         'label' => 'Móvil',
         'placeholder' => 'número de móvil completo',
       ),
-      'nin' => 
-      array (
+      'nin' =>
+      array(
         'label' => 'DNI',
         'placeholder' => 'número de identificación nacional',
       ),
-      'notes' => 
-      array (
+      'notes' =>
+      array(
         'label' => 'Notas',
         'placeholder' => 'notas',
       ),
-      'prerequisites' => 
-      array (
+      'prerequisites' =>
+      array(
         'label' => 'Prerequisitos',
       ),
     ),
-    'import' => 
-    array (
+    'import' =>
+    array(
       'title' => 'Importar contactos',
     ),
-    'label' => 
-    array (
+    'label' =>
+    array(
       'birthdate' => 'Nacimiento',
       'email' => 'Email',
       'member_since' => 'Suscripto desde',
@@ -284,14 +283,14 @@ return array (
       'next_appointment' => 'Próximo Turno',
       'username' => 'Usuario',
     ),
-    'list' => 
-    array (
-      'btn' => 
-      array (
+    'list' =>
+    array(
+      'btn' =>
+      array(
         'filter' => 'Filtro',
       ),
-      'header' => 
-      array (
+      'header' =>
+      array(
         'email' => 'Email',
         'firstname' => 'Nombre',
         'lastname' => 'Apellidos',
@@ -300,102 +299,102 @@ return array (
         'username' => 'Usuario',
         'gender' => 'Sexo',
       ),
-      'msg' => 
-      array (
+      'msg' =>
+      array(
         'filter_no_results' => 'Nada por aquí',
       ),
     ),
-    'msg' => 
-    array (
-      'destroy' => 
-      array (
+    'msg' =>
+    array(
+      'destroy' =>
+      array(
         'success' => '¡Contacto eliminado!',
       ),
-      'import' => 
-      array (
+      'import' =>
+      array(
         'success' => ':count Contactos importados',
       ),
-      'store' => 
-      array (
+      'store' =>
+      array(
         'success' => '¡Contacto registrado Ok!',
         'warning_showing_existing_contact' => 'Encontramos el contacto ya registrado',
       ),
-      'update' => 
-      array (
+      'update' =>
+      array(
         'success' => 'Actualizado Ok',
       ),
     ),
     'title' => 'Mis Clientes',
   ),
-  'service' => 
-  array (
-    'btn' => 
-    array (
+  'service' =>
+  array(
+    'btn' =>
+    array(
       'delete' => 'Eliminar',
       'update' => 'Actualizar',
     ),
-    'form' => 
-    array (
-      'name' => 
-      array (
+    'form' =>
+    array(
+      'name' =>
+      array(
         'label' => 'Nombre del servicio',
       ),
     ),
-    'msg' => 
-    array (
-      'store' => 
-      array (
+    'msg' =>
+    array(
+      'store' =>
+      array(
         'success' => '¡Servicio Guardado!',
       ),
     ),
   ),
-  'services' => 
-  array (
-    'btn' => 
-    array (
+  'services' =>
+  array(
+    'btn' =>
+    array(
       'create' => 'Agregar un servicio',
       'store' => 'Guardar',
     ),
-    'create' => 
-    array (
-      'alert' => 
-      array (
+    'create' =>
+    array(
+      'alert' =>
+      array(
         'go_to_vacancies' => '¡Muy bien! Ya podés indicar tu disponibilidad y hacerla visible a tus clientes',
       ),
-      'btn' => 
-      array (
+      'btn' =>
+      array(
         'go_to_vacancies' => 'Publicar disponibilidad',
       ),
       'title' => 'Agregar un servicio',
     ),
-    'index' => 
-    array (
-      'th' => 
-      array (
+    'index' =>
+    array(
+      'th' =>
+      array(
         'name' => 'Nombre',
         'duration' => 'Duración',
         'slug' => 'Alias',
       ),
       'title' => 'Servicios',
     ),
-    'msg' => 
-    array (
-      'destroy' => 
-      array (
+    'msg' =>
+    array(
+      'destroy' =>
+      array(
         'success' => '¡Servicio eliminado!',
       ),
     ),
   ),
-  'vacancies' => 
-  array (
-    'edit' => 
-    array (
+  'vacancies' =>
+  array(
+    'edit' =>
+    array(
       'title' => 'Disponibilidades',
     ),
-    'msg' => 
-    array (
-      'store' => 
-      array (
+    'msg' =>
+    array(
+      'store' =>
+      array(
         'nothing_changed' => 'Debés completar tu disponibilidad para alguna fecha',
         'success' => 'Disponibilidades registradas!',
       ),
@@ -404,13 +403,12 @@ return array (
         'no_services' => 'No hay servicios registrados. Favor de registrar servicios para tu negocio.',
       ),
     ),
-    'table' => 
-    array (
-      'th' => 
-      array (
+    'table' =>
+    array(
+      'th' =>
+      array(
         'date' => 'Fecha',
       ),
     ),
   ),
-  //================================== Obsolete strings ==================================//
 );
