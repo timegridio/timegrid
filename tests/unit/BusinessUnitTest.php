a4fa1f38ed7af234fb055616a2484efe809ea670
Ariel Vallese <alariva@gmail.com>
2015-11-21 12:13:20 +0100

    Enhance Business Unit Test coverage

--- tests/unit/BusinessUnitTest.php
+++ tests/unit/BusinessUnitTest.php
@@ -1,22 +1,56 @@
 <?php
 
+use App\Business;
+use App\Presenters\BusinessPresenter;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class BusinessUnitTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function testBusinessCreationWithSuccess()
+   /**
+     * @covers            \App\Business::create
+     */
+    public function testCreatedBusinessGetsStoredInDatabase()
     {
-        $business = factory(App\Business::class)->create();
+        $business = factory(Business::class)->create();
 
         $this->seeInDatabase('businesses', ['slug' => $business->slug]);
     }
 
-    public function testBusinessPresenter()
+   /**
+     * @covers            \App\Business::getPresenter
+     */
+    public function testBusinessGetPresenter()
     {
-        $business = factory(App\Business::class)->create();
+        $business = factory(Business::class)->create();
 
         $businessPresenter = $business->getPresenter();
+
+        $this->assertInstanceOf(BusinessPresenter::class, $businessPresenter);
+    }
+
+   /**
+     * @covers            \App\Business::setPhoneAttribute
+     */
+    public function testSetEmptyPhoneAttributeGetsNull()
+    {
+        $business = factory(Business::class)->create();
+
+        $business->phone = '';
+
+        $this->assertNull($business->phone);
+    }
+
+   /**
+     * @covers            \App\Business::setPostalAddressAttribute
+     */
+    public function testSetEmptyPostalAddressAttributeGetsNull()
+    {
+        $business = factory(Business::class)->create();
+
+        $business->postal_address = '';
+
+        $this->assertNull($business->postal_address);
     }
 }
