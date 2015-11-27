2e2668b495795b9baf6d3f2b079e78525327772c
Ariel Vallese <alariva@gmail.com>
2015-11-27 20:26:27 +0100

    Review code and optimize
    Remove unused imports
    PSR-2

--- app/Http/Controllers/Manager/BusinessServiceController.php
+++ app/Http/Controllers/Manager/BusinessServiceController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Manager;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Requests;
 use App\Business;
 use App\Service;
 use Redirect;
