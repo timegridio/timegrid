3c14bcd67b6bd4088de3e6ab0b908107c8f172f6
Ariel Vallese <alariva@gmail.com>
2015-11-27 20:29:45 +0100

    Review code and optimize
    Remove unused imports
    PSR-2

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
-use Illuminate\Validation\ValidationServiceProvider;
 use App\Http\Controllers\Controller;
 use App\Events\NewRegisteredUser;
 use App\User;
