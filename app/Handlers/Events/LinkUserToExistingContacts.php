fe1ce22b4ca49f40dd909d620a7e1ef6abe61002
Ariel Vallese <alariva@gmail.com>
2015-11-28 14:51:46 +0100

    Remove logging facility

--- app/Handlers/Events/LinkUserToExistingContacts.php
+++ app/Handlers/Events/LinkUserToExistingContacts.php
@@ -5,20 +5,9 @@ namespace App\Handlers\Events;
 use App\Events\NewRegisteredUser;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Log;
 
 class LinkUserToExistingContacts
 {
-    /**
-     * Create the event handler.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
     /**
      * Handle the event.
      *
@@ -27,7 +16,8 @@ class LinkUserToExistingContacts
      */
     public function handle(NewRegisteredUser $event)
     {
-        Log::info("Handle NewRegisteredUser.LinkUserToExistingContacts(): Linking <{$event->user->email}> to contacts");
+        # $this->log->info("Handle NewRegisteredUser.LinkUserToExistingContacts(): ".
+        #                  "Linking <{$event->user->email}> to contacts");
         $event->user->linkToContacts();
     }
 }
