f8797a4044b59e9776ecdc12bf6ac59b390be45f
Ariel Vallese <alariva@gmail.com>
2015-11-29 12:14:30 +0100

    Bugfix: Change App and Mail access
    As in this context the dependencies are not resolved through
    the Service Container:
    
    Use helper app() as can be used
    For mail use direct Facade call

--- app/Handlers/Events/SendBookingNotification.php
+++ app/Handlers/Events/SendBookingNotification.php
@@ -3,7 +3,6 @@
 namespace App\Handlers\Events;
 
 use App\Events\NewBooking;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Mail;
 use Fenos\Notifynder\Facades\Notifynder;
 use Illuminate\Queue\InteractsWithQueue;
@@ -11,29 +10,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 
 class SendBookingNotification
 {
-    /**
-     * Mail facade
-     * @var mail
-     */
-    protected $mail;
-
-    /**
-     * App facade
-     * @var app
-     */
-    protected $app;
-
-    /**
-     * Create the event handler.
-     *
-     * @return void
-     */
-    public function __construct(Mail $mail, App $app)
-    {
-        $this->mail = $mail;
-        $this->app  = $app;
-    }
-
     /**
      * Handle the event.
      *
@@ -55,11 +31,11 @@ class SendBookingNotification
                    ->extra(compact('business_name', 'code', 'date'))
                    ->send();
 
-        $locale = $this->app->getLocale();
-        $this->mail->send("emails.{$locale}.appointments.user._new", ['user' => $event->user, 'appointment' => $event->appointment->getPresenter()], function ($m) use ($event) {
+        $locale = app()->getLocale();
+        Mail::send("emails.{$locale}.appointments.user._new", ['user' => $event->user, 'appointment' => $event->appointment->getPresenter()], function ($m) use ($event) {
             $m->to($event->user->email, $event->user->name)->subject(trans('emails.user.appointment.reserved.subject'));
         });
-        $this->mail->send("emails.{$locale}.appointments.manager._new", ['user' => $event->appointment->business->owner(), 'appointment' => $event->appointment->getPresenter()], function ($m) use ($event) {
+        Mail::send("emails.{$locale}.appointments.manager._new", ['user' => $event->appointment->business->owner(), 'appointment' => $event->appointment->getPresenter()], function ($m) use ($event) {
             $m->to($event->appointment->business->owner()->email, $event->appointment->business->owner()->name)->subject(trans('emails.manager.appointment.reserved.subject'));
         });
     }
--- app/Handlers/Events/SendMailUserWelcome.php
+++ app/Handlers/Events/SendMailUserWelcome.php
@@ -2,7 +2,6 @@
 
 namespace App\Handlers\Events;
 
-use App;
 use App\Events\NewRegisteredUser;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Queue\InteractsWithQueue;
@@ -10,29 +9,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 
 class SendMailUserWelcome
 {
-    /**
-     * Mail facade
-     * @var mail
-     */
-    protected $mail;
-
-    /**
-     * App facade
-     * @var app
-     */
-    protected $app;
-
-    /**
-     * Create the event handler.
-     *
-     * @return void
-     */
-    public function __construct(Mail $mail, App $app)
-    {
-        $this->mail = $mail;
-        $this->app  = $app;
-    }
-
     /**
      * Handle the event.
      *
@@ -42,8 +18,8 @@ class SendMailUserWelcome
     public function handle(NewRegisteredUser $event)
     {
         logger()->info('Handle NewRegisteredUser.SendMailUserWelcome()');
-        $locale = $this->app->getLocale();
-        $this->mail->send("emails.{$locale}.welcome", ['user' => $event->user], function ($m) use ($event) {
+        $locale = app()->getLocale();
+        Mail::send("emails.{$locale}.welcome", ['user' => $event->user], function ($m) use ($event) {
             $m->to($event->user->email, $event->user->name)->subject(trans('emails.user.welcome.subject'));
         });
     }
