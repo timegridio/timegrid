ebcf1b87915bbaa8e744bd4b9b1d8fe9afd18471
Ariel Vallese <alariva@gmail.com>
2015-11-29 01:46:50 +0100

    Upgrade Laravel To 5.1.11
    http://laravel.com/docs/5.1/upgrade#upgrade-5.1.11
    
    pkg: Remove kodeine/laravel-acl as now supported from core

--- app/User.php
+++ app/User.php
@@ -4,15 +4,17 @@ namespace App;
 
 use App\Contact;
 use Fenos\Notifynder\Notifable;
-use Kodeine\Acl\Traits\HasRole;
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
-    use Authenticatable, CanResetPassword, HasRole, Notifable;
+    use Authenticatable, Authorizable, CanResetPassword, Notifable;
 
     /**
      * The database table used by the model.
--- config/acl.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'role'       => 'Kodeine\Acl\Models\Eloquent\Role',
-    'permission' => 'Kodeine\Acl\Models\Eloquent\Permission',
-];
