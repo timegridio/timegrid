3b656c117bc099bd1bef6a06d83e3e1fb4b6fe78
Ariel Vallese <alariva@gmail.com>
2015-12-01 23:26:22 +0100

    Layout enhancement
    Fixes https://github.com/alariva/timegrid/issues/14

--- resources/views/user/businesses/_row.blade.php
+++ resources/views/user/businesses/_row.blade.php
@@ -1,9 +1,6 @@
 @section('css')
 @parent
 <style>
-.bizinfo {
-    color: #666;
-}
 .glyphicon {
     margin-bottom: 4px;
     margin-right: 10px;
@@ -12,20 +9,40 @@
 </style>
 @endsection
 
-<div class="col-xs-2 col-sm-2 col-md-2">
-    <a href="{{route('user.businesses.home', ['business' => $business->id])}}">{!! $business->getPresenter()->getFacebookImg('normal') !!}</a>
-</div>
-<div class="col-xs-10 col-sm-10 col-md-10">
-    <h4><a href="{{route('user.businesses.home', ['business' => $business->id])}}">{{ str_limit($business->name, 50) }}</a></h4>
-    <div class="bizinfo">
-        @if ( $business->postal_address )
-            <p title="{{ $business->postal_address }}"><i class="glyphicon glyphicon-map-marker"></i>&nbsp;{{ $business->postal_address }}</p>
-        @endif
-        @if ( $business->category )
-            <p><i class="glyphicon glyphicon-home"></i>&nbsp;{!! trans('app.business.category.'.strtolower($business->category->slug)) !!}</p>
-        @endif
-        @if ( $business->description )
-            <p><i class="glyphicon glyphicon-info-sign"></i>&nbsp;{{ $business->description }}</p>
-        @endif
-    </div>
-</div>
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-2 col-sm-3 text-left">
+            <a href="{{route('user.businesses.home', ['business' => $business->id])}}">{!! $business->getPresenter()->getFacebookImg('normal') !!}</a>
+        </div>
+        <div class="col-md-10 col-sm-9">
+            <h3>
+                <a href="{{route('user.businesses.home', ['business' => $business->id])}}">{{ str_limit($business->name, 50) }}</a>
+                &nbsp;
+                <small class="text-muted"><i class="glyphicon glyphicon-star"></i>{{ $business->subscriptionsCount }}</small>
+            </h3>
+            <div class="row">
+                <div class="col-xs-9">
+
+
+
+                @if ( $business->category )
+                    <p><i class="glyphicon glyphicon-home"></i>&nbsp;{!! trans('app.business.category.'.strtolower($business->category->slug)) !!}</p>
+                @endif
+
+                @if ( $business->postal_address )
+                    <p title="{{ $business->postal_address }}"><i class="glyphicon glyphicon-map-marker"></i>&nbsp;{{ $business->postal_address }}</p>
+                @endif
+
+                @if ( $business->description )
+                    <p><i class="glyphicon glyphicon-info-sign"></i>&nbsp;{{ str_limit($business->description, 80) }}</p>
+                @endif
+
+                </div>
+                <div class="col-xs-3"></div>
+            </div>
+        </div>
+
+    </div>{{-- row --}}
+</div>{{-- container --}}
+<hr />
