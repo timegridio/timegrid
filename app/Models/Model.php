b8517088bad5f00fbcb0ec3cd455797d27557504
Ariel Vallese <alariva@gmail.com>
2015-12-01 16:28:41 +0100

    pkg: Remove watson/rememberable

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Watson\Rememberable\Rememberable;
+#use Watson\Rememberable\Rememberable;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 abstract class Model extends Eloquent
 {
-    use Rememberable;
+    #use Rememberable;
 }
