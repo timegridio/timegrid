dd28470485ae8c0ebd59b409b0bb97281970eb8a
Ariel Vallese <alariva@gmail.com>
2015-12-02 14:46:49 +0100

    Enhance code styling
    Reindent
    [codeclimate] Avoid unused local variables such as '$table'
    Warp lines exceeding 120 chars

--- database/migrations/2015_02_07_000200_charify_countries_table.php
+++ database/migrations/2015_02_07_000200_charify_countries_table.php
@@ -11,15 +11,19 @@ class CharifyCountriesTable extends Migration
      */
     public function up()
     {
-        Schema::table(\Config::get('countries.table_name'), function ($table) {
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY country_code CHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY iso_3166_2 CHAR(2) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY iso_3166_3 CHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY region_code CHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY sub_region_code CHAR(3) NOT NULL DEFAULT ''");
-            });
+        Schema::table(\Config::get('countries.table_name'), function () {
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY country_code CHAR(3) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY iso_3166_2 CHAR(2) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY iso_3166_3 CHAR(3) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY region_code CHAR(3) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY sub_region_code CHAR(3) NOT NULL DEFAULT ''");
+        });
     }
-    
 
     /**
      * Reverse the migrations.
@@ -28,12 +32,17 @@ class CharifyCountriesTable extends Migration
      */
     public function down()
     {
-        Schema::table(\Config::get('countries.table_name'), function ($table) {
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY country_code VARCHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY iso_3166_2 VARCHAR(2) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY iso_3166_3 VARCHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY region_code VARCHAR(3) NOT NULL DEFAULT ''");
-                DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " MODIFY sub_region_code VARCHAR(3) NOT NULL DEFAULT ''");
-            });
+        Schema::table(\Config::get('countries.table_name'), function () {
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY country_code VARCHAR(3) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY iso_3166_2 VARCHAR(2) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY iso_3166_3 VARCHAR(3) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY region_code VARCHAR(3) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') .
+                " MODIFY sub_region_code VARCHAR(3) NOT NULL DEFAULT ''");
+        });
     }
 }
