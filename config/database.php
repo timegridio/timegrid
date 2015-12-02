d62eb4aa325c720dab5a3bca5f20e8d3b952d3e2
Ariel Vallese <alariva@gmail.com>
2015-12-02 16:26:21 +0100

    Setup travis params

--- config/database.php
+++ config/database.php
@@ -67,9 +67,9 @@ return [
         'mysql_testing' => [
             'driver'    => 'mysql',
             'host'      => env('TEST_DB_HOST', 'localhost'),
-            'database'  => env('TEST_DB_DATABASE', 'test.timegrid'),
-            'username'  => env('TEST_DB_USERNAME', 'test.timegrid'),
-            'password'  => env('TEST_DB_PASSWORD', 'test.timegrid'),
+            'database'  => env('TEST_DB_DATABASE', 'test_timegrid'),
+            'username'  => env('TEST_DB_USERNAME', 'test_timegrid'),
+            'password'  => env('TEST_DB_PASSWORD', 'test_timegrid'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
