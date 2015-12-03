50e010db045b505c31c2c43c73bb33009e27355d
Ariel Vallese <alariva@gmail.com>
2015-12-03 19:21:10 +0100

    Disable prevent double submit JS function as seems to cause problems

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -97,24 +97,6 @@
 <script type="text/javascript">
 $(document).ready(function() {
     $('.btn').tooltipster({animation: "grow", theme: 'tooltipster-timegrid'});
-
-    // jQuery plugin to prevent double submission of forms
-    jQuery.fn.preventDoubleSubmission = function() {
-      $(this).on('submit',function(e){
-        var $form = $(this);     
-        if ($form.data('submitted') === true) {
-            // Previously submitted - don't submit again
-            e.preventDefault();
-        } else {
-            // Mark it so that the next submit can be ignored
-            $form.data('submitted', true);
-        }
-      });
-      // Keep chainability
-      return this;
-    };
-
-    $('form').preventDoubleSubmission();
 });
 </script>
 
