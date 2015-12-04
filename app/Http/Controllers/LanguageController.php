76a040217d82170e40dc3f9d9ee93bcac19868bf
Ariel Vallese <alariva@gmail.com>
2015-12-04 21:03:30 +0100

    Refactor
    Extract logic into helpers

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -14,14 +14,38 @@ class LanguageController extends Controller
     {
         $this->log->info(sprintf("%s: %s", __METHOD__, $lang));
  
-        if (array_key_exists($lang, config()->get('languages'))) {
-            session()->set('applocale', $lang);
-            $locale = \Locale::parseLocale($lang);
-            session()->set('language', $locale['language']);
-
-            $this->log->info("  Language Switched:{$locale['language']}");
+        if ($this->isValidLanguage($lang)) {
+            $this->setSessionLanguage($lang);
         }
  
         return redirect()->back();
+    }
+
+    /////////////
+    // HELPERS //
+    /////////////
+
+    /**
+     * Determine if is a language is accepted by app config
+     * 
+     * @param  string  $lang Requested language
+     * @return boolean       Is an accepted language for this app
+     */
+    protected function isValidLanguage($lang)
+    {
+        return array_key_exists($lang, config()->get('languages'));
+    }
+
+    /**
+     * set Language to Session
+     * 
+     * @param string $lang Requested language
+     */
+    protected function setSessionLanguage($lang)
+    {
+        session()->set('applocale', $lang);
+        $locale = \Locale::parseLocale($lang);
+        session()->set('language', $locale['language']);
+        $this->log->info("Language Switched:{$locale['language']}");
     }
 }
