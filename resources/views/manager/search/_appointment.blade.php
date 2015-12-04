e38df02f9ac97245eb85d9d25f17db44a774c712
Ariel Vallese <alariva@gmail.com>
2015-12-04 22:36:55 +0100

    Fix invalid method access
    This is a workaround as all the presentation logic needs a new approach
    design.

--- resources/views/manager/search/_appointment.blade.php
+++ resources/views/manager/search/_appointment.blade.php
@@ -1,8 +1,7 @@
 <li class="list-group-item">
     <div class="btn-group">
         {!! Button::normal($appointment->business->name)->large()->withIcon(Icon::home())->asLinkTo(route('manager.business.show', $appointment->business->id)) !!}
-        {!! Button::normal(substr($appointment->code,0,4))->large()->withIcon(Icon::calendar())->asLinkTo(route('manager.business.agenda.index', $appointment->business->id)) !!}
-        {!! Button::normal($appointment->widget()->dateLabel())->large()->withIcon(Icon::calendar())->asLinkTo(route('manager.business.agenda.index', $appointment->business->id)) !!}
+        {!! Button::normal($appointment->date)->large()->withIcon(Icon::calendar())->asLinkTo(route('manager.business.agenda.index', $appointment->business->id)) !!}
         {!! Button::normal($appointment->contact->fullname)->large()->withIcon(Icon::user())->asLinkTo(route('manager.business.contact.show', [$appointment->business->id, $appointment->contact->id])) !!}
         {!! Button::normal($appointment->service->name)->large()->withIcon(Icon::tag())->asLinkTo(route('manager.business.service.show', [$appointment->business->id, $appointment->service->id])) !!}
     </div>
