8b7353e5da66959507d33c547f4cef58f111bf0f
Ariel Vallese <alariva@gmail.com>
2015-12-04 21:44:25 +0100

    Enhance readability

--- resources/views/_errors.blade.php
+++ resources/views/_errors.blade.php
@@ -1,10 +1,12 @@
+{{-- Validation Error Messages --}}
 @if($errors->has())
     <div class="alert alert-danger">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
         <ul>
-        @foreach($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
         </ul>
     </div>
 @endif
+{{-- Validation Error Messages --}}
