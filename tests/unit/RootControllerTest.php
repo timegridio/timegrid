bcef0f106355239db72e93f371eb4195977f7f0d
Ariel Vallese <alariva@gmail.com>
2015-12-04 18:51:00 +0100

    Add tests

--- tests/unit/RootControllerTest.php
+++ tests/unit/RootControllerTest.php
@@ -3,6 +3,7 @@
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Auth;
 
 class RootControllerTest extends TestCase
 {
@@ -59,5 +60,51 @@ class RootControllerTest extends TestCase
         // Then I should see the Root access warning
         $this->seePageIs('/home')
              ->dontSee('Registered Users');
+    }
+
+    /**
+     * @covers   App\Http\Controllers\Root\RootController::getSudo
+     * @test
+     */
+    public function it_sudoes_as_user()
+    {
+        // Given I am an authenticated user
+        $root = factory(User::class)->create();
+        $role = factory(Role::class)->create(['name' => 'root', 'slug' => 'root']);
+        $root->assignRole('root');
+
+        $user = factory(User::class)->create();
+
+        // With the Root role
+        $this->actingAs($root);
+
+        // And I go to root dashboard
+        $this->visit("/root/sudo/{$user->id}");
+
+        // Then I should see the Root access warning
+        $this->see('AUTHORIZED USE ONLY');
+        $this->assertEquals($user->id, Auth::user()->id);
+    }
+
+    /**
+     * @covers   App\Http\Controllers\Root\RootController::getSudo
+     * @test
+     */
+    public function it_fails_sudoing_as_user()
+    {
+        // Given I am an authenticated user
+        $nonRoot = factory(User::class)->create();
+
+        $user = factory(User::class)->create();
+
+        // With the Root role
+        $this->actingAs($nonRoot);
+
+        // And I go to root dashboard
+        $this->visit("/root/sudo/{$user->id}");
+
+        // Then I should see the Root access warning
+        $this->dontSee('AUTHORIZED USE ONLY');
+        $this->assertNotEquals($user->id, Auth::user()->id);
     }
 }
