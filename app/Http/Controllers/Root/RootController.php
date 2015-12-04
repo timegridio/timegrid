323d2489388681aa0ad7543f146e7ee61f60395f
Ariel Vallese <alariva@gmail.com>
2015-12-04 20:51:13 +0100

    Enhance readability

--- app/Http/Controllers/Root/RootController.php
+++ app/Http/Controllers/Root/RootController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Root;
 use App\Models\User;
 use Laracasts\Flash\Flash;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Redirect;
 
 class RootController extends Controller
 {
@@ -19,18 +18,28 @@ class RootController extends Controller
         $this->log->info(__METHOD__);
         $this->log->warning('[ROOT ACCESS]');
 
+        //////////////////
+        // FOR REFACTOR //
+        //////////////////
+
         $users = User::all();
+
         return view('root.dashboard', compact('users'));
     }
 
+    /**
+     * Switch authentication into another user for support purpose
+     *
+     * @return Response Show Root dashboard
+     */
     public function getSudo($userId)
     {
         $this->log->info(__METHOD__);
 
-        auth()->loginUsingId($userId);
         $this->log->warning("[!] ROOT SUDO userId:{$userId}");
+        auth()->loginUsingId($userId);
         
-        Flash::warning('!!! ADVICE THIS FOR IS AUTHORIZED USE ONLY AND YOUR ACTION IS RECORDERED !!!');
-        return Redirect::route('user.businesses.list');
+        Flash::warning('ADVICE: THIS IS FOR AUTHORIZED USE ONLY AND YOUR ACTIONS ARE BEING RECORDERED !!!');
+        return redirect()->route('user.businesses.list');
     }
 }
