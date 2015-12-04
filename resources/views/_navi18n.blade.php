41b171fa8b9549a185daa4942e80c23cdaa906a0
Ariel Vallese <alariva@gmail.com>
2015-12-04 21:43:01 +0100

    Switch App:: to app()->

--- resources/views/_navi18n.blade.php
+++ resources/views/_navi18n.blade.php
@@ -1,9 +1,11 @@
-{{-- Language Switcher --}}
+{{-- Language Switcher Dropdown --}}
 <li id="navLang" class="dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Config::get('languages')[App::getLocale()] }} <b class="caret"></b></a>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        {{ Config::get('languages')[app()->getLocale()] }} <b class="caret"></b>
+    </a>
     <ul class="dropdown-menu">
         @foreach (Config::get('languages') as $lang => $language)
-            @if ($lang != App::getLocale())
+            @if ($lang != app()->getLocale())
                 <li>
                     {!! link_to_route('lang.switch', $language, $lang) !!}
                 </li>
@@ -11,4 +13,4 @@
         @endforeach
     </ul>
 </li>
-{{-- Language Switcher --}} 
+{{-- Language Switcher Dropdown --}} 
