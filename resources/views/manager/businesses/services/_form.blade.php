2134b21df240b8abe9aac86608062a444b5dd3d9
Ariel Vallese <alariva@gmail.com>
2015-12-04 12:46:26 +0100

    Enhance layouts and readability

--- resources/views/manager/businesses/services/_form.blade.php
+++ resources/views/manager/businesses/services/_form.blade.php
@@ -1,26 +1,32 @@
+{{-- Services Form Partial --}}
 <div class="row">
     <div class="form-group col-xs-6">
-        {!! Form::text('name', null,
-            array('required',
-                  'class'=>'form-control',
-                  'placeholder'=> trans('manager.service.form.name.label') )) !!}
+        {!! Form::text('name', null, [
+            'required',
+            'class'=>'form-control',
+            'placeholder'=> trans('manager.service.form.name.label')
+            ]) !!}
     </div>
 </div>
 <div class="row">
     <div class="col-xs-12">
         <div class="form-group">
-            {!! Form::textarea('description', null,
-                array('class'=>'form-control',
-                      'placeholder'=> trans('manager.contacts.form.description.label') )) !!}
+            {!! Form::textarea('description', null, [
+                'class'=>'form-control',
+                'rows'=> '3',
+                'placeholder'=> trans('manager.contacts.form.description.label') 
+                ]) !!}
         </div>
     </div>
 </div>
 <div class="row">
     <div class="col-xs-12">
         <div class="form-group">
-            {!! Form::textarea('prerequisites', null,
-                array('class'=>'form-control',
-                      'placeholder'=> trans('manager.contacts.form.prerequisites.label') )) !!}
+            {!! Form::textarea('prerequisites', null, [
+                'class'=>'form-control',
+                'rows'=> '3',
+                'placeholder'=> trans('manager.contacts.form.prerequisites.label') 
+                ]) !!}
         </div>
     </div>
 </div>
--- resources/views/manager/businesses/services/create.blade.php
+++ resources/views/manager/businesses/services/create.blade.php
@@ -2,21 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-
     {!! Alert::info(trans('manager.services.create.instructions')) !!}
 
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ trans('manager.services.create.title') }}</div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            {{ trans('manager.services.create.title') }}
+        </div>
 
-                <div class="panel-body">
-                    @include('_errors')
+        <div class="panel-body">
+            @include('_errors')
 
-                    {!! Form::model($service, ['route' => ['manager.business.service.store', $business]]) !!}
-                        @include('manager.businesses.services._form',['submitLabel' => trans('manager.services.btn.store')])
-                    {!! Form::close() !!}
-                </div>
-            </div>
+            {!! Form::model($service, ['route' => ['manager.business.service.store', $business]]) !!}
+                @include('manager.businesses.services._form',['submitLabel' => trans('manager.services.btn.store')])
+            {!! Form::close() !!}
+        </div>
     </div>
 </div>
 @endsection
