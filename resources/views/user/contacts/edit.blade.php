850c23f218484d38250ec98a3ad6b100d9a1d400
Ariel Vallese <alariva@gmail.com>
2015-12-04 21:51:20 +0100

    Reindent

--- resources/views/user/contacts/edit.blade.php
+++ resources/views/user/contacts/edit.blade.php
@@ -3,18 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ trans('user.contacts.create.title') }}</div>
+        <div class="panel panel-default">
 
-                <div class="panel-body">
-                    @include('_errors')
+            <div class="panel-heading">{{ trans('user.contacts.create.title') }}</div>
 
-                    {!! Form::model($contact, ['method' => 'put', 'route' => ['user.business.contact.update', $business->id, $contact->id ]]) !!}
-                        @include('user.contacts._form', ['submitLabel' => trans('user.contacts.btn.update')])
-                    {!! Form::close() !!}
-                </div>
+            <div class="panel-body">
+                @include('_errors')
 
+                {!! Form::model($contact, ['method' => 'put', 'route' => ['user.business.contact.update', $business->id, $contact->id ]]) !!}
+                    @include('user.contacts._form', ['submitLabel' => trans('user.contacts.btn.update')])
+                {!! Form::close() !!}
             </div>
+
+        </div>
     </div>
 </div>
 @endsection
