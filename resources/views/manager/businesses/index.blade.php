fa9b4c971c216c398f80a7352f1e9b1d2bddc5d6
Ariel Vallese <alariva@gmail.com>
2015-12-05 14:36:02 +0100

    Fix horizontal-scrolling issue on business listing

--- resources/views/manager/businesses/index.blade.php
+++ resources/views/manager/businesses/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+
         <div class="col-md-10 col-md-offset-1">
 
             <div class="panel panel-default">
@@ -35,7 +35,7 @@
                 </div>
             </div>
         </div>
-    </div>
+
 </div>
 @endsection
 
--- resources/views/user/businesses/index.blade.php
+++ resources/views/user/businesses/index.blade.php
@@ -2,35 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
+    <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
 
-                <div class="panel-heading">
-                    {{ trans('user.businesses.index.title') }}
-                </div>
-
-                <div class="panel-body">
-                    @if (!$businesses->isEmpty())
-                        @foreach ($businesses as $business)
-                        <div class="row">
-                            @include('user.businesses._row', $business)
-                        </div>
-                        @endforeach
-                    @else
-                        {{ trans('user.businesses.list.no_businesses') }}
-                    @endif
-                </div>
-
-                <div class="panel-footer">
-                    @if(auth()->user()->hasBusiness())
-                        {!! Button::normal(trans('user.businesses.index.btn.manage'))->asLinkTo( route('manager.business.index') ) !!}
-                    @else
-                        {!! Button::primary(trans('user.businesses.index.btn.create'))->asLinkTo( route('manager.business.register') ) !!}
-                    @endif
-                </div>
-
+            <div class="panel-heading">
+                {{ trans('user.businesses.index.title') }}
             </div>
+
+            <div class="panel-body">
+                @if (!$businesses->isEmpty())
+                    @foreach ($businesses as $business)
+                        @include('user.businesses._row', $business)
+                    @endforeach
+                @else
+                    {{ trans('user.businesses.list.no_businesses') }}
+                @endif
+            </div>
+
+            <div class="panel-footer">
+                @if(auth()->user()->hasBusiness())
+                    {!! Button::normal(trans('user.businesses.index.btn.manage'))->asLinkTo( route('manager.business.index') ) !!}
+                @else
+                    {!! Button::primary(trans('user.businesses.index.btn.create'))->asLinkTo( route('manager.business.register') ) !!}
+                @endif
+            </div>
+
         </div>
     </div>
 </div>
