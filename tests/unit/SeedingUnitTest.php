174cc3541c3cba4cb269e2dfae17341064354744
Ariel Vallese <alariva@gmail.com>
2015-12-05 16:29:56 +0100

    Add test

---
+++ tests/unit/SeedingUnitTest.php
@@ -0,0 +1,22 @@
+
+<?php
+
+# use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class SeedingUnitTest extends TestCase
+{
+    use DatabaseTransactions;
+
+   /**
+     * @test
+     */
+    public function it_creates_a_demo_scenario()
+    {
+        $this->seed('TestingDatabaseSeeder');
+
+        $this->seeInDatabase('users', ['email' => 'demo@timegrid.io']);
+        $this->seeInDatabase('users', ['email' => 'guest@example.org']);
+    }
+}
