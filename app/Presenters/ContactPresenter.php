50b92b6985aff38521b2f27eb02b68a46574863f
Ariel Vallese <alariva@gmail.com>
2015-12-06 15:26:22 +0100

    Implement Contact Model using ContactPresenter

--- app/Presenters/ContactPresenter.php
+++ app/Presenters/ContactPresenter.php
@@ -11,4 +11,59 @@ class ContactPresenter extends BasePresenter
     {
         $this->wrappedObject = $resource;
     }
+
+    /**
+     * get fullname
+     *
+     * @return string Contact firstname and lastname
+     */
+    public function fullname()
+    {
+        return trim($this->wrappedObject->firstname . ' ' . $this->wrappedObject->lastname);
+    }
+
+    /**
+     * TODO: Check if needs to get moved to a calculator class
+     *
+     * get Quality
+     *
+     * @return float Contact quality percentual score calculated from profile completion
+     */
+    public function quality()
+    {
+        $quality  = 0;
+        $quality += isset($this->wrappedObject->firstname) ? 1 : 0;
+        $quality += isset($this->wrappedObject->lastname) ? 1 : 0;
+        $quality += isset($this->wrappedObject->nin) ? 5 : 0;
+        $quality += isset($this->wrappedObject->birthdate) ? 2 : 0;
+        $quality += isset($this->wrappedObject->mobile) ? 4 : 0;
+        $quality += isset($this->wrappedObject->email) ? 4 : 0;
+        $quality += isset($this->wrappedObject->postal_address) ? 3 : 0;
+        $total    = 20;
+        return $quality/$total*100;
+    }
+
+    /**
+     * ToDo: Use Carbon instead of DateTime
+     *
+     * get Age
+     *
+     * @return int Age in years
+     */
+    public function age()
+    {
+        if ($this->wrappedObject->birthdate == null) {
+            return null;
+        }
+        
+        $reference = new \DateTime;
+        $born = new \DateTime($this->wrappedObject->birthdate);
+
+        if ($this->wrappedObject->birthdate > $reference) {
+            return null;
+        }
+
+        $diff = $reference->diff($born);
+        return $diff->y;
+    }
 }
