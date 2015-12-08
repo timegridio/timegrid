5e246b78f8d60891ffe352b1f605e715891d5200
Ariel Vallese <alariva@gmail.com>
2015-12-08 11:00:13 +0100

    Remove else

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -39,11 +39,9 @@ class Authenticate
         if ($this->auth->guest()) {
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
-            } else {
-                return redirect()->guest('auth/login');
             }
+            return redirect()->guest('auth/login');
         }
-
         return $next($request);
     }
 }
