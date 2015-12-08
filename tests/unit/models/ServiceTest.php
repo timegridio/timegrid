2b886a3046b1c093a5794b61b944a777cf672b2d
Ariel Vallese <alariva@gmail.com>
2015-12-08 21:08:20 +0100

    Add test

---
+++ tests/unit/models/ServiceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Business;
+use App\Models\Service;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ServiceTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected $service;
+
+    /**
+     * @covers  App\Models\Service::slug
+     * @test
+     */
+    public function it_scopes_by_slug()
+    {
+        $this->business = factory(Business::class)->create();
+        $this->service = factory(Service::class)->make();
+
+        $this->business->services()->save($this->service);
+
+        $services = Service::slug($this->service->slug);
+        $count = $services->count();
+        $service = $services->first();
+
+        /* Perform Test */
+        $this->assertInstanceOf(Service::class, $service);
+        $this->assertEquals($count, 1);
+    }
+}
