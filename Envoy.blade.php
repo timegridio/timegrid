1efdebe5af597b5773ddd276c5b67c1d4f5e26be
Ariel Vallese <alariva@gmail.com>
2015-12-10 23:40:17 +0100

    Update deploy process and cleanup

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,24 +1,5 @@
-@servers(['demo' => 'timegrid'])
+@servers(['moongate' => 'timegrid'])
 
-@task('check', ['on' => 'demo'])
-    cd /var/www/timegrid.io/dev/htdocs
-    phpunit
-@endtask
-
-@task('deploy', ['on' => 'demo'])
-    sudo /usr/local/bin/deploy.sh development
-@endtask
-
-@task('seed', ['on' => 'demo'])
-    {{-- Target the project directory --}}
-    cd /var/www/timegrid.io/demo/htdocs
-    {{-- If there is anything to migrate, migrate it --}}
-    php artisan db:seed --class="CategoriesSeeder"
-@endtask
-
-@task('refresh', ['on' => 'demo'])
-    {{-- Target the project directory --}}
-    cd /var/www/timegrid.io/dev/htdocs    
-    {{-- If there is anything to migrate, migrate it --}}
-    php artisan migrate:refresh --seed
+@task('deploy', ['on' => 'moongate'])
+    sudo /usr/local/bin/deploy.sh {{ $environment }}
 @endtask
