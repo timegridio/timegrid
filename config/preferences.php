b0f9d4b678ba865d5c63c3299fba2426037cee5b
Ariel Vallese <alariva@gmail.com>
2015-12-16 19:32:11 +0100

    Add default step for setting (per hour)

--- config/preferences.php
+++ config/preferences.php
@@ -7,9 +7,10 @@ return [
         'show_map'                      => ['type' => 'bool', 'value' => false],
         'show_postal_address'           => ['type' => 'bool', 'value' => false],
         'show_phone'                    => ['type' => 'bool', 'value' => false],
-        'appointment_annulation_pre_hs' => ['type' => 'int', 'value' => '48', 'icon' => 'hourglass'],
+        'appointment_annulation_pre_hs' => ['type' => 'int', 'value' => '48', 'step' => 1, 'icon' => 'hourglass'],
         'appointment_take_today'        => ['type' => 'bool', 'value' => false],
         'appointment_code_length'       => ['type' => 'int', 'value' => 4, 'icon' => 'barcode'],
         'service_default_duration'      => ['type' => 'int', 'value' => 30, 'step' => 5, 'icon' => 'hourglass'],
+        'annulation_policy_advice'      => ['type' => 'string', 'value' => ''],
     ],
 ];
