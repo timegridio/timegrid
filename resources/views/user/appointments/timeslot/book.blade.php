9cbda51256fcacf59840a1141be67965f0e09439
Ariel Vallese <alariva@gmail.com>
2015-12-16 20:07:45 +0100

    Enhance form layout

--- resources/views/user/appointments/timeslot/book.blade.php
+++ resources/views/user/appointments/timeslot/book.blade.php
@@ -7,56 +7,51 @@
 
 @section('content')
 {!! Form::open(['route' => 'user.booking.store', 'class' => 'form']) !!}
-{!! Form::hidden('businessId', $business->id, ['required', 'id' => 'business'] ) !!}
-{!! Form::hidden('_date', null, array('required', 'class'=>'form-control', 'id'=>'date', 'min'=> date('Y-m-d') )) !!}
+{!! Form::hidden('businessId', $business->id, ['required', 'id' => 'business']) !!}
+{!! Form::hidden('_date', null, ['required', 'id'=>'date', 'min'=> date('Y-m-d')]) !!}
+{!! Form::hidden('service_id', null, ['required', 'id'=>'service']) !!}
 
 <div class="container">
+
     <div class="row">
         @include('user.appointments.timeslot._timetable', ['dates' => $availability])
     </div>
 
     <div class="row">
-
-        <div class="form-group col-sm-6">
-            {!! Form::label( trans('user.appointments.form.time.label') ) !!}
+        <div class="form-group col-sm-5">
+            {!! Form::label(trans('user.appointments.form.time.label')) !!}
             <br/>
-            <select id="times" name="_time"></select>
+            <select id="times" name="_time" class="form-control"></select>
         </div>
 
-        <div class="form-group col-sm-6">
-            {!! Form::label( trans('user.appointments.form.duration.label') ) !!}
-            {!! Form::text('duration', 0,
-                array('readonly',
+        <div class="form-group col-sm-7">
+            {!! Form::label(trans('user.appointments.form.duration.label')) !!}
+            {!! Form::text('duration', null, [
+                      'readonly',
                       'id'=>'duration',
                       'class'=>'form-control',
-                      'placeholder'=> trans('user.appointments.form.duration.label') )) !!}
+                      'placeholder'=> trans('user.appointments.form.duration.label')
+            ]) !!}
         </div>
     </div>
 
     <div class="row">
         <div class="form-group col-sm-12">
-            {!! Form::hidden('service_id', 0,
-                array('required',
-                      'id'=>'service',
+            {!! Form::label(trans('user.appointments.form.comments.label')) !!}
+            {!! Form::text('comments', null, [
+                      'required',
                       'class'=>'form-control',
-                      'placeholder'=> trans('user.appointments.form.service.label') )) !!}
+                      'placeholder'=> trans('user.appointments.form.comments.label')
+            ]) !!}
         </div>
     </div>
 
-    <div class="row">
-        <div class="form-group col-sm-12">
-            {!! Form::label( trans('user.appointments.form.comments.label') ) !!}
-            {!! Form::text('comments', 'test',
-                array('required',
-                      'class'=>'form-control',
-                      'placeholder'=> trans('user.appointments.form.comments.label') )) !!}
-        </div>
-    </div>
     <div class="row">
         <div class="form-group col-sm-12">
             {!! Button::primary(trans('manager.contacts.btn.store'))->block()->submit() !!}
         </div>
     </div>
+
 </div>
 {!! Form::close() !!}
 @endsection
