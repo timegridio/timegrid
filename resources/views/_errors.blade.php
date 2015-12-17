dd2f03c418ba0f7ecda80c6a149ad7ef09f26ee2
Ariel Vallese <alariva@gmail.com>
2015-12-17 19:59:23 +0100

    Reindent

--- resources/views/_errors.blade.php
+++ resources/views/_errors.blade.php
@@ -5,13 +5,13 @@
 --}}
 
 @if($errors->has())
-    <div class="alert alert-danger">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+<div class="alert alert-danger">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
 @endif
 {{-- Validation Error Messages --}}
