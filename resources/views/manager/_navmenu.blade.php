a714ce1c29f0e2d760a6ec040dda6b3d0ff536d4
Ariel Vallese <alariva@gmail.com>
2015-12-19 21:02:04 +0100

    Change navigation items not to use button type

--- resources/views/manager/_navmenu.blade.php
+++ resources/views/manager/_navmenu.blade.php
@@ -1,11 +1,24 @@
 @if(($business = Session::get('selected.business')) && auth()->user()->isOwner($business))
 
-    <li>{!! Button::withIcon(Icon::calendar())->withAttributes(['id' => 'btnAgenda', 'title' => trans('manager.business.btn.tooltip.agenda')])->normal()->asLinkTo( route('manager.business.agenda.index', $business) ) !!}</li>
-    <li>{!! Button::withIcon(Icon::user())->withAttributes(['id' => 'btnContacts', 'title' => trans('manager.business.btn.tooltip.contacts')])->normal()->asLinkTo( route('manager.business.contact.index', $business) ) !!}</li>
-    <li>{!! Button::withIcon(Icon::time())->normal()->withAttributes(['id' => 'btnVacancies', 'title' => trans('manager.business.btn.tooltip.vacancies')])->asLinkTo( route('manager.business.vacancy.create', $business) ) !!}</li>
-    <li>{!! Button::withIcon(Icon::tag())->normal()->withAttributes(['id' => 'btnServices', 'title' => trans('manager.business.btn.tooltip.services')])->asLinkTo( route('manager.business.service.index', $business) ) !!}</li>
+    <li id="btnAgenda" title="{{ trans('manager.business.btn.tooltip.agenda') }}" >
+        <a href="{{ route('manager.business.agenda.index', $business) }}">{!! Icon::calendar() !!}</a>
+    </li>
 
-    <li id="navHome"><a href="{{ route('manager.business.show', $business->slug) }}">{!! Icon::home() !!} {{ $business->name }}</a></li>
+    <li id="btnContacts" title="{{ trans('manager.business.btn.tooltip.contacts') }}" >
+        <a href="{{ route('manager.business.contact.index', $business) }}">{!! Icon::user() !!}</a>
+    </li>
+
+    <li id="btnVacancies" title="{{ trans('manager.business.btn.tooltip.vacancies') }}" >
+        <a href="{{ route('manager.business.vacancy.create', $business) }}">{!! Icon::time() !!}</a>
+    </li>
+
+    <li id="btnServices" title="{{ trans('manager.business.btn.tooltip.services') }}" >
+        <a href="{{ route('manager.business.service.index', $business) }}">{!! Icon::tag() !!}</a>
+    </li>
+
+    <li id="navHome">
+        <a href="{{ route('manager.business.show', $business->slug) }}">{!! Icon::home() !!} {{ $business->name }}</a>
+    </li>
 
     {!! Form::open(['method' => 'post', 'url' => route('manager.search', $business), 'class' => 'navbar-form navbar-left', 'role' => 'search']) !!}
     <div class="form-group">
