74dd1070d1774969abe78571e73d280c5c29df9a
Ariel Vallese <alariva@gmail.com>
2015-12-22 21:45:55 +0100

    Update assets load method

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" />
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
         <link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
         <style type="text/css">
