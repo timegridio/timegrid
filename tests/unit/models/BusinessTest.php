b02586502f4570adf76cf7dbc8c0b1b2ba55ad0b
Ariel Vallese <alariva@gmail.com>
2015-12-22 09:08:10 +0100

    Tidy up tests and enhance coverage

--- tests/unit/models/BusinessTest.php
+++ tests/unit/models/BusinessTest.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Models\Business;
+use App\Models\User;
 use App\Presenters\BusinessPresenter;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class BusinessTest extends TestCase
@@ -9,22 +11,22 @@ class BusinessTest extends TestCase
     use DatabaseTransactions;
 
     /**
-     * @covers            \App\Models\Business::__construct
+     * @covers \App\Models\Business::__construct
      * @test
      */
-    public function test_it_creates_a_business()
+    public function it_creates_a_business()
     {
         $business = factory(Business::class)->create();
 
-        return $business;
+        $this->assertInstanceOf(Business::class, $business);
     }
 
     /**
-     * @covers            \App\Models\Business::__construct
-     * @covers            \App\Models\Business::save
+     * @covers \App\Models\Business::__construct
+     * @covers \App\Models\Business::save
      * @test
      */
-    public function test_it_creates_a_business_that_appears_in_db()
+    public function it_creates_a_business_that_appears_in_db()
     {
         $business = factory(Business::class)->create();
 
@@ -32,12 +34,12 @@ class BusinessTest extends TestCase
     }
 
     /**
-     * @covers            \App\Models\Business::__construct
-     * @covers            \App\Models\Business::setSlugAttribute
-     * @covers            \App\Models\Business::save
+     * @covers \App\Models\Business::__construct
+     * @covers \App\Models\Business::setSlugAttribute
+     * @covers \App\Models\Business::save
      * @test@
      */
-    public function test_it_generates_slug_from_name()
+    public function it_generates_slug_from_name()
     {
         $business = factory(Business::class)->create();
 
@@ -47,10 +49,10 @@ class BusinessTest extends TestCase
     }
 
     /**
-     * @covers            \App\Models\Business::getPresenterClass
+     * @covers \App\Models\Business::getPresenterClass
      * @test
      */
-    public function test_gets_business_presenter()
+    public function it_gets_business_presenter()
     {
         $business = factory(Business::class)->create();
 
@@ -60,10 +62,10 @@ class BusinessTest extends TestCase
     }
 
     /**
-     * @covers            \App\Models\Business::setPhoneAttribute
+     * @covers \App\Models\Business::setPhoneAttribute
      * @test
      */
-    public function test_sets_empty_phone_attribute()
+    public function it_sets_empty_phone_attribute()
     {
         $business = factory(Business::class)->make(['phone' => '']);
 
@@ -71,13 +73,46 @@ class BusinessTest extends TestCase
     }
 
     /**
-     * @covers            \App\Models\Business::setPostalAddressAttribute
+     * @covers \App\Models\Business::setPostalAddressAttribute
      * @test
      */
-    public function test_sets_empty_postal_address_attribute()
+    public function it_sets_empty_postal_address_attribute()
     {
         $business = factory(Business::class)->make(['postal_address' => '']);
 
         $this->assertNull($business->postal_address);
+    }
+
+    /**
+     * @covers \App\Models\Business::owner
+     * @test
+     */
+    public function it_gets_the_business_owner()
+    {
+        $owner = factory(User::class)->create();
+        
+        $business = factory(Business::class)->create();
+        $business->owners()->save($owner);
+
+        $this->assertInstanceOf(User::class, $business->owner());
+        $this->assertEquals($owner->name, $business->owner()->name);
+    }
+
+    /**
+     * @covers \App\Models\Business::owners
+     * @test
+     */
+    public function it_gets_the_business_owners()
+    {
+        $owner1 = factory(User::class)->create();
+        $owner2 = factory(User::class)->create();
+        
+        $business = factory(Business::class)->create();
+        
+        $business->owners()->save($owner1);
+        $business->owners()->save($owner2);
+
+        $this->assertInstanceOf(Collection::class, $business->owners);
+        $this->assertCount(2, $business->owners);
     }
 }
