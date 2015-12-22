5af61b27e7210ce727fa335daf783e3433e181ba
Ariel Vallese <alariva@gmail.com>
2015-12-22 10:45:22 +0100

    Increment test coverage

--- tests/unit/models/ContactTest.php
+++ tests/unit/models/ContactTest.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Appointment;
+use App\Models\Business;
 use App\Models\Contact;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ContactTest extends TestCase
@@ -12,7 +15,7 @@ class ContactTest extends TestCase
      * @covers App\Models\Contact::autoLinkToUser
      * @test
      */
-    public function test_it_links_to_existing_user()
+    public function it_links_to_existing_user()
     {
         $user = $this->createUser(['email' => 'guest@example.org', 'password' => bcrypt('demoguest')]);
 
@@ -27,7 +30,7 @@ class ContactTest extends TestCase
      * @covers App\Models\Contact::autoLinkToUser
      * @test
      */
-    public function test_it_links_another_to_existing_user()
+    public function it_links_another_to_existing_user()
     {
         $user = $this->createUser(['email' => 'guest@example.org', 'password' => bcrypt('demoguest')]);
 
@@ -50,7 +53,7 @@ class ContactTest extends TestCase
      * @covers App\Models\Contact::autoLinkToUser
      * @test
      */
-    public function test_it_unlinks_removed_user()
+    public function it_unlinks_removed_user()
     {
         $user = $this->createUser(['email' => 'guest@example.org', 'password' => bcrypt('demoguest')]);
 
@@ -73,6 +76,87 @@ class ContactTest extends TestCase
         $contactSecond->autoLinkToUser();
 
         $this->assertEquals(0, $user->contacts()->count());
+    }
+
+    /**
+     * @covers App\Models\Contact::appointments
+     * @test
+     */
+    public function it_gets_the_appointments()
+    {
+        $business = factory(Business::class)->create();
+
+        $contact = $this->createContact();
+
+        $appointment1 = factory(Appointment::class)->create([
+            'business_id' => $business->id,
+            'contact_id' => $contact->id,
+            'start_at' => Carbon::now()->addDays(1),
+            ]);
+        $appointment2 = factory(Appointment::class)->create([
+            'business_id' => $business->id,
+            'contact_id' => $contact->id,
+            'start_at' => Carbon::now()->addDays(2),
+            ]);
+
+        $this->assertCount(2, $contact->appointments);
+    }
+
+    /**
+     * @covers App\Models\Contact::getEmailAttribute
+     * @test
+     */
+    public function it_gets_the_native_contact_email()
+    {
+        $business = factory(Business::class)->create();
+
+        $email = 'test@example.org';
+
+        $contact = $this->createContact(['email' => $email]);
+
+        $this->assertEquals($email, $contact->email);
+    }
+
+    /**
+     * @covers App\Models\Contact::getEmailAttribute
+     * @test
+     */
+    public function it_gets_the_native_contact_email_and_not_the_user_fallback()
+    {
+        $business = factory(Business::class)->create();
+
+        $userFallbackEmail = 'tosto@example.org';
+        $contactEmail = 'test@example.org';
+
+        $user = $this->createUser(['email' => $userFallbackEmail]);
+
+        $contact = $this->createContact([
+            'email' => $contactEmail,
+            'user_id' => $user->id
+            ]);
+
+        $this->assertEquals($contactEmail, $contact->email);
+    }
+
+    /**
+     * @covers App\Models\Contact::getEmailAttribute
+     * @test
+     */
+    public function it_gets_the_user_fallback_contact_email()
+    {
+        $business = factory(Business::class)->create();
+
+        $userFallbackEmail = 'tosto@example.org';
+        $contactEmail = 'test@example.org';
+
+        $user = $this->createUser(['email' => $userFallbackEmail]);
+
+        $contact = $this->createContact([
+            'email' => null,
+            'user_id' => $user->id
+            ]);
+
+        $this->assertEquals($userFallbackEmail, $contact->email);
     }
 
     /////////////
