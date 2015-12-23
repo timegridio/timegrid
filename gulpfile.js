197317ac5cc0ec7bafca74dccfe2b1577c7b8e67
Ariel Vallese <alariva@gmail.com>
2015-12-23 12:31:09 +0100

    Reorganize assets in purpose groups
    Not all the assets are used together among all the app. In most of the
    cases they are required as groups. Say a form view is loaded, then
    those plugins providing form and input facilities need to be loaded,
    and some others groups which are not needed may be avoided.

--- gulpfile.js
+++ gulpfile.js
@@ -14,20 +14,42 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.less('app.less');
 
+    // Base Scripts and Styles
+
     mix.scripts([
         './bower_components/jquery/dist/jquery.min.js',
         './bower_components/bootstrap/dist/js/bootstrap.min.js',
         './bower_components/tooltipster/js/jquery.tooltipster.min.js'
     ], 'public/js/app.js');
 
-    mix.copy([
-        './bower_components/bootstrap-select/dist/js/i18n/*.min.js',
-    ], 'public/js/bootstrap-select/i18n');
+    mix.styles([
+        './bower_components/tooltipster/css/themes/tooltipster-light.css',
+        './bower_components/tooltipster/css/tooltipster.css',
+        './bower_components/animate.css/animate.min.css',
+    ], 'public/css/styles.css');
+
+    // News Helpers
+
+    mix.scripts([
+        './bower_components/jquery-bootstrap-newsbox/dist/jquery.bootstrap.newsbox.min.js',
+    ], 'public/js/newsbox.js');
+
+    // Form Helpers
 
     mix.scripts([
         './bower_components/bootstrap-select/dist/js/bootstrap-select.min.js',
         './bower_components/bootstrap-validator/dist/validator.min.js',
     ], 'public/js/forms.js');
+
+    mix.copy([
+        './bower_components/bootstrap-select/dist/js/i18n/*.min.js',
+    ], 'public/js/bootstrap-select/i18n');
+
+    mix.styles([
+        './bower_components/bootstrap-select/dist/css/bootstrap-select.min.css',
+    ], 'public/css/forms.css');
+
+    // Date & Time Helpers
 
     mix.scripts([
         './bower_components/moment/min/moment-with-locales.min.js',
@@ -39,21 +61,7 @@ elixir(function(mix) {
         './bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
     ], 'public/css/datetime.css');
 
-    mix.scripts([
-        './bower_components/jquery-bootstrap-newsbox/dist/jquery.bootstrap.newsbox.min.js',
-    ], 'public/js/newsbox.js');
-
-    mix.styles([
-        './bower_components/tooltipster/css/themes/tooltipster-light.css',
-        './bower_components/tooltipster/css/tooltipster.css',
-        './bower_components/animate.css/animate.min.css',
-    ], 'public/css/styles.css');
-
-    mix.styles([
-        './bower_components/bootstrap-select/dist/css/bootstrap-select.min.css',
-    ], 'public/css/forms.css');
-
-    // Help & Tour
+    // Tour Helpers
 
     mix.scripts([
         './bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js',
