9586eab81c6eed2b888d706f65f131166e24c6df
Ariel Vallese <alariva@gmail.com>
2015-12-25 20:07:28 +0100

    Add Vacancy Parser class for batch vacancy updates

---
+++ app/Services/VacancyParserService.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Service;
+use App\Models\Vacancy;
+use Carbon\Carbon;
+
+class VacancyParserService
+{
+    const REGEX_PATTERN_VACANCY = '/(?P<services>.*)\n\ (?P<days>.*)\n\ \ (?<hours>.*)/i';
+
+    /////////////////////
+    // VACANCY PARSING //
+    /////////////////////
+
+    public function readVacancies($vacancyString)
+    {
+        preg_match_all(self::REGEX_PATTERN_VACANCY, $vacancyString, $matches, PREG_SET_ORDER);
+
+        return $matches;
+    }
+
+    public function buildVacancies(array $vacancyParameters)
+    {
+        $services = $this->services($vacancyParameters['services']);
+        $days = $this->dates($vacancyParameters['days']);
+        $hourRanges = $this->hours($vacancyParameters['hours']);
+
+        $ret = [];
+        foreach ($services as $service) {
+            foreach ($days as $day) {
+                foreach ($hourRanges as $hourRange) {
+                    $ret[] = [
+                        'serivce' => $service['slug'],
+                        'date' => $day,
+                        'startAt' => $hourRange['startAt'],
+                        'finishAt' => $hourRange['finishAt'],
+                        'capacity' => $service['capacity'],
+                    ];
+                }
+            }
+        }
+        return $ret;
+    }
+
+    /////////////////////
+    // SERVICE PARSING //
+    /////////////////////
+
+    public function services($services)
+    {
+        $services = $this->splitServices($services);
+
+        return $this->getServicesCapacity($services);
+    }
+
+    public function splitServices($services)
+    {
+        return preg_split('/\ *\,\ */', $services);
+    }
+
+    public function getServicesCapacity(array $services)
+    {
+        $converted = [];
+        foreach ($services as $service) {
+            $converted[] = $this->getServiceCapacity($service);
+        }
+        return $converted;
+    }
+
+    public function getServiceCapacity($service)
+    {
+        $capacity = 1;
+        if (strpos($service, ':') !== false) {
+            list($service, $capacity) = explode(':', $service);
+        }
+        return ['slug' => $service, 'capacity' => $capacity];
+    }
+
+    //////////////////
+    // DATE PARSING //
+    //////////////////
+
+    public function dates($days)
+    {
+        $days = $this->splitDates($days);
+
+        return $this->convertDaysToDate($days);
+    }
+
+    public function splitDates($days)
+    {
+        return preg_split('/\ *\,\ */', $days);
+    }
+
+    public function convertDaysToDate(array $days)
+    {
+        $converted = [];
+        foreach ($days as $day) {
+            $converted[] = $this->dayToDate($day);
+        }
+        return $converted;
+    }
+
+    public function dayToDate($day)
+    {
+        return date('Y-m-d', strtotime($day));
+    }
+
+    //////////////////
+    // TIME PARSING //
+    //////////////////
+
+    public function hours($string)
+    {
+        $ranges = $this->splitRanges($string);
+
+        return $this->normalizeRanges($ranges);
+    }
+
+    public function normalizeRanges($ranges)
+    {
+        $normalizedRanges = [];
+
+        foreach ($ranges as $range) {
+            list($startAt, $finishAt) = preg_split('/\ *\-\ */', $range);
+            $normalizedRanges[] = [
+                'startAt'  => $this->milTimeToStandard($startAt),
+                'finishAt' => $this->milTimeToStandard($finishAt)
+            ];
+        }
+        return $normalizedRanges;
+    }
+
+    public function splitRanges($string)
+    {
+        return preg_split('/,\ */', $string);
+    }
+
+    public function milTimeToStandard($militaryTime)
+    {
+        if (trim($militaryTime) == '') {
+            return '';
+        }
+
+        if (strpos($militaryTime, ':') !== false) {
+            return $militaryTime;
+        }
+
+        $parts = [];
+
+        if (strlen($militaryTime) <= 2) {
+            $parts[] = intval($militaryTime);
+            $parts[] = '00';
+        }
+        if (strlen($militaryTime) == 3) {
+            $parts[0] = substr($militaryTime, 0, 1);
+            $parts[1] = substr($militaryTime, 1, 2);
+        }
+        if (strlen($militaryTime) == 4) {
+            $parts[0] = substr($militaryTime, 0, 2);
+            $parts[1] = substr($militaryTime, 2, 2);
+        }
+        
+        return implode(':', $parts);
+    }
+}
---
+++ tests/unit/services/VacancyParserTest.php
@@ -0,0 +1,246 @@
+<?php
+
+use App\Models\Business;
+use App\Models\Service;
+use App\Models\Vacancy;
+use App\Services\VacancyParserService;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class VacancyParserTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $vacancyParser;
+
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->vacancyParser = new VacancyParserService;
+
+        $this->setFixture();
+    }
+
+    protected function setfixture()
+    {
+        $business = factory(Business::class)->create();
+
+        $service1 = factory(Service::class)->create([
+            'business_id' => $business->id,
+            'name' => 'Massage',
+            'slug' => 'massage',
+            'duration' => 30
+        ]);
+        $service2 = factory(Service::class)->create([
+            'business_id' => $business->id,
+            'name' => 'Relax',
+            'slug' => 'relax',
+            'duration' => 30
+        ]);
+    }
+
+    /////////////////////
+    // VACANCY PARSING //
+    /////////////////////
+
+    /**
+     * @test
+     */
+    public function it_parses_vacancies()
+    {
+        $vacancyString = <<<EOD
+massage:1
+ mon, tue
+  8 - 12, 14 - 20
+
+massage:2
+ mon, tue, fri
+  9 - 14
+EOD;
+
+        $vacancies = $this->vacancyParser->readVacancies($vacancyString);
+
+        foreach ($vacancies as $vacancyString) {
+            $vacancies = $this->vacancyParser->buildVacancies($vacancyString);
+        }
+
+        $array = [
+            [
+                "serivce" => "massage",
+                "date" => "2015-12-28",
+                "startAt" => "9:00",
+                "finishAt" => "14:00",
+                "capacity" => "2",
+            ],
+            [
+                "serivce" => "massage",
+                "date" => "2015-12-29",
+                "startAt" => "9:00",
+                "finishAt" => "14:00",
+                "capacity" => "2",
+            ],
+            [
+                "serivce" => "massage",
+                "date" => "2015-12-25",
+                "startAt" => "9:00",
+                "finishAt" => "14:00",
+                "capacity" => "2",
+            ]
+        ];
+
+        $this->assertEquals($vacancies, $array);
+    }
+
+    /////////////////////
+    // SERVICE PARSING //
+    /////////////////////
+
+    /**
+     * @test
+     */
+    public function it_splits_services()
+    {
+        $services = $this->vacancyParser->splitServices('massage:1, relax:2, stones, massage-other:1');
+
+        $array = [
+            'massage:1',
+            'relax:2',
+            'stones',
+            'massage-other:1',
+        ];
+
+        $this->assertEquals($services, $array);
+    }
+
+    /**
+     * @test
+     */
+    public function it_splits_service_capacity()
+    {
+        $services = $this->vacancyParser->services('massage:1, relax:2, stones, massage-other:1');
+
+        $array = [
+            ['slug' => 'massage', 'capacity' => 1],
+            ['slug' => 'relax', 'capacity' => 2],
+            ['slug' => 'stones', 'capacity' => 1],
+            ['slug' => 'massage-other', 'capacity' => 1],
+        ];
+
+        $this->assertEquals($services, $array);
+    }
+
+    //////////////////
+    // DATE PARSING //
+    //////////////////
+
+    /**
+     * @test
+     */
+    public function it_splits_dates()
+    {
+        $dates = $this->vacancyParser->splitDates('mon,tue,wed,thu,fri,sat,sun');
+
+        $array = [ 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun' ];
+
+        $this->assertEquals($dates, $array);
+    }
+
+    /**
+     * @test
+     */
+    public function it_splits_and_convert_dates()
+    {
+        $dates = $this->vacancyParser->dates('next mon, next tue, next wed, next thu, next fri, next sat, next sun');
+
+        $array = [
+            date('Y-m-d', strtotime('next mon')),
+            date('Y-m-d', strtotime('next tue')),
+            date('Y-m-d', strtotime('next wed')),
+            date('Y-m-d', strtotime('next thu')),
+            date('Y-m-d', strtotime('next fri')),
+            date('Y-m-d', strtotime('next sat')),
+            date('Y-m-d', strtotime('next sun')),
+        ];
+
+        $this->assertEquals($dates, $array);
+    }
+
+    /**
+     * @test
+     */
+    public function it_splits_and_convert_human_dates()
+    {
+        $dates = $this->vacancyParser->dates('today, tomorrow, 2015-11-10, 2015-11-10 + 1 day');
+
+        $array = [
+            date('Y-m-d', strtotime('today')),
+            date('Y-m-d', strtotime('tomorrow')),
+            date('Y-m-d', strtotime('2015-11-10')),
+            date('Y-m-d', strtotime('2015-11-11')),
+        ];
+
+        $this->assertEquals($dates, $array);
+    }
+
+    //////////////////
+    // TIME PARSING //
+    //////////////////
+
+    /**
+     * @test
+     */
+    public function it_splits_the_elements_by_date_timerange_service()
+    {
+        $hours = $this->vacancyParser->hours('830-1130,14-20,2215-2330,2330-2345');
+        
+        $array = [
+            ['startAt' =>  '8:30', 'finishAt' => '11:30'],
+            ['startAt' => '14:00', 'finishAt' => '20:00'],
+            ['startAt' => '22:15', 'finishAt' => '23:30'],
+            ['startAt' => '23:30', 'finishAt' => '23:45'],
+        ];
+
+        $this->assertEquals($hours, $array);
+    }
+
+    /**
+     * @test
+     */
+    public function it_splits_the_elements_by_date_timerange_service_allowing_spaces()
+    {
+        $hours = $this->vacancyParser->hours('830 - 1130, 14 - 20, 2215 - 2330, 2330 - 2345');
+        
+        $array = [
+            ['startAt' =>  '8:30', 'finishAt' => '11:30'],
+            ['startAt' => '14:00', 'finishAt' => '20:00'],
+            ['startAt' => '22:15', 'finishAt' => '23:30'],
+            ['startAt' => '23:30', 'finishAt' => '23:45'],
+        ];
+
+        $this->assertEquals($hours, $array);
+    }
+
+    /**
+     * @test
+     */
+    public function it_converts_military_time_to_standard()
+    {
+        $time = $this->vacancyParser->milTimeToStandard('830');
+
+        $returns = '8:30';
+
+        $this->assertEquals($time, $returns);
+    }
+
+    /**
+     * @test
+     */
+    public function it_keeps_standard_time_into_standard()
+    {
+        $time = $this->vacancyParser->milTimeToStandard('8:30');
+
+        $returns = '8:30';
+
+        $this->assertEquals($time, $returns);
+    }
+}
