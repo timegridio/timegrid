6644972beb4c64ddf35cf4955cc642ea3629ad51
Ariel Vallese <alariva@gmail.com>
2016-01-02 20:00:43 +0100

    Increment test coverage

--- tests/unit/controllers/Manager/ManagerBusinessServiceControllerTest.php
+++ tests/unit/controllers/Manager/ManagerBusinessServiceControllerTest.php
@@ -57,6 +57,99 @@ class ManagerBusinessServiceControllerTest extends TestCase
         $this->assertCount(1, $this->business->fresh()->services);
     }
 
+    /**
+     * @covers   App\Http\Controllers\Manager\BusinessServiceController::show
+     * @test
+     */
+    public function it_shows_the_business_service_page_for_management()
+    {
+        $this->arrangeBusinessWithOwner();
+
+        $service = $this->makeService();
+
+        $this->business->services()->save($service);
+
+        $this->actingAs($this->owner);
+
+        $this->visit(route('manager.business.service.show', ['business' => $this->business, 'service' => $service]));
+
+        $this->assertResponseOk();
+        $this->see($service->name);
+        $this->see($service->slug);
+        $this->see($service->description);
+    }
+
+    /**
+     * @covers   App\Http\Controllers\Manager\BusinessServiceController::edit
+     * @test
+     */
+    public function it_shows_the_business_service_edit_page_for_management()
+    {
+        $this->arrangeBusinessWithOwner();
+
+        $service = $this->makeService();
+
+        $this->business->services()->save($service);
+
+        $this->actingAs($this->owner);
+
+        $this->visit(route('manager.business.service.edit', ['business' => $this->business, 'service' => $service]));
+
+        $this->assertResponseOk();
+        $this->see($service->name);
+        $this->see($service->color);
+        $this->see($service->description);
+    }
+
+    /**
+     * @covers   App\Http\Controllers\Manager\BusinessServiceController::update
+     * @test
+     */
+    public function it_updates_the_business_service()
+    {
+        $this->arrangeBusinessWithOwner();
+
+        $service = $this->makeService();
+
+        $this->business->services()->save($service);
+
+        $this->actingAs($this->owner);
+
+        $service->name = 'New Name';
+
+        $this->call('PUT', route('manager.business.service.update', [
+            'business' => $this->business,
+            'service' => $service]),
+            $service->toArray()
+            );
+
+        $this->assertEquals($service->name, $this->business->fresh()->services()->whereId($service->id)->first()->name);
+    }
+
+    /**
+     * @covers   App\Http\Controllers\Manager\BusinessServiceController::destroy
+     * @test
+     */
+    public function it_removes_the_business_service()
+    {
+        $this->arrangeBusinessWithOwner();
+
+        $service = $this->makeService();
+
+        $this->business->services()->save($service);
+
+        $this->assertCount(1, $this->business->fresh()->services);
+
+        $this->actingAs($this->owner);
+
+        $this->call('DELETE', route('manager.business.service.destroy', [
+            'business' => $this->business,
+            'service' => $service])
+            );
+
+        $this->assertCount(0, $this->business->fresh()->services);
+    }
+
     //////////////////////
     // Scenario Helpers //
     //////////////////////
