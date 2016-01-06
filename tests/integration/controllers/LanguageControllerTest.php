c64811eec21eef42bdce0a0c83d75f75261bc5a7
Ariel Vallese <alariva@gmail.com>
2016-01-06 01:04:42 +0100

    Fix user agent preferred language autoloading

--- tests/integration/controllers/LanguageControllerTest.php
+++ tests/integration/controllers/LanguageControllerTest.php
@@ -27,4 +27,19 @@ class LanguageControllerTest extends TestCase
         $this->assertSessionHas('applocale', $applocale);
         $this->assertResponseStatus(302);
     }
+
+    /**
+     * TODO: For some reason the custom header is not working, thus the test not
+     * feasible by now.
+     * 
+     * @test
+     */
+//    public function it_attempts_to_use_agent_preferred_languages_first_spanish()
+//    {
+//        $this->get('/', [
+//            'HTTP_ACCEPT_LANGUAGE'  => 'es-ES,es;q=0.8,en-US;q=0.6,en;q=0.4',
+//        ]);
+//
+//        $this->see('La agenda de citas para profesionales exitosos');
+//    }
 }
