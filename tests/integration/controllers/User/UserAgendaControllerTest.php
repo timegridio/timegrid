2ad4d06f77c28c2112447419fe0b38734f9d8b80
Ariel Vallese <alariva@gmail.com>
2016-01-12 20:51:27 +0100

    Remove tests' comments
    Tests should be self-explanatory

--- tests/integration/controllers/User/UserAgendaControllerTest.php
+++ tests/integration/controllers/User/UserAgendaControllerTest.php
@@ -13,14 +13,11 @@ class UserAgendaControllerTest extends TestCase
      */
     public function it_shows_empty_reservations_list()
     {
-        // Given I am an authenticated user
         $user = $this->createUser();
         $this->actingAs($user);
 
-        // And I go to favourites (subscriptions) section
         $this->visit('/')->click('My Reservations');
 
-        // Then I should see my empty reservations list
         $this->see('You have no ongoing reservations by now');
     }
 
@@ -29,13 +26,10 @@ class UserAgendaControllerTest extends TestCase
      */
     public function it_shows_reservations_list_with_a_reserved_appointment()
     {
-        // Given I am an authenticated user
         $owner = $this->createUser();
         $user = $this->createUser();
         $this->actingAs($user);
 
-        // And there exist a registered business
-        // And which I am subscribed as contact
         $business = $this->createBusiness(['name' => 'tosto this tosti']);
         $business->owners()->save($owner);
 
@@ -46,7 +40,6 @@ class UserAgendaControllerTest extends TestCase
         $service = $this->makeService();
         $business->services()->save($service);
 
-        // And I have a RESERVED appointment
         $appointment = factory(Appointment::class)->make(['status' => Appointment::STATUS_RESERVED]);
         $appointment->issuer()->associate($user);
         $appointment->contact()->associate($contact);
@@ -54,11 +47,8 @@ class UserAgendaControllerTest extends TestCase
         $appointment->service()->associate($service);
         $appointment->save();
 
-        // And I go to favourites (subscriptions) section
         $this->visit('/')->click('My Reservations');
 
-        // Then I should see my reservations list
-        // and the reservation details
         $this->see('Reserved')
             ->see($appointment->code)
             ->see($appointment->business->name);
@@ -102,13 +92,10 @@ class UserAgendaControllerTest extends TestCase
      */
     public function it_shows_reservations_list_with_an_annulated_appointment()
     {
-        // Given I am an authenticated user
         $owner = $this->createUser();
         $user = $this->createUser();
         $this->actingAs($user);
 
-        // And there exist a registered business
-        // And which I am subscribed as contact
         $business = $this->createBusiness(['name' => 'tosto this tosti']);
         $business->owners()->save($owner);
 
@@ -119,7 +106,6 @@ class UserAgendaControllerTest extends TestCase
         $service = $this->makeService();
         $business->services()->save($service);
 
-        // And I have a RESERVED appointment
         $appointment = factory(Appointment::class)->make(['status' => Appointment::STATUS_ANNULATED]);
         $appointment->issuer()->associate($user);
         $appointment->contact()->associate($contact);
@@ -127,11 +113,8 @@ class UserAgendaControllerTest extends TestCase
         $appointment->service()->associate($service);
         $appointment->save();
 
-        // And I go to favourites (subscriptions) section
         $this->visit('/')->click('My Reservations');
 
-        // Then I should see my reservations list
-        // and the reservation details
         $this->see('Annulated')
             ->see($appointment->code)
             ->see($appointment->business->name);
@@ -142,13 +125,10 @@ class UserAgendaControllerTest extends TestCase
      */
     public function it_does_not_show_an_inactive_appointment_on_reservations_list()
     {
-        // Given I am an authenticated user
         $owner = $this->createUser();
         $user = $this->createUser();
         $this->actingAs($user);
 
-        // And there exist a registered business
-        // And which I am subscribed as contact
         $business = $this->createBusiness(['name' => 'tosto this tosti']);
         $business->owners()->save($owner);
 
@@ -159,7 +139,6 @@ class UserAgendaControllerTest extends TestCase
         $service = $this->makeService();
         $business->services()->save($service);
 
-        // And I have a RESERVED appointment
         $appointment = factory(Appointment::class)->make([
             'status'   => Appointment::STATUS_ANNULATED,
             'start_at' => Carbon::now()->subDays(50),
@@ -170,15 +149,10 @@ class UserAgendaControllerTest extends TestCase
         $appointment->service()->associate($service);
         $appointment->save();
 
-        // And I go to favourites (subscriptions) section
         $this->visit('/')->click('My Reservations');
 
-        // Then I should see my reservations list
-        // and the reservation details
         $this->see('You have no ongoing reservations')
              ->dontSee('Annulated');
-             //->dontSee($appointment->code)
-             //->dontSee($appointment->business->name);
     }
 
     /**
@@ -186,13 +160,10 @@ class UserAgendaControllerTest extends TestCase
      */
     public function it_does_show_an_old_but_active_appointment_on_reservations_list()
     {
-        // Given I am an authenticated user
         $owner = $this->createUser();
         $user = $this->createUser();
         $this->actingAs($user);
 
-        // And there exist a registered business
-        // And which I am subscribed as contact
         $business = $this->createBusiness(['name' => 'tosto this tosti']);
         $business->owners()->save($owner);
 
@@ -203,7 +174,6 @@ class UserAgendaControllerTest extends TestCase
         $service = $this->makeService();
         $business->services()->save($service);
 
-        // And I have a RESERVED appointment
         $appointment = factory(Appointment::class)->make([
             'status'   => Appointment::STATUS_RESERVED,
             'start_at' => Carbon::now()->subDays(50),
@@ -214,11 +184,8 @@ class UserAgendaControllerTest extends TestCase
         $appointment->service()->associate($service);
         $appointment->save();
 
-        // And I go to favourites (subscriptions) section
         $this->visit('/')->click('My Reservations');
 
-        // Then I should see my reservations list
-        // and the reservation details
         $this->see('Reserved')
             ->see($appointment->code)
             ->see($appointment->business->name);
@@ -229,13 +196,10 @@ class UserAgendaControllerTest extends TestCase
      */
     public function it_queries_vacancies()
     {
-        // Given I am an authenticated user
         $owner = $this->createUser();
         $user = $this->createUser();
         $this->actingAs($user);
 
-        // And there exist a registered business
-        // And which I am subscribed as contact
         $business = $this->createBusiness(['name' => 'tosto this tosti']);
         $business->owners()->save($owner);
 
@@ -246,18 +210,12 @@ class UserAgendaControllerTest extends TestCase
         $service = $this->makeService();
         $business->services()->save($service);
 
-        // And there is vacancy for the service
         $this->vacancy = $this->makeVacancy();
         $this->vacancy->service()->associate($service);
         $business->vacancies()->save($this->vacancy);
 
-        // And I go to favourites (subscriptions) section
         $this->visit(route('user.booking.book', ['business' => $business]));
 
-        // WARNING; may return false positive as the view includes the services description
-
-        // Then I should see my reservations list
-        // and the reservation details
         $this->see('Select a service to reserve')
              ->see($service->name)
              ->see('Confirm');
@@ -339,30 +297,22 @@ class UserAgendaControllerTest extends TestCase
      */
     public function it_tries_to_query_vacancies_without_subscription()
     {
-        // Given I am an authenticated user
         $owner = $this->createUser();
         $user = $this->createUser();
         $this->actingAs($user);
 
-        // And there exist a registered business
-        // And which I am NOT subscribed as contact
         $business = $this->createBusiness(['name' => 'tosto this tosti']);
         $business->owners()->save($owner);
 
         $service = $this->makeService();
         $business->services()->save($service);
 
-        // And there is vacancy for the service (OPTIONAL)
         $this->vacancy = $this->makeVacancy();
         $this->vacancy->service()->associate($service);
         $business->vacancies()->save($this->vacancy);
 
-        // And I go to business home
         $this->visit(route('user.businesses.home', ['business' => $business]));
 
-        // WARNING; may return false positive as the view includes the services description
-
-        // Then I should see Subscribe button for that business
         $this->see('Subscribe')
              ->see($business->name);
     }
@@ -376,11 +326,9 @@ class UserAgendaControllerTest extends TestCase
      */
     public function it_makes_a_reservation()
     {
-        // Given I am an authenticated user
         $user = $this->createUser();
         $this->actingAs($user);
 
-        // And there exist a registered business that provides a service
         $owner = $this->createUser();
         $business = $this->createBusiness(['name' => 'tosto this tosti']);
         $business->owners()->save($owner);
@@ -388,13 +336,11 @@ class UserAgendaControllerTest extends TestCase
         $service = $this->makeService();
         $business->services()->save($service);
 
-        // And which I am subscribed-to as contact
         $contact = $this->createContact([
             'user_id' => $user->id,
             ]);
         $business->contacts()->save($contact);
 
-        // And there is vacancy for the service (OPTIONAL)
         $this->vacancy = $this->makeVacancy([
             'business_id' => $business->id,
             'service_id'  => $service->id,
@@ -405,7 +351,6 @@ class UserAgendaControllerTest extends TestCase
         $this->vacancy->service()->associate($service);
         $business->vacancies()->save($this->vacancy);
 
-        // And I submit the reservation form
         $this->withoutMiddleware();
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
@@ -415,7 +360,6 @@ class UserAgendaControllerTest extends TestCase
             'comments'   => 'test comments',
             ]);
 
-        // Then I should see Subscribe button for that business
         $this->seeInDatabase('appointments', ['business_id' => $business->id]);
     }
 
@@ -448,7 +392,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($user);
 
-        // This attempt gets accepted
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -459,7 +402,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->assertCount(1, $business->fresh()->bookings()->get());
 
-        // This attempt gets rejected
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -503,7 +445,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($userOne);
 
-        // This attempt gets accepted
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -516,7 +457,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($userTwo);
 
-        // This attempt gets rejected
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -560,7 +500,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($userOne);
 
-        // This attempt gets accepted
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -573,7 +512,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($userTwo);
 
-        // This attempt gets rejected
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -635,11 +573,9 @@ class UserAgendaControllerTest extends TestCase
      */
     public function it_makes_a_reservation_with_timeslot()
     {
-        // Given I am an authenticated user
         $user = $this->createUser();
         $this->actingAs($user);
 
-        // And there exist a registered business that provides a service
         $owner = $this->createUser();
         $business = $this->createBusiness(['name' => 'tosto this tosti', 'strategy' => 'timeslot']);
         $business->owners()->save($owner);
@@ -647,13 +583,11 @@ class UserAgendaControllerTest extends TestCase
         $service = $this->makeService();
         $business->services()->save($service);
 
-        // And which I am subscribed-to as contact
         $contact = $this->createContact([
             'user_id' => $user->id,
             ]);
         $business->contacts()->save($contact);
 
-        // And there is vacancy for the service (OPTIONAL)
         $this->vacancy = $this->makeVacancy([
             'business_id' => $business->id,
             'service_id'  => $service->id,
@@ -664,7 +598,6 @@ class UserAgendaControllerTest extends TestCase
         $this->vacancy->service()->associate($service);
         $business->vacancies()->save($this->vacancy);
 
-        // And I submit the reservation form
         $this->withoutMiddleware();
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
@@ -674,7 +607,6 @@ class UserAgendaControllerTest extends TestCase
             'comments'   => 'test comments',
             ]);
 
-        // Then I should see Subscribe button for that business
         $this->seeInDatabase('appointments', ['business_id' => $business->id]);
     }
 
@@ -707,7 +639,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($user);
 
-        // This attempt gets accepted
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -718,7 +649,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->assertCount(1, $business->fresh()->bookings()->get());
 
-        // This attempt gets rejected
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -762,7 +692,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($userOne);
 
-        // This attempt gets accepted
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -775,7 +704,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($userTwo);
 
-        // This attempt gets rejected
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -819,7 +747,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($userOne);
 
-        // This attempt gets accepted
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
@@ -832,7 +759,6 @@ class UserAgendaControllerTest extends TestCase
 
         $this->actingAs($userTwo);
 
-        // This attempt gets rejected
         $this->call('POST', route('user.booking.store', ['business' => $business]), [
             'businessId' => $business->id,
             'service_id' => $service->id,
