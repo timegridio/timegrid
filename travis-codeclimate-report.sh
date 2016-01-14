dea70d4ae8e0fa77d058511edb2ae19e947d29fe
Ariel Vallese <alariva@gmail.com>
2016-01-14 22:43:39 +0100

    Add travis-codeclimate-report.sh

--- travis-codeclimate-report.sh
+++ travis-codeclimate-report.sh
@@ -4,6 +4,10 @@
 #  This script is intended to be run by TravisCI only #
 #######################################################
 
+echo "Submitting CodeClimate report"
+
 php vendor/bin/test-reporter --stdout > codeclimate.json
 
 curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
+
+echo Finish with status $?
