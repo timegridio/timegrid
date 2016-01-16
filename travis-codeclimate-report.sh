d2c2d25a4279558c00f4c6a421c7d3954cae44a5
Ariel Vallese <alariva@gmail.com>
2016-01-16 01:02:57 +0100

    Rollback to after_success method and use TRAVIS_BRANCH to compare

--- travis-codeclimate-report.sh
+++ travis-codeclimate-report.sh
@@ -4,9 +4,12 @@
 #  This script is intended to be run by TravisCI only #
 #######################################################
 
-echo "Submitting Test Coverage Report to CodeClimate..."
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
 
-## Should only be called for Master branch
+## Coverage reports should only be sent on master branch
+
+echo "Submitting Test Coverage Report to CodeClimate..."
 
 ## Method I
 
@@ -17,6 +20,12 @@ php vendor/bin/test-reporter
 # php vendor/bin/test-reporter --stdout > codeclimate.json
 
 # curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
+
+else
+
+    echo "Not in Master branch. Will not publish coverage.\n\n"
+
+fi
 
 ## Finish
 
