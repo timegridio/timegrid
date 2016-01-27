8e1e4dfd4f7d0d75b53b69212697ce350584689a
Ariel Vallese <alariva@gmail.com>
2016-01-27 15:52:02 -0300

    Update wording

--- resources/lang/en_US.utf8/passwords.php
+++ resources/lang/en_US.utf8/passwords.php
@@ -1,8 +1,22 @@
 <?php
-/*************************************************************************
- Generated via "php artisan localization:missing" at 2015/12/09 15:37:10
-*************************************************************************/
 
 return [
-  'user' => 'We did not find this email. Are you registered?',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that e-mail address.",
+
 ];
--- resources/lang/es_ES.utf8/passwords.php
+++ resources/lang/es_ES.utf8/passwords.php
@@ -1,8 +1,22 @@
 <?php
-/*************************************************************************
- Generated via "php artisan localization:missing" at 2015/12/09 15:37:10 
-*************************************************************************/
 
-return  [
-  'user' => 'No encontramos ese email en nuestra base. ¿Te registraste?',
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe ser de al menos 6 caracteres.',
+    'reset' => 'Tu contraseña fue actualizada.',
+    'sent' => 'Te enviamos un correo para el balnqueo de contraseña.',
+    'token' => 'El código de blanqueo de contraseña es inválido.',
+    'user' => "No encontramos un usuario en nuestra base. ¿Te registraste?",
+
 ];
