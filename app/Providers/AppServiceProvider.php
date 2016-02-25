8324e4b09994e166885f172cc0e7ad2a180edd2b
Ariel Vallese <alariva@gmail.com>
2016-02-25 10:31:12 -0300

    :package::new: Add potsky/laravel-localization-helpers back

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,7 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() == 'local') {
             $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
             $this->app->register('Barryvdh\Debugbar\ServiceProvider');
-            // $this->app->register('Potsky\LaravelLocalizationHelpers\LaravelLocalizationHelpersServiceProvider');
+            $this->app->register('Potsky\LaravelLocalizationHelpers\LaravelLocalizationHelpersServiceProvider');
         }
     }
 }
