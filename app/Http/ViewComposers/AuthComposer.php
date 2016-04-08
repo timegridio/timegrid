53b87f8081704a7a08714732480a2daeeaedc532
Ariel Vallese <alariva@gmail.com>
2016-04-08 16:30:10 -0300

    Enhance code style quality

--- app/Http/ViewComposers/AuthComposer.php
+++ app/Http/ViewComposers/AuthComposer.php
@@ -16,19 +16,16 @@ class AuthComposer
      */
     public function compose(View $view)
     {
-        view()->share('isGuest', auth()->guest());
-        view()->share('signedIn', auth()->check());
-        view()->share('user', auth()->user());
+        $view->share('isGuest', auth()->guest());
+        $view->share('signedIn', auth()->check());
+        $view->share('user', auth()->user());
 
-        if(auth()->user())
-        {
-            view()->share('gravatarURL', Gravatar::get(auth()->user()->email, ['size' => 24, 'secure' => true]));
-            view()->share('appointments', auth()->user()->appointments()->active()->get() );
-        }
-        else
-        {
-            view()->share('gravatarURL', 'http://placehold.it/150x150');
-            view()->share('appointments', [] );
+        if (auth()->user()) {
+            $view->share('gravatarURL', Gravatar::get(auth()->user()->email, ['size' => 24, 'secure' => true]));
+            $view->share('appointments', auth()->user()->appointments()->active()->get());
+        } else {
+            $view->share('gravatarURL', 'http://placehold.it/150x150');
+            $view->share('appointments', []);
         }
     }
 }
