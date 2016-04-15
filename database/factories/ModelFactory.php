7fde60888e76aaee5e9ebaaa93133fe969035d4d
mgamal <mg.freelancer92@gmail.com>
2016-04-15 03:18:35 +0200

    Fix unique fields on factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -4,11 +4,11 @@
 // User //
 //////////
 
-$factory->define('App\Models\User', function (Faker\Generator $faker) {
+$factory->define(App\Models\User::class , function (Faker\Generator $faker) {
     return [
-        'username' => $faker->firstName,
+        'username' => $faker->unique()->firstName . rand(1,1000),
         'name'     => $faker->firstName,
-        'email'    => $faker->safeEmail,
+        'email'    => $faker->unique()->safeEmail,
         'password' => bcrypt('password'),
     ];
 });
@@ -36,7 +36,7 @@ $factory->define(Timegridio\Concierge\Models\Contact::class, function (Faker\Gen
         'firstname'      => $faker->firstName,
         'lastname'       => $faker->lastName,
         'nin'            => $faker->numberBetween(25000000, 50000000),
-        'email'          => $faker->safeEmail,
+        'email'          => $faker->unique()->safeEmail,
         'birthdate'      => \Carbon\Carbon::now()->subYears(30),
         'mobile'         => null,
         'mobile_country' => null,
