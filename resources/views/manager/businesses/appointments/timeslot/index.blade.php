bdba033cf28565257c782a411b0cf3139a2bf58d
Ariel Vallese <alariva@gmail.com>
2016-05-26 20:52:20 -0300

    Display timezone tooltip

--- resources/views/manager/businesses/appointments/timeslot/index.blade.php
+++ resources/views/manager/businesses/appointments/timeslot/index.blade.php
@@ -26,8 +26,8 @@
                     <tr id="{{ $appointment->code }}">
                         <td>&nbsp;<code>{!! Icon::barcode() !!}&nbsp;{{ $appointment->code }}</code>&nbsp;{!! $appointment->statusIcon !!}</td>
                         <td>{{ $appointment->date('d/M') }}</td>
-                        <td title="{{ $appointment->start_at->diffForHumans() }}">{{ $appointment->time }}</td>
-                        <td title="{{ $appointment->tz }}">{{ $appointment->finishTime }}</td>
+                        <td title="{{ $appointment->timezone() }} {{ $appointment->start_at->diffForHumans() }}">{{ $appointment->time }}</td>
+                        <td title="{{ $appointment->timezone() }}">{{ $appointment->finishTime }}</td>
                         <td>{{ trans_duration($appointment->duration()) }}</td>
                         <td>{{ $appointment->service ? $appointment->service->name : '' }}</td>
                         <td>{{ $appointment->contact->fullname }}</td>
