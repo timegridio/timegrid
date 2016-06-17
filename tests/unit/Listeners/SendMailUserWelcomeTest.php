70bcf89fbb4fa9be7622fe3bc0c408ee781a9081
Ariel Vallese <alariva@gmail.com>
2016-06-17 19:03:48 -0300

    Add SendMailUserWelcomeTest

---
+++ tests/unit/Listeners/SendMailUserWelcomeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Events\NewUserWasRegistered;
+use App\Listeners\SendMailUserWelcome;
+use App\TransMail;
+
+class SendMailUserWelcomeTest extends TestCase
+{
+    use CreateUser;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->transmail = Mockery::mock(TransMail::class);
+
+        $this->user = $this->createUser();
+    }
+
+    /**
+     * @test
+     */
+    public function it_sends_a_soft_appointment_validation_request_email()
+    {
+        $this->transmail->shouldReceive('template')->once()->andReturn($this->transmail);
+        $this->transmail->shouldReceive('subject')->once()->andReturn($this->transmail);
+        $this->transmail->shouldReceive('send')->once()->andReturn($this->transmail);
+        $this->transmail->shouldReceive('success')->andReturn(true);
+
+        $listener = new SendMailUserWelcome($this->transmail);
+
+        $listener->handle(new NewUserWasRegistered($this->user));
+
+        $this->assertTrue($this->transmail->success());
+    }
+}
