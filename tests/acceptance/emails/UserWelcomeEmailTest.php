34de86d31cba023dad35e613a106fbc42860a478
Ariel Vallese <alariva@gmail.com>
2016-06-24 11:50:02 -0300

    :package::new: Add package tightenco/mailthief
    Add UserWelcomeEmailTest

---
+++ tests/acceptance/emails/UserWelcomeEmailTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use MailThief\Facades\MailThief;
+
+class UserWelcomeEmailTest extends TestCase
+{
+    use CreateUser;
+    use DatabaseTransactions;
+    /**
+     * @test
+     */
+    public function it_sends_a_welcome_email_to_new_registered_user()
+    {
+        MailThief::hijack();
+
+        $user = $this->makeUser();
+
+        $this->visit('auth/register');
+
+        $this->type($user->name, 'name');
+        $this->type($user->email, 'email');
+        $this->type('password', 'password');
+        $this->type('password', 'password_confirmation');
+
+        $this->press('Register');
+
+        // Check that an email was sent to this email address
+        $this->assertTrue(MailThief::hasMessageFor($user->email));
+
+        // Make sure the email has the correct subject
+        $this->assertEquals('Welcome to timegrid.io', MailThief::lastMessage()->subject);
+    }
+}
