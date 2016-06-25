10ff8a3ae2b51eea9c97d9b0515a799123f3ad2a
Ariel Vallese <alariva@gmail.com>
2016-06-25 15:22:29 -0300

    Add UserRepository test

---
+++ tests/unit/Repositories/UserRepositoryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Repositories\UserRepository;
+
+class UserRepositoryTest extends TestCase
+{
+    use CreateUser;
+
+    /**
+     * @var UserRepository
+     */
+    protected $users;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->users = new UserRepository();
+    }
+
+    /**
+     * @test
+     */
+    public function it_finds_a_user_by_email()
+    {
+        $user = $this->createUser();
+
+        $retrievedUser = $this->users->findOrCreate($user);
+
+        $this->assertEquals($retrievedUser->email, $user->email);
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_a_user_when_not_found()
+    {
+        $user = $this->makeUser();
+
+        $retrievedUser = $this->users->findOrCreate($user);
+
+        $this->assertEquals($retrievedUser->email, $user->email);
+    }
+}
