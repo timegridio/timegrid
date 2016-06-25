257bac961946d66023fd675e66997ca9d5f2ac9f
Ariel Vallese <alariva@gmail.com>
2016-06-25 16:45:56 -0300

    Remove redundant method with default behavior

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,16 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
      * Register any application services.
      *
      * @return void
