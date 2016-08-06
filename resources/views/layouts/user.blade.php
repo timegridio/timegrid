13d7aaf5f8af8768ed87c36f34b01f16b831147e
Ariel Vallese <alariva@gmail.com>
2016-08-06 03:22:18 -0300

    Fix logo link to home

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -44,7 +44,7 @@
             <nav class="navbar navbar-static-top">
                 <div class="container">
                     <div class="navbar-header">
-                        <a href="../../index2.html" class="navbar-brand">time<b>grid</b></a>
+                        <a href="{{ route('home') }}" class="navbar-brand">time<b>grid</b></a>
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
                             <i class="fa fa-bars"></i>
                         </button>
