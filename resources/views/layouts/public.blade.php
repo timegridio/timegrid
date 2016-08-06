d7c1582fce6d9cdb28725893cce9690c066d2342
Ariel Vallese <alariva@gmail.com>
2016-08-06 03:20:07 -0300

    PSR-2 Reindent

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -1,70 +1,70 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>{{ isset($business) ? $business->name . ' / ' : '' }}{{ trans('app.name') }}</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ isset($business) ? $business->name . ' / ' : '' }}{{ trans('app.name') }}</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
-  <!-- iCheck -->
-  
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
+    <!-- iCheck -->
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 
-  @yield('css')
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 
-  @yield('headscripts')
+@yield('css')
+
+@yield('headscripts')
 
 </head>
 <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
 <body class="hold-transition skin-blue layout-top-nav">
-{!! Analytics::render() !!}
+    {!! Analytics::render() !!}
 
-@if (config('env') == 'demo')
-<a target="_blank" href="https://github.com/alariva/timegrid">
-    <img style="position: absolute; top: 0; left: 0; border: 0; z-index:99999" src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png">
-</a>
-@endif
+    @if (config('env') == 'demo')
+    <a target="_blank" href="https://github.com/alariva/timegrid">
+        <img style="position: absolute; top: 0; left: 0; border: 0; z-index:99999" src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png">
+    </a>
+    @endif
 
-<div class="wrapper">
+    <div class="wrapper">
 
-  <header class="main-header">
+        <header class="main-header">
 
-  </header>
-  <!-- Full Width Column -->
-  <div class="content-wrapper">
-    <div class="container">
+        </header>
+        <!-- Full Width Column -->
+        <div class="content-wrapper">
+            <div class="container">
 
-      <!-- Main content -->
-      <section class="content">
+                <!-- Main content -->
+                <section class="content">
 
-        @include('flash::message')
+                    @include('flash::message')
 
-        @yield('content')
+                    @yield('content')
 
-      </section>
-      <!-- /.content -->
-    </div>
-    <!-- /.container -->
-  </div>
-  <!-- /.content-wrapper -->
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /.content-wrapper -->
 
-  @include('_footer')
+        @include('_footer')
 
-    <!-- /.container -->
-  </footer>
+        <!-- /.container -->
+    </footer>
 </div>
 <!-- ./wrapper -->
 
@@ -72,11 +72,11 @@
 <script src="{{ asset('js/app.min.js') }}"></script>
 
 <script type="text/javascript">
-$(document).ready(function() {
-    $('.btn').tooltipster({
-      animation: "grow", theme: 'tooltipster-light'
+    $(document).ready(function() {
+        $('.btn').tooltipster({
+            animation: "grow", theme: 'tooltipster-light'
+        });
     });
-});
 </script>
 
 @stack('footer_scripts')
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -1,111 +1,111 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>{{ isset($business) ? $business->name . ' / ' : '' }}{{ trans('app.name') }}</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{ isset($business) ? $business->name . ' / ' : '' }}{{ trans('app.name') }}</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
-  <!-- iCheck -->
-  
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
+    <!-- iCheck -->
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 
-  @yield('css')
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 
-  @yield('headscripts')
+@yield('css')
+
+@yield('headscripts')
 
 </head>
 <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
 <body class="hold-transition skin-blue layout-top-nav">
-{!! Analytics::render() !!}
+    {!! Analytics::render() !!}
 
-@if (config('env') == 'demo')
-<a target="_blank" href="https://github.com/alariva/timegrid">
-    <img style="position: absolute; top: 0; left: 0; border: 0; z-index:99999" src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png">
-</a>
-@endif
+    @if (config('env') == 'demo')
+    <a target="_blank" href="https://github.com/alariva/timegrid">
+        <img style="position: absolute; top: 0; left: 0; border: 0; z-index:99999" src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png">
+    </a>
+    @endif
 
-<div class="wrapper">
+    <div class="wrapper">
 
-  <header class="main-header">
-    <nav class="navbar navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <a href="../../index2.html" class="navbar-brand">time<b>grid</b></a>
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-            <i class="fa fa-bars"></i>
-          </button>
+        <header class="main-header">
+            <nav class="navbar navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <a href="../../index2.html" class="navbar-brand">time<b>grid</b></a>
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            @include('_navi18n')
+                            @include('user._navmenu')
+                        </ul>
+                        <!-- Search input here -->
+                    </div>
+                    <!-- /.navbar-collapse -->
+                    <!-- Navbar Right Menu -->
+                    <div class="navbar-custom-menu">
+                        <ul class="nav navbar-nav">
+
+                            <!-- Notifications Menu -->
+                            @include('user._notifications-menu')
+
+                            <!-- User Account Menu -->
+                            @include('_user-account-menu')
+
+                        </ul>
+                    </div>
+                    <!-- /.navbar-custom-menu -->
+                </div>
+                <!-- /.container-fluid -->
+            </nav>
+        </header>
+        <!-- Full Width Column -->
+        <div class="content-wrapper">
+            <div class="container">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        @yield('title', '')
+                        <small>@yield('subtitle', '')</small>
+                    </h1>
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+
+                    @include('flash::message')
+                    @include('_errors')
+
+                    @yield('content')
+
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- /.container -->
         </div>
+        <!-- /.content-wrapper -->
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
-          <ul class="nav navbar-nav">
-            @include('_navi18n')
-            @include('user._navmenu')
-          </ul>
-          <!-- Search input here -->
-        </div>
-        <!-- /.navbar-collapse -->
-        <!-- Navbar Right Menu -->
-        <div class="navbar-custom-menu">
-          <ul class="nav navbar-nav">
+        @include('_footer')
 
-            <!-- Notifications Menu -->
-            @include('user._notifications-menu')
-
-            <!-- User Account Menu -->
-            @include('_user-account-menu')
-
-          </ul>
-        </div>
-        <!-- /.navbar-custom-menu -->
-      </div>
-      <!-- /.container-fluid -->
-    </nav>
-  </header>
-  <!-- Full Width Column -->
-  <div class="content-wrapper">
-    <div class="container">
-      <!-- Content Header (Page header) -->
-      <section class="content-header">
-        <h1>
-          @yield('title', '')
-          <small>@yield('subtitle', '')</small>
-        </h1>
-      </section>
-
-      <!-- Main content -->
-      <section class="content">
-
-        @include('flash::message')
-        @include('_errors')
-
-        @yield('content')
-
-      </section>
-      <!-- /.content -->
-    </div>
-    <!-- /.container -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  @include('_footer')
-
-    <!-- /.container -->
-  </footer>
+        <!-- /.container -->
+    </footer>
 </div>
 <!-- ./wrapper -->
 
@@ -113,9 +113,9 @@
 <script src="{{ asset('js/app.min.js') }}"></script>
 
 <script type="text/javascript">
-$(document).ready(function() {
-    $('.btn').tooltipster({animation: "grow", theme: 'tooltipster-light'});
-});
+    $(document).ready(function() {
+        $('.btn').tooltipster({animation: "grow", theme: 'tooltipster-light'});
+    });
 </script>
 
 @stack('footer_scripts')
