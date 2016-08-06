c4aa657a3fcc62d149326b3e1a974ddf368eecee
Ariel Vallese <alariva@gmail.com>
2016-08-06 03:26:54 -0300

    PSR-2 Reindent

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,70 +7,70 @@
 
 @section('content')
 <div class="login-box">
-  <div class="login-logo">
-    <a href="{{ url('/') }}">time<b>grid</b></a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg">{{ trans('auth.login.title') }}</p>
+    <div class="login-logo">
+        <a href="{{ url('/') }}">time<b>grid</b></a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+        <p class="login-box-msg">{{ trans('auth.login.title') }}</p>
 
-    @if ($errors->has())
+        @if ($errors->has())
         <div class="alert alert-danger">
             <strong>{{ trans('auth.login.alert.whoops') }}</strong> {{ trans('auth.login.alert.message') }}<br><br>
             <ul>
                 @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
+                <li>{{ $error }}</li>
                 @endforeach
             </ul>
         </div>
 
         {!! Button::success(trans('auth.btn.not_registered'))->block()->asLinkTo(url('/auth/register')) !!}<p>&nbsp;</p>
-    @endif
+        @endif
 
-    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-      <input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
-      <div class="form-group has-feedback">
-        <input type="email" name="email" class="form-control" placeholder="{{ trans('auth.login.email') }}">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback">
-        <input type="password" name="password" class="form-control" placeholder="{{ trans('auth.login.password') }}">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
+        <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+            <input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
+            <div class="form-group has-feedback">
+                <input type="email" name="email" class="form-control" placeholder="{{ trans('auth.login.email') }}">
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" name="password" class="form-control" placeholder="{{ trans('auth.login.password') }}">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="row">
+                <div class="col-xs-8">
 
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox" name="remember"> {{ trans('auth.login.remember_me') }}
-            </label>
-          </div>
+                    <div class="checkbox icheck">
+                        <label>
+                            <input type="checkbox" name="remember"> {{ trans('auth.login.remember_me') }}
+                        </label>
+                    </div>
 
+                </div>
+                <!-- /.col -->
+                <div class="col-xs-4">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('auth.login.login') }}</button>
+                </div>
+                <!-- /.col -->
+            </div>
+        </form>
+
+        <div class="social-auth-links text-center">
+            <p>- {{ trans('auth.label.oauth_direct_access') }} -</p>
+            @include('auth/social')
         </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('auth.login.login') }}</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
+        <!-- /.social-auth-links -->
 
-    <div class="social-auth-links text-center">
-      <p>- {{ trans('auth.label.oauth_direct_access') }} -</p>
-      @include('auth/social')
+        <a class="btn btn-link" href="{{ url('/auth/password/reset') }}">{{ trans('auth.login.forgot') }}</a><br>
+
+        <div id="notRegisteredLink" class="row hidden" style="margin-top: 20px;">
+            <div class="col-md-12">
+                {!! Button::success(trans('auth.btn.not_registered'))->withAttributes(['id' => 'btnNotRegistered', 'class' => ''])->block()->asLinkTo(url('/auth/register')) !!}
+            </div>
+        </div>
+
     </div>
-    <!-- /.social-auth-links -->
-
-    <a class="btn btn-link" href="{{ url('/auth/password/reset') }}">{{ trans('auth.login.forgot') }}</a><br>
-    
-    <div id="notRegisteredLink" class="row hidden" style="margin-top: 20px;">
-        <div class="col-md-12">
-            {!! Button::success(trans('auth.btn.not_registered'))->withAttributes(['id' => 'btnNotRegistered', 'class' => ''])->block()->asLinkTo(url('/auth/register')) !!}
-        </div>
-    </div>
-
-  </div>
-  <!-- /.login-box-body -->
+    <!-- /.login-box-body -->
 </div>
 <!-- /.login-box -->
 @endsection
@@ -78,22 +78,22 @@
 @push('footer_scripts')
 <script src="{{ asset('js/iCheck/icheck.min.js') }}"></script>
 <script type="text/javascript">
-$(document).ready(function(){
+    $(document).ready(function(){
 
-    $('#notRegisteredLink').hide();
-    var timer;
-    clearTimeout(timer);
-    timer = setTimeout(function (event) {
-        console.log('Search keypress');
-        $('#notRegisteredLink').removeClass('hidden').show('slow');
-    }, 10000);
+        $('#notRegisteredLink').hide();
+        var timer;
+        clearTimeout(timer);
+        timer = setTimeout(function (event) {
+            console.log('Search keypress');
+            $('#notRegisteredLink').removeClass('hidden').show('slow');
+        }, 10000);
 
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%' // optional
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        });
+
     });
-
-});
 </script>
 @endpush
