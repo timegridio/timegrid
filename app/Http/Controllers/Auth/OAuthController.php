126e543a3736d626e5648361fe8f242c3ed5bfc7
Ariel Vallese <alariva@gmail.com>
2016-11-08 09:47:43 -0300

    WIP Social Authentication L53 upgrade

--- app/Http/Controllers/Auth/OAuthController.php
+++ app/Http/Controllers/Auth/OAuthController.php
@@ -5,15 +5,13 @@ namespace App\Http\Controllers\Auth;
 use App\AuthenticateUser;
 use App\AuthenticateUserListener;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class OAuthController extends Controller implements AuthenticateUserListener
 {
-    use RegistersUsers, AuthenticatesUsers, ThrottlesLogins;
+    use ThrottlesLogins;
 
     public function __construct()
     {
