7386b17c47d77e530c8a428e85bc7d87a1a8f7ba
Ariel Vallese <alariva@gmail.com>
2016-11-24 12:11:40 -0300

    Remove payment gateways footer and resources
    Should be added in a more flexible way

--- resources/views/manager/pricing.blade.php
+++ resources/views/manager/pricing.blade.php
@@ -156,40 +156,6 @@
         </div> <!-- /offset -->
     </div> <!-- /flat -->
 
-    <div class="row well" style="background-color:#fff" id="payment">
-
-        <ul class="list-inline">
-            <li><img src="{!! asset('img/payment/logos/paypal-logo.png') !!}" alt="Cobrar con PayPal" height="60"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_c1.gif') !!}" alt="Saldo CuentaDigital" width="108" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_c11.gif') !!}" alt="VoucherDigital" width="68" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_c20.gif') !!}" alt="PagoFacil Pago Facil" width="30" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_c27.gif') !!}" alt="RapiPago Rapi Pago" width="68" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_c56.gif') !!}" alt="CobroExpress" width="48" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_c63.gif') !!}" alt="Ripsa" width="44" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_c68.png') !!}" alt="Link RedLink PagosLink LinkPagos" width="26" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_c76.gif') !!}" alt="PagoDirecto Pago Directo Debito Automatico" width="43" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r2_c36.gif') !!}" alt="Bapro BaproPagos" width="68" height="20"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r2_c44.gif') !!}" alt="ProvinciaPagos Provincia Pagos" width="65" height="21"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_formo.gif') !!}" alt="FormoPagos" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_pagolisto.gif') !!}" alt="Pagolisto" height="31"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_pampa.gif') !!}" alt="PampaPagos" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_chubut.gif') !!}" alt="ChubutPagos" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r1_coope.gif') !!}" alt="Cooperativa Obrera" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/logo_r8_c40.gif') !!}" alt="Transferencia Bancaria Local" width="76" height="25"></li>
-            <li><img src="{!! asset('img/payment/logos/visa.png') !!}" alt="cobrar con VISA" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/mastercard.png') !!}" alt="cobrar con MASTERCARD" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/argencard.png') !!}" alt="cobrar con ARGENCARD" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/amex.png') !!}" alt="cobrar con American Express AMEX" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/tarjetanaranja.png') !!}" alt="cobrar con tarjeta NARANJA" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/shopping.png') !!}" alt="cobrar con TARJETA SHOPPING" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/cencosud.png') !!}" alt="cobrar con TARJETA CENCOSUD" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/nativa.png') !!}" alt="cobrar con NATIVA" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/tarjetamas.png') !!}" alt="cobrar con TARJETA MAS" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/diners.png') !!}" alt="cobrar con DINERS" height="30"></li>
-            <li><img src="{!! asset('img/payment/logos/cordobesa.png') !!}" alt="cobrar con tarjeta CORDOBESA" height="30"></li>
-        </ul>
-    </div>
-
 </div> <!-- /container -->
 @endsection
 
@@ -228,12 +194,6 @@ var tour = new Tour({
         element: "#plan2",
         title: "{{trans('tour.pricing.step5.title')}}",
         content: "{{trans('tour.pricing.step5.content')}}"
-    },
-    {
-        element: "#payment",
-        title: "{{trans('tour.pricing.step6.title')}}",
-        content: "{{trans('tour.pricing.step6.content')}}",
-        placement: "top"
     }
     ]});
 
