8b11c98098d5accb7a2eed76913ed14b6be382b3
Ariel Vallese <alariva@gmail.com>
2016-11-30 11:54:01 -0300

    [i18n] Update wording

--- resources/lang/en_US/emptystate.php
+++ resources/lang/en_US/emptystate.php
@@ -3,8 +3,8 @@
 return  [
   'manager' => [
     'appointments' => [
-      'title' => 'You don\'t have current appointments',
-      'hint'  => 'Let your customers know you provide services! or start by adding a new Contact to your addressbook and give him an Appointment!',
+      'title' => 'Your schedule is empty',
+      'hint'  => 'Share your timegrid page to your clients to start taking reservations.',
     ],
   ],
 ];
