3c36eeec6bd0822838dbe0572e3c7e2bed340f62
User <user@Users-iMac-2.local>
2017-06-05 12:20:59 +0100

    Change to the Features hover state on the welcome template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,13 +22,6 @@
         color:#fff;
         background-color: #367FA9;
     }
-    img {
-        filter: none;
-        -webkit-filter: grayscale(20%);
-        -moz-filter:    grayscale(20%);
-        -ms-filter:     grayscale(20%);
-        -o-filter:      grayscale(20%);
-    }
 	.image-container {
 		position: relative;
 		display: inline-block;
@@ -38,13 +31,6 @@
 		background-position: center center;
 		border-radius: 4px;
 	}
-    img:hover {
-        filter: none;
-        -webkit-filter: grayscale(0%);
-        -moz-filter:    grayscale(0%);
-        -ms-filter:     grayscale(0%);
-        -o-filter:      grayscale(0%);
-    }
     #inspire {font-family: 'Arvo', serif;}
     .panel{
     	text-align: center;
